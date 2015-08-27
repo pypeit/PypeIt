52fdfd98c00f29433f6091c9e2bdf08b001ade82
J. Xavier Prochaska <xavier@ucolick.org>
2015-08-27 08:15:30 -0700

    more wave_calib

--- src/pypit.py
+++ src/pypit.py
@@ -498,11 +498,12 @@ class ClassMain:
                 # Save the tilts
                 arsave.save_tilts(self, self._msarc_name)
             # Note: self._tilts is the
-            ###############
-            # Extract arc and identify lines
-            self.wv_calib = ararc.simple_calib(self, self._msarc)
+                # Setup arc parameters (e.g. linelist)
+                self._arcparam = ararc.setup(self, sc)
+                ###############
+                # Extract arc and identify lines
+                self.wv_calib = ararc.simple_calib(self)
             msgs.error("JXP :: working on wavelength calibration")
-
 
             ###############
             # Check if the user only wants to prepare the calibrations
