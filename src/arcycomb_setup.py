6fbfe35d756b30aa1e468127e35debb147667f70
Asher Wasserman <adwasser@ucsc.edu>
2016-01-14 15:53:32 -0800

    added in PyYAML in requirements, and fixed arcycomb to compile on OSX Yosemite

--- src/arcycomb_setup.py
+++ src/arcycomb_setup.py
@@ -5,6 +5,6 @@ import numpy
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("arcycomb", ["arcycomb.pyx"], )],
-    include_dirs = [numpy.get_include(),],
+    ext_modules = [Extension("arcycomb", ["arcycomb.pyx"],
+                             include_dirs=[numpy.get_include()])]
 )
