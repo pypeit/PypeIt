6268d7553d6dc12db80b8c788dceecbf68778694
J. Xavier Prochaska <xavier@ucolick.org>
2016-04-19 08:48:40 -0700

    done?

--- src/pypit.py
+++ src/pypit.py
@@ -27,7 +27,8 @@ except ImportError:
     import pdb as debugger
 
 
-def PYPIT(redname, progname=__file__, quick=False, ncpus=1, verbose=1, logname=None):
+def PYPIT(redname, progname=__file__, quick=False, ncpus=1, verbose=1,
+          logname=None, use_masters=False):
     """
     Main driver of the PYPIT code. Default settings and
     user-specified changes are made, and passed to the
@@ -52,6 +53,8 @@ def PYPIT(redname, progname=__file__, quick=False, ncpus=1, verbose=1, logname=N
         0 = No output
         1 = Minimal output (default - suitable for the average user)
         2 = All output
+    use_masters : bool, optional
+      Load calibration files from MasterFrames directory, if they exist
     logname : string
       The name of an ascii log file which is used to
       save the output details of the reduction
@@ -116,6 +119,10 @@ def PYPIT(redname, progname=__file__, quick=False, ncpus=1, verbose=1, logname=N
     # Load any changes to the spectrograph settings based on the user input file
     spect = arload.load_spect(progname, specname, spect=spect, lines=spclines)
 
+    # Command line arguments
+    if use_masters:
+        argflag['masters']['use'] = True
+
     # If a quick reduction has been requested, make sure the requested pipeline
     # is the quick implementation (if it exists), otherwise run the standard pipeline.
     if quick:
@@ -171,13 +178,15 @@ if __name__ == "__main__":
     qck = False
     cpu = 1
     vrb = 2
+    use_masters = False
 
     if len(sys.argv) < 2:
         initmsgs.usage(None)
 
     # Load options from command line
     try:
-        opt, arg = getopt.getopt(sys.argv[1:], 'hqc:v:', ['help',
+        opt, arg = getopt.getopt(sys.argv[1:], 'hmqc:v:', ['help',
+                                                           'use_masters',
                                                           'quick',
                                                           'cpus',
                                                           'verbose'])
@@ -190,6 +199,8 @@ if __name__ == "__main__":
                 cpu = int(a)
             elif o in ('-v', '--verbose'):
                 vrb = int(a)
+            elif o in ('-m', '--use_masters'):
+                use_masters=True
         lnm = os.path.splitext(arg[0])[0] + ".log"
         red = arg[0]
     except getopt.GetoptError, err:
@@ -197,10 +208,12 @@ if __name__ == "__main__":
 
     # Execute the reduction, and catch any bugs for printout
     if debug['develop']:
-        PYPIT(red, progname=sys.argv[0], quick=qck, ncpus=cpu, verbose=vrb, logname=lnm)
+        PYPIT(red, progname=sys.argv[0], quick=qck, ncpus=cpu, verbose=vrb,
+              logname=lnm, use_masters=use_masters)
     else:
         try:
-            PYPIT(red, progname=sys.argv[0], quick=qck, ncpus=cpu, verbose=vrb, logname=lnm)
+            PYPIT(red, progname=sys.argv[0], quick=qck, ncpus=cpu, verbose=vrb,
+                  logname=lnm, use_masters=use_masters)
         except:
             # There is a bug in the code, print the file and line number of the error.
             et, ev, tb = sys.exc_info()
