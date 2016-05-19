f656223de923df63a6140c1fb522044bfb2eec91
rcooke-ast <rcooke.ast@gmail.com>
2016-05-18 17:51:31 -0700

    Added functionality to save argflag settings (see Issue #53)

--- src/arparse.py
+++ src/arparse.py
@@ -23,19 +23,35 @@ class NestedDict(dict):
 
 
 class BaseArgFlag:
-    def __init__(self):
+    def __init__(self, defname, savname):
         self._argflag = NestedDict()
-        self._argflag["run"]["ncpus"] = 1
-        self._argflag["a"]["b"]["c"]["d"]["e"] = 5
-        self._argflag["a"]["b"]["c"]["d"]["f"] = 6
-        self._argflag["a"]["b"]["c"]["next"] = 3
-        self._argflag["a"]["b"]["c"]["s"] = "zing"
-        self._argflag["a"]["b"]["g"]["d"]["e"] = "foo"
-        self._argflag["a"]["h"]["c"]["d"]["e"] = "bar"
-        print self._argflag
+        self._defname = defname
+        self._afout = open(savname, 'w')
+        # self._argflag["run"]["ncpus"] = 1
+        # self._argflag["a"]["b"]["c"]["d"]["e"] = 5
+        # self._argflag["a"]["b"]["c"]["d"]["f"] = 6
+        # self._argflag["a"]["b"]["c"]["next"] = 3
+        # self._argflag["a"]["b"]["c"]["s"] = "zing"
+        # self._argflag["a"]["b"]["g"]["d"]["e"] = "foo"
+        # self._argflag["a"]["h"]["c"]["d"]["e"] = "bar"
+        # Load the default settings
+        self.load()
 
-    def a_b_c_d_e(self, v):
-        self.update(int(v))
+    def load(self):
+        msgs.info("Loading the default settings")
+        lines = open(self._defname, 'r').readlines()
+        self.load_lines(lines)
+        return
+
+    def load_lines(self, lines):
+        for ll in lines:
+            if len(ll.strip()) == 0:
+                # Nothing on a line
+                continue
+            elif ll.strip()[0] == '#':
+                # A comment line
+                continue
+        return
 
     def run_ncpus(self, v):
         # Check that v is allowed
@@ -74,6 +90,38 @@ class BaseArgFlag:
         # Update argument
         self.update(v)
 
+    def save(self):
+        """
+        Save the settings used for this reduction
+        """
+        def savedict(dct):
+            for key, value in dct.iteritems():
+                self._afout.write(str(key))
+                if isinstance(value, dict):
+                    savedict(value)
+                else:
+                    self._afout.write(" " + str(value) + "\n")
+        savedict(self._argflag.copy())
+        self._afout.close()
+        return
+
+    def set_flag(self, lst):
+        cnt = 1
+        func = None
+        while cnt < len(lst):
+            try:
+                func = "self." + "_".join(lst[:-cnt]) + "({0:s})".format(" ".join(lst[-cnt:]))
+                break
+            except:
+                cnt += 1
+                continue
+        if func is not None:
+            eval(func)
+        else:
+            msgs.error("There appears to be an error on the following input line:" + msgs.newline() +
+                       " ".join(lst))
+        return
+
     def update(self, v, ll=None):
         """
         Update an element in argflag
@@ -102,23 +150,6 @@ class BaseArgFlag:
             udct = dict({ll[-ii-1]: udct.copy()})
         # Update the master dictionary
         self._argflag = ingest(dstr, udct).copy()
-        return
-
-    def set_flag(self, lst):
-        cnt = 1
-        func = None
-        while cnt < len(lst):
-            try:
-                func = "self." + "_".join(lst[:-cnt]) + "({0:s})".format(" ".join(lst[-cnt:]))
-                break
-            except:
-                cnt += 1
-                continue
-        if func is not None:
-            eval(func)
-        else:
-            msgs.error("There appears to be an error on the following input line:" + msgs.newline() +
-                       " ".join(lst))
         return
 
 
@@ -184,8 +215,9 @@ def get_spect(init=None):
 
     return pypit_spect
 
+"""
 af = get_argflag("ARMLSD")
 af.set_flag(["run", "ncpus", "5"])
 af.set_flag(["a", "b", "c", "d", "e", "47"])
 pdb.set_trace()
-
+"""
