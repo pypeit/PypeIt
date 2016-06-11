8fd3a5f03c156c3abc91dc61d06d7ba387119959
J. Xavier Prochaska <xavier@ucolick.org>
2016-06-10 21:57:57 -0700

    skip test

--- pypit/tests/test_scripts.py
+++ pypit/tests/test_scripts.py
@@ -8,6 +8,8 @@ matplotlib.use('Agg')  # For Travis
 import sys, os
 import pytest
 
+skip = pytest.mark.skip
+
 from pypit.scripts import arcid_plot, show_1dspec, view_fits
 from pypit import pyputils
 msgs = pyputils.get_dummy_logger()
@@ -27,12 +29,14 @@ def test_arcid_plot():
     arcid_plot.main(pargs)
 
 
-@pytest.mark.skip()
+"""
+@skip
 def test_show_1dspec():
     spec_file = data_path('spec1d_J0025-0312_KASTr_2015Jan23T025323.85.fits')
     pargs = show_1dspec.parser([spec_file])
     # Run
     show_1dspec.main(pargs, unit_test=True)
+"""
 
 def test_view_fits():
     """ Only test the list option
