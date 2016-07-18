1910ae3b6f0563dc3aa2fa2b08593326152bd978
Asher Wasserman <asher.d.wasserman@gmail.com>
2016-07-18 00:06:56 -0700

    associating slits to detectors needs work still

--- pypit/ardeimos.py
+++ pypit/ardeimos.py
@@ -1,12 +1,12 @@
 '''
-Implements DEIMOS-specific functions, including reading in slitmask design files and lamp ID.
+Implements DEIMOS-specific functions, including reading in slitmask design files.
 '''
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
 from astropy.io import fits
-from pypit import armsgs
-# import armsgs
+# from pypit import armsgs
+import armsgs
 try:
     from xastropy.xutils import xdebug as debugger
 except:
@@ -30,7 +30,7 @@ class DEIMOS_slits(object):
         BluSlits - contains the slit corners in arcseconds away from the center
     '''
 
-    def __init__(self, hdulist, plate_scale=0.1185):
+    def __init__(self, hdulist, plate_scale=0.1185, pix_per_chip=2048):
         '''
         Parameters
         ----------
@@ -38,6 +38,7 @@ class DEIMOS_slits(object):
                   the fits filename, or an HDUList instance from
                   astropy's fits module
         plate_scale : the DEIMOS plate scale, arcseconds per pixel
+        pix_per_chip : number of pixels along the spatial direction of CCDs
         '''
 
         if isinstance(hdulist, str):
@@ -47,8 +48,10 @@ class DEIMOS_slits(object):
         
         # mask attributes
         self.plate_scale = plate_scale
+        self.pix_per_chip = pix_per_chip
         
-        error_str = ' bin table not found, verify the FITS file is in the correct format!'
+        error_str = (' bin table not found, verify the FITS file '
+                     'is in the correct format!')
         try:
             mask_design = hdulist['MaskDesign'].data
         except KeyError:
@@ -80,10 +83,11 @@ class DEIMOS_slits(object):
         self.slit_pa = desi_slits['slitLPA']           # in degrees ccw of North
         self.slit_length = desi_slits['slitLen']       # in arcsec
         self.slit_width = desi_slits['slitWid']        # in arcsec
-        self.slit_isalign = (desi_slits['slitTyp'] == 'A')  # boolean array, true if alignment slit
+        self.slit_isalign = (desi_slits['slitTyp'] == 'A')  # boolean array
         # assign slit names based on the DSIM input names
         assert len(desi_slits) == len(slit_obj_map)
-        idx = np.array([(i in slit_obj_map['ObjectId']) for i in obj_cat['ObjectId']])
+        idx = np.array([(i in slit_obj_map['ObjectId'])
+                        for i in obj_cat['ObjectId']])
         self.slit_name = obj_cat[idx]['OBJECT']
 
         # [b]ottom/[t]op, [l]eft/[r]ight corners, given as [nslits x 2] arrays
@@ -96,13 +100,123 @@ class DEIMOS_slits(object):
         self.slit_tlc = np.array(zip(blu_slits['SlitX3'], blu_slits['SlitY3']))
         self.slit_trc = np.array(zip(blu_slits['SlitX4'], blu_slits['SlitY4']))
 
+        # check that slits have paired left and paired right corneres
+        assert np.all(np.isclose(self.slit_brc[:, 0],
+                                 self.slit_trc[:, 0], atol=0.01))
+        assert np.all(np.isclose(self.slit_blc[:, 0],
+                                 self.slit_tlc[:, 0], atol=0.01))
+        
     def __repr__(self):
-        return '<DEIMOS_slits: ' + self.mask_name + ' (' + str(self.nslits) + ' slits)>'
+        return ('<DEIMOS_slits: ' + self.mask_name +
+                ' (' + str(self.nslits) + ' slits)>')
 
     def __len__(self):
         return self.nslits
 
-    def get_edges(self, det):
+    def mosaic_location(self, det):
+        '''
+        Locates the chip within the DEIMOS detector mosaic.
+
+        Parameters
+        ----------
+        det : int, detector index, measured from 1, matching the 'detXX' syntax 
+              in the settings.deimos file
+
+        Returns
+        -------
+        row : int, bottow (0) is blue side, top (1) is red side
+        col : int, left to right
+        '''
+        # check that the requested detector(s) make(s) sense
+        try:
+            good = det in range(1, 9)
+        except ValueError:
+            # det should be iterable
+            good = all([d in range(1, 9) for d in det])
+            det = np.array(det)
+        assert good
+        return divmod(det - 1, 4)
+
+    def mask_to_det_coords(self, x, det):
+        '''
+        Transform mask spatial arcsec coordinates to pixel coordinates of the 
+        detector.
+
+        Parameters
+        ----------
+        x : float, arcsec in mask coord, can be array
+        det : int, detector index, measured from 1, matching the 'detXX' syntax 
+              in the settings.deimos file
+
+        Returns
+        -------
+        pix_x : float, detector pixel value of mask coord, will be array if 
+                input is an array
+        '''
+        row, col = self.mosaic_location(det)
+        
+        # bottom row detectors are oriented with origin in blc right-handed
+        pix_x = x / self.plate_scale - (col - 2) * self.pix_per_chip
+        if row == 1:
+            # top row detectors are oriented with origin in trc, right-handed
+            pix_x = self.pix_per_chip - pix_x
+        return pix_x
+
+    def det_to_mask_coords(self, pix_x, det):
+        '''
+
+        Transforms detector pixel coordinate to mask coordinate in arcsec.
+
+        Parameters
+        ----------
+        pix_x : float, detector pixel value of mask coord, can be array
+        det : int, detector index, measured from 1, matching the 'detXX' syntax 
+              in the settings.deimos file
+
+        Returns
+        -------
+        x : float, arcsec in mask coord, will be array if pix_x is one
+        '''
+
+        row, col = self.mosaic_location(det)
+        
+        if row == 1:
+            pix_x = self.pix_per_chip - pix_x
+        x = (pix_x + (col - 2) * self.pix_per_chip) * self.plate_scale
+        return x
+            
+    def slits_in_det(self, det):
+        '''
+        Gets a boolean array corresponding to slits falling on the specified
+        detector.
+
+        Parameters
+        ----------
+        det : int, detector index, measured from 1, matching the 'detXX' syntax 
+              in the settings.deimos file
+        
+        Returns
+        -------
+        slits : boolean array of size nslits
+        '''
+        row, col = self.mosaic_location(det)
+
+        if row == 0:
+            left = 0
+            right = self.pix_per_chip
+        else:
+            left = self.pix_per_chip
+            right = 0
+        chip_left_edge = self.det_to_mask_coords(left, det)
+        chip_right_edge = self.det_to_mask_coords(right, det)
+
+        left_edges = self.slit_blc[:, 0]
+        right_edges = self.slit_brc[:, 0]
+        slits = (chip_left_edge < left_edges) & (right_edges < chip_right_edge)
+        return slits
+        
+
+    def get_edges(self, det, sort=True):
         '''
         Calculates the pixel values of slit edges for the associated detector.
 
@@ -113,6 +227,7 @@ class DEIMOS_slits(object):
         ----------
         det : int, detector index, measured from 1, matching the 'detXX' syntax 
               in the settings.deimos file
+        sort : boolean, if true, sort in ascending order
 
         Returns
         -------
@@ -120,6 +235,19 @@ class DEIMOS_slits(object):
                 Each row in the array gives the left and right pixel values of
                 the slit.
         '''
-        pass
+        slits = self.slits_in_det(det)
+
+        left_edges = self.mask_to_det_coords(self.slit_blc[:, 0][slits], det)
+        right_edges = self.mask_to_det_coords(self.slit_brc[:, 0][slits], det)
+        names = self.slit_name[slits]
+        
+        if sort:
+            idx = np.argsort(left_edges)
+            left_edges = left_edges[idx]
+            right_edges = right_edges[idx]
+            names = names[idx]
+            
+        return names, np.column_stack([left_edges, right_edges])
+        
 
     
