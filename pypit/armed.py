7ed35cf8d1621050d77f2669c74b5ff865704572
rcooke-ast <rcooke.ast@gmail.com>
2017-01-06 20:08:22 +0000

    echelle tilt

--- pypit/armed.py
+++ pypit/armed.py
@@ -148,8 +148,11 @@ def ARMED(fitsdict, reuseMaster=False, reloadMaster=True):
                 armbase.UpdateMasters(sciexp, sc, det, ftype="flat", chktype="trace")
             ###############
             # Prepare the pixel flat field frame
-            update = slf.MasterFlatField(fitsdict, det)
-            if update and reuseMaster: armbase.UpdateMasters(sciexp, sc, det, ftype="flat", chktype="pixelflat")
+            if False:
+                update = slf.MasterFlatField(fitsdict, det)
+                if update and reuseMaster: armbase.UpdateMasters(sciexp, sc, det, ftype="flat", chktype="pixelflat")
+            else:
+                msgs.bug("PIXEL FLAT NOT BEING CREATED!!!")
             ###############
             # Generate the 1D wavelength solution
             update = slf.MasterWaveCalib(fitsdict, sc, det)
@@ -170,7 +173,7 @@ def ARMED(fitsdict, reuseMaster=False, reloadMaster=True):
                         settings.argflag['reduce']['masters']['loaded'].append('tilts'+settings.argflag['reduce']['masters']['setup'])
                 if 'tilts'+settings.argflag['reduce']['masters']['setup'] not in settings.argflag['reduce']['masters']['loaded']:
                     # First time tilts are derived for this arc frame --> derive the order tilts
-                    tilts, satmask, outpar = artrace.model_tilt(slf, det, slf._msarc[det-1])
+                    tilts, satmask, outpar = artrace.echelle_tilt(slf, slf._msarc[det-1], det)
                     slf.SetFrame(slf._tilts, tilts, det)
                     slf.SetFrame(slf._satmask, satmask, det)
                     slf.SetFrame(slf._tiltpar, outpar, det)
