e6f299e6d8a2b5e3158f0085e87a7cdfb0000fb2
J. Xavier Prochaska <xavier@ucolick.org>
2017-02-07 09:59:18 -0800

    objname in 1dspec + test

--- pypit/scripts/show_1dspec.py
+++ pypit/scripts/show_1dspec.py
@@ -13,6 +13,7 @@ def parser(options=None):
     parser.add_argument("file", type=str, help="Spectral file")
     parser.add_argument("--list", default=False, help="List the extensions only?", action="store_true")
     parser.add_argument("--exten", type=int, default=1, help="FITS extension")
+    parser.add_argument("--obj", type=str, help="Object name in lieu of extension, e.g. O424-S1466-D02-I0013")
     parser.add_argument("--extract", type=str, default='box', help="Extraction method. Default is boxcar. ['box', 'opt']")
 
     if options is None:
@@ -42,7 +43,7 @@ def main(args, unit_test=False):
     from pypit import arload
 
     # Load spectrum
-    spec = arload.load_1dspec(args.file, exten=args.exten, extract=args.extract)
+    spec = arload.load_1dspec(args.file, exten=args.exten, extract=args.extract, objname=args.obj)
 
     if unit_test is False:
         from PyQt4 import QtGui
