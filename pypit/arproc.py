642febf61a0a47cac42cf81c757c2a2d662a5604
J. Xavier Prochaska <xavier@ucolick.org>
2017-05-06 18:34:58 -0700

    int

--- pypit/arproc.py
+++ pypit/arproc.py
@@ -787,8 +787,7 @@ def reduce_prepare(slf, sciframe, scidx, fitsdict, det, standard=False):
       Standard star frame?
     """
     # Check inputs
-    if not isinstance(scidx, int):
-        debugger.set_trace()
+    if not isinstance(scidx, (int,np.integer)):
         raise IOError("scidx needs to be an int")
     # Convert ADUs to electrons
     sciframe *= gain_frame(slf, det)
