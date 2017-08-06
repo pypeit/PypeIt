8afd3be5f7679a96cb7a0185c3debe343e06e752
rcooke-ast <rcooke.ast@gmail.com>
2017-08-06 17:36:44 +0100

    remove clean_edges

--- pypit/arcytrace.pyx
+++ pypit/arcytrace.pyx
@@ -34,52 +34,6 @@ cdef extern from "gsl/gsl_multifit.h":
 
 
 #@cython.boundscheck(False)
-#def clean_edges(np.ndarray[DTYPE_t, ndim=2] diff not None,
-#                np.ndarray[DTYPE_t, ndim=2] tedges not None):
-#    cdef int sz_x, sz_y
-#    cdef int x, y
-#    cdef int ldct, rdct, ymax
-#    cdef double lmax, rmax
-#
-#    sz_x = tedges.shape[0]
-#    sz_y = tedges.shape[1]
-#
-#    # Set up the array which marks the edge detections
-#    cdef np.ndarray[ITYPE_t, ndim=2] edgdet = np.zeros((sz_x, sz_y), dtype=ITYPE)
-#
-#    for x in range(sz_x):
-#        ldct = 0
-#        rdct = 0
-#        for y in range(sz_y):
-#            if tedges[x, y] == -1.0:
-#                if ldct == 1:
-#                    if diff[x, y] > lmax:  # Recall, diff is very positive for significant left edge detections
-#                        lmax = diff[x, y]
-#                        ymax = y
-#                else:
-#                    lmax = diff[x, y]
-#                    ymax = y
-#                    ldct = 1
-#            elif tedges[x, y] == 1.0:
-#                if rdct == 1:
-#                    if diff[x, y] < rmax:  # Recall, diff is very negative for significant right edge detections
-#                        rmax = diff[x, y]
-#                        ymax = y
-#                else:
-#                    rmax = diff[x, y]
-#                    ymax = y
-#                    rdct = 1
-#            else:
-#                if ldct == 1:
-#                    edgdet[x, ymax] = -1
-#                    ldct = 0
-#                elif rdct == 1:
-#                    edgdet[x, ymax] = +1
-#                    rdct = 0
-#    return edgdet
-
-
-#@cython.boundscheck(False)
 def close_edges(np.ndarray[ITYPE_t, ndim=2] edgdet not None,
                 np.ndarray[ITYPE_t, ndim=1] dets not None,
                 int npix):
