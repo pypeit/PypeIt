88993041dbc032a35d4c56404c2c044c691d5d41
rcooke-ast <rcooke.ast@gmail.com>
2017-10-14 14:43:42 +0100

    change pixt

--- pypit/ararc.py
+++ pypit/ararc.py
@@ -120,9 +120,9 @@ def detect_lines(slf, det, msarc, censpec=None, MK_SATMASK=False):
     # Find all significant detections
     # The last argument is the overall minimum significance level of an arc line detection and the second
     # last argument is the level required by an individual pixel before the neighbourhood of this pixel is searched.
-    tpixt, num = arcyarc.detections_sigma(yprep, yerr, np.zeros(censpec.shape[0], dtype=np.int), siglev/2.0, siglev)
-    pixt = arcyarc.remove_similar(tpixt, num)
-    pixt = pixt[np.where(pixt != -1)].astype(np.int)
+    pixt = np.where((yprep/yerr>0.0) & (yprep>np.roll(yprep,1)) & (yprep>=np.roll(yprep,-1))
+                                      & (np.roll(yprep,1)>np.roll(yprep,2)) & (np.roll(yprep,-1)>np.roll(yprep,-2))
+                                      & (np.roll(yprep,2)>np.roll(yprep,3)) & (np.roll(yprep,-2)>np.roll(yprep,-3)))[0]
     tampl, tcent, twid, ngood = arcyarc.fit_arcorder(xrng, yprep, pixt, fitp)
     w = np.where((~np.isnan(twid)) & (twid > 0.0) & (twid < 10.0/2.35) & (tcent > 0.0) & (tcent < xrng[-1]))
     # Check the results
