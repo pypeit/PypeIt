f23251f486b02541677bdfb3850bfe49d7848536
thsyu <thsyu@ucsc.edu>
2017-11-13 15:57:28 -0800

    add manual extraction keyword and script

--- pypit/arproc.py
+++ pypit/arproc.py
@@ -948,7 +948,7 @@ def reduce_multislit(slf, sciframe, scidx, fitsdict, det, standard=False):
     ###############
     # Find objects and estimate their traces
     scitrace = artrace.trace_object(slf, det, sciframe-bgframe, modelvarframe, crmask,
-                                    bgreg=20, doqa=False)
+                                    bgreg=20, doqa=False, standard=standard)
     if scitrace is None:
         msgs.info("Not performing extraction for science frame"+msgs.newline()+fitsdict['filename'][scidx[0]])
         debugger.set_trace()
@@ -1024,7 +1024,7 @@ def reduce_frame(slf, sciframe, rawvarframe, modelvarframe, bgframe, scidx, fits
     if scitrace is None:
         msgs.info("Performing final object trace")
         scitrace = artrace.trace_object(slf, det, sciframe-bgframe, modelvarframe, crmask,
-                                        bgreg=20, doqa=(not standard))
+                                        bgreg=20, doqa=(not standard), standard=standard)
     if standard:
         slf._msstd[det-1]['trace'] = scitrace
         specobjs = arspecobj.init_exp(slf, scidx, det, fitsdict, scitrace, objtype='standard')
