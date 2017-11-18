8e456bbbbb82d633865761c2b2ee39a78893eee1
rcooke-ast <rcooke.ast@gmail.com>
2017-11-18 21:18:41 +0000

    LRIS iraf fmt

--- pypit/arproc.py
+++ pypit/arproc.py
@@ -596,9 +596,9 @@ def get_datasec_trimmed(slf, fitsdict, det, scidx):
         # Loop on amplifiers
         for kk in range(settings.spect[dnum]['numamplifiers']):
             datasec = "datasec{0:02d}".format(kk+1)
-            settings.spect[dnum][datasec] = settings.load_sections(secs[0][kk])
+            settings.spect[dnum][datasec] = settings.load_sections(secs[0][kk], fmt_iraf=False)
             oscansec = "oscansec{0:02d}".format(kk+1)
-            settings.spect[dnum][oscansec] = settings.load_sections(secs[1][kk])
+            settings.spect[dnum][oscansec] = settings.load_sections(secs[1][kk], fmt_iraf=False)
     # For convenience
     naxis0, naxis1 = int(fitsdict['naxis0'][scidx]), int(fitsdict['naxis1'][scidx])
     # Initialize the returned array
