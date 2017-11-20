2c15d4ffc854c49f9c27d7c6a324f8fd79eda0aa
rcooke-ast <rcooke.ast@gmail.com>
2017-11-20 20:49:35 +0000

    better detect_lines

--- pypit/ararc.py
+++ pypit/ararc.py
@@ -48,7 +48,7 @@ def detect_lines(slf, det, msarc, censpec=None, MK_SATMASK=False):
       An index array indicating which detections are the most reliable.
     satsnd : ndarray
       A mask indicating where which pixels contain saturation streaks
-    yprep : ndarray
+    detns : ndarray
       The spectrum used to find detections. This spectrum has
       had any "continuum" emission subtracted off
     """
@@ -82,60 +82,30 @@ def detect_lines(slf, det, msarc, censpec=None, MK_SATMASK=False):
         satsnd = np.zeros_like(ordcen)
     # Detect the location of the arc lines
     msgs.info("Detecting the strongest, nonsaturated lines")
-    #####
-    # Old algorithm for arc line detection
-#   arcdet = arcyarc.detections_allorders(censpec, satsnd)
-    #####
-    # New algorithm for arc line detection
-    #pixels=[]
-    siglev = settings.argflag['arc']['calibrate']['detection']
-    bpfit = 5  # order of the polynomial used to fit the background 'continuum'
+
     fitp = settings.argflag['arc']['calibrate']['nfitpix']
-    if len(censpec.shape) == 3: detns = censpec[:, 0].flatten()
-    else: detns = censpec.copy()
+    if len(censpec.shape) == 3:
+        detns = censpec[:, 0].flatten()
+    else:
+        detns = censpec.copy()
     xrng = np.arange(float(detns.size))
-    yrng = np.zeros(detns.size)
-    mask = np.zeros(detns.size, dtype=np.int)
-    mask[np.where(detns < 0.0)] = 1.0
-    mskcnt = np.sum(mask)
-    while True:
-        w = np.where(mask == 0)
-        xfit = xrng[w]
-        yfit = detns[w]
-        ct = np.polyfit(xfit, yfit, bpfit)
-        yrng = np.polyval(ct, xrng)
-        sigmed = 1.4826*np.median(np.abs(detns[w]-yrng[w]))
-        w = np.where(detns > yrng + siglev*sigmed)
-        mask[w] = 1
-        if mskcnt == np.sum(mask):
-            break  # No new values have been included in the mask
-        mskcnt = np.sum(mask)
-    w = np.where(mask == 0)
-    xfit = xrng[w]
-    yprep = detns - yrng
-    sfit = 1.4826*np.abs(detns[w]-yrng[w])
-    ct = np.polyfit(xfit, sfit, bpfit)
-    yerr = np.polyval(ct, xrng)
-    myerr = np.median(np.sort(yerr)[:yerr.size//2])
-    yerr[np.where(yerr < myerr)] = myerr
+
     # Find all significant detections
-    # The last argument is the overall minimum significance level of an arc line detection and the second
-    # last argument is the level required by an individual pixel before the neighbourhood of this pixel is searched.
-    pixt = np.where((yprep/yerr > 0.0) &  # (yprep < slf._nonlinear[det-1]) &
-                    (yprep > np.roll(yprep, 1)) & (yprep >= np.roll(yprep, -1)) &
-                    (np.roll(yprep, 1) > np.roll(yprep, 2)) & (np.roll(yprep, -1) > np.roll(yprep, -2)) &#)[0]
-                    (np.roll(yprep, 2) > np.roll(yprep, 3)) & (np.roll(yprep, -2) > np.roll(yprep, -3)))[0]
-#                    (np.roll(yprep, 3) > np.roll(yprep, 4)) & (np.roll(yprep, -3) > np.roll(yprep, -4)) & # )[0]
-#                    (np.roll(yprep, 4) > np.roll(yprep, 5)) & (np.roll(yprep, -4) > np.roll(yprep, -5)))[0]
-    tampl, tcent, twid, ngood = arcyarc.fit_arcorder(xrng, yprep, pixt, fitp)
+    pixt = np.where((detns > 0.0) &  # (detns < slf._nonlinear[det-1]) &
+                    (detns > np.roll(detns, 1)) & (detns >= np.roll(detns, -1)) &
+                    (np.roll(detns, 1) > np.roll(detns, 2)) & (np.roll(detns, -1) > np.roll(detns, -2)) &#)[0]
+                    (np.roll(detns, 2) > np.roll(detns, 3)) & (np.roll(detns, -2) > np.roll(detns, -3)))[0]
+#                    (np.roll(detns, 3) > np.roll(detns, 4)) & (np.roll(detns, -3) > np.roll(detns, -4)) & # )[0]
+#                    (np.roll(detns, 4) > np.roll(detns, 5)) & (np.roll(detns, -4) > np.roll(detns, -5)))[0]
+    tampl, tcent, twid, ngood = arcyarc.fit_arcorder(xrng, detns, pixt, fitp)
     w = np.where((~np.isnan(twid)) & (twid > 0.0) & (twid < 10.0/2.35) & (tcent > 0.0) & (tcent < xrng[-1]))
     # Check the results
     #plt.clf()
-    #plt.plot(xrng,yprep,'k-')
+    #plt.plot(xrng,detns,'k-')
     #plt.plot(tcent,tampl,'ro')
     #plt.show()
     # Return
-    return tampl, tcent, twid, w, satsnd, yprep
+    return tampl, tcent, twid, w, satsnd, detns
 
 
 def setup_param(slf, sc, det, fitsdict):
