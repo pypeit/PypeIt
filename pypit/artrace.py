3c7b1d876cbb84cd157baa6ca7d7a30dee3311b3
rcooke-ast <rcooke.ast@gmail.com>
2018-04-12 11:21:30 +0100

    start of new algorithm

--- pypit/artrace.py
+++ pypit/artrace.py
@@ -376,25 +376,34 @@ def trace_objbg_image(slf, det, sciframe, slitn, objreg, bgreg, trim=2, triml=No
     yint = np.append(-yint[1], np.append(yint, 2.0-yint[-2]))
     msgs.info("Creating an image weighted by object pixels")
     rec_obj_img = np.zeros((sciframe.shape[0], sciframe.shape[1], nobj))
+    import pdb, time
     for o in range(nobj):
         #msgs.info("obj {:d}".format(o))
-        obj = np.zeros(npix)
-        obj[objreg[0][o]:objreg[1][o]+1] = 1
-        scitmp = np.append(0.0, np.append(obj, 0.0))
-        objframe = scitmp.reshape(1, -1).repeat(sciframe.shape[0], axis=0)
-        objspl = interp.RectBivariateSpline(xint, yint, objframe, bbox=[0.0, 1.0, yint.min(), yint.max()], kx=1, ky=1, s=0)
-        xx, yy = np.meshgrid(np.linspace(0, 1.0, sciframe.shape[0]), np.arange(0, sciframe.shape[1]), indexing='ij')
-        lo = (slf._lordloc[det-1][:, slitn]+triml).reshape((-1, 1))
-        vv = ((yy-lo)/(npix-1.0)).flatten()
-        xx = xx.flatten()
-        wf = np.where((vv >= yint[0]) & (vv <= yint[-1]))
-        rec_obj_arr = objspl.ev(xx[wf], vv[wf])
-        idxarr = np.zeros(sciframe.shape).flatten()
-        idxarr[wf] = 1
-        idxarr = idxarr.reshape(sciframe.shape)
-        rec_img = np.zeros_like(sciframe)
-        rec_img[np.where(idxarr == 1)] = rec_obj_arr
-        rec_obj_img[:, :, o] = rec_img.copy()
+        if settings.spect['mosaic']['reduction'] == 'ARMED' or True:
+            a = time.time()
+            obj = np.zeros(npix)
+            obj[objreg[0][o]:objreg[1][o]+1] = 1
+            scitmp = np.append(0.0, np.append(obj, 0.0))
+            objframe = scitmp.reshape(1, -1).repeat(sciframe.shape[0], axis=0)
+            objspl = interp.RectBivariateSpline(xint, yint, objframe, bbox=[0.0, 1.0, yint.min(), yint.max()], kx=1, ky=1, s=0)
+            xx, yy = np.meshgrid(np.linspace(0, 1.0, sciframe.shape[0]), np.arange(0, sciframe.shape[1]), indexing='ij')
+            lo = (slf._lordloc[det-1][:, slitn]+triml).reshape((-1, 1))
+            vv = ((yy-lo)/(npix-1.0)).flatten()
+            xx = xx.flatten()
+            wf = np.where((vv >= yint[0]) & (vv <= yint[-1]))
+            rec_obj_arr = objspl.ev(xx[wf], vv[wf])
+            idxarr = np.zeros(sciframe.shape).flatten()
+            idxarr[wf] = 1
+            idxarr = idxarr.reshape(sciframe.shape)
+            rec_img = np.zeros_like(sciframe)
+            rec_img[np.where(idxarr == 1)] = rec_obj_arr
+            rec_obj_img[:, :, o] = rec_img.copy()
+            print(time.time()-a, 's')
+            pdb.set_trace()
+        elif settings.spect['mosaic']['reduction'] == 'ARMLSD':
+            pass
+        else:
+            msgs.bug("Not ready for this type of reduction: " + settings.spect['mosaic']['reduction'])
     # Make an image of pixel weights for the background region of each object
     msgs.info("Creating an image weighted by background pixels")
     rec_bg_img = np.zeros((sciframe.shape[0], sciframe.shape[1], nobj))
