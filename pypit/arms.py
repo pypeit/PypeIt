a4f2874516ceaf15ec00739a36e3a2f6aa25667d
profxj <xavier@ucolick.org>
2018-05-25 14:43:07 -0700

    edits to RC

--- pypit/arms.py
+++ pypit/arms.py
@@ -30,9 +30,9 @@ from pypit import traceimage
 from pypit import ardebug as debugger
 
 
-def ARMLSD(fitstbl, setup_dict, reuseMaster=False, reloadMaster=True, sciexp=None):
+def ARMS(fitstbl, setup_dict, reuseMaster=False, reloadMaster=True, sciexp=None):
     """
-    Automatic Reduction and Modeling of Long Slit Data
+    Automatic Reduction of Multislit Data
 
     Parameters
     ----------
