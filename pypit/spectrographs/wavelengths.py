0a87398c5b8aa0c0fe5bf4c4b7dd4783ac2bcd16
profxj <xavier@ucolick.org>
2018-06-28 20:44:37 -0700

    bug fix

--- pypit/spectrographs/wavelengths.py
+++ pypit/spectrographs/wavelengths.py
@@ -5,9 +5,8 @@ import numpy as np
 from astropy.io import fits
 
 from pypit import msgs
-from pypit.spectrographs import deimos
-from pypit.spectrographs import lris
 from pypit import arparse
+from pypit import ararclines
 
 from pypit import ardebug as debugger
 
