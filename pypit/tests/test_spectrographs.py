aef9500d1c512d83994c75ecb1c9cac8e1f83042
Kyle Westfall <kbwestfall@gmail.com>
2018-07-03 16:39:48 -0700

    spectrograph tests

---
+++ pypit/tests/test_spectrographs.py
@@ -0,0 +1,104 @@
+# Module to test spectrograph read functions
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+
+import pytest
+import glob
+
+from pypit.par.pypitpar import _pypit_root_directory
+from pypit import spectrographs
+
+# These tests are not run on Travis
+if os.getenv('PYPIT_DEV') is None:
+    skip_test=True
+else:
+    skip_test=False
+
+def test_keckdeimos():
+    s = spectrographs.keck_deimos.KeckDEIMOSSpectrograph()
+    if skip_test:
+        return
+    example_file = os.path.join(os.environ['PYPIT_DEV'], 'RAW_DATA', 'Keck_DEIMOS',
+                                '830G_L', 'd0914_0002.fits')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for Keck DEIMOS read.')
+    s.load_raw_frame(example_file)
+
+
+def test_kecklrisblue():
+    s = spectrographs.keck_lris.KeckLRISBSpectrograph()
+    if skip_test:
+        return
+    example_file = os.path.join(os.environ['PYPIT_DEV'], 'RAW_DATA', 'Keck_LRIS_blue',
+                                'long_400_3400_d560', 'LB.20160109.14149.fits.gz')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for Keck LRIS Blue read.')
+    s.load_raw_frame(example_file)
+
+
+def test_kecklrisred():
+    s = spectrographs.keck_lris.KeckLRISRSpectrograph()
+    if skip_test:
+        return
+    example_file = os.path.join(os.environ['PYPIT_DEV'], 'RAW_DATA', 'Keck_LRIS_red',
+                                'long_600_7500_d560', 'LR.20160216.05529.fits')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for Keck LRIS Red read.')
+    s.load_raw_frame(example_file)
+
+
+def test_kecknirspec():
+    s = spectrographs.keck_nirspec.KeckNIRSPECSpectrograph()
+    if skip_test:
+        return
+    example_file = os.path.join(os.environ['PYPIT_DEV'], 'RAW_DATA', 'Keck_NIRSPEC',
+                                'NIRSPEC-1', 'NS.20160414.02171.fits.gz')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for Keck LRIS Red read.')
+    s.load_raw_frame(example_file)
+
+
+def test_shanekastblue():
+    s = spectrographs.shane_kast.ShaneKastBlueSpectrograph()
+    example_file = os.path.join(_pypit_root_directory(), 'pypit', 'tests', 'files',
+                                'b1.fits.gz')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for Shane Kast Blue read.')
+    s.load_raw_frame(example_file)
+
+
+def test_shanekastred():
+    s = spectrographs.shane_kast.ShaneKastRedSpectrograph()
+    if skip_test:
+        return
+    example_file = os.path.join(os.environ['PYPIT_DEV'], 'RAW_DATA', 'Shane_Kast_red',
+                                '600_7500_d55', 'r112.fits.gz')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for Shane Kast Red read.')
+    s.load_raw_frame(example_file)
+
+
+def test_shanekastredret():
+    spectrographs.shane_kast.ShaneKastRedRetSpectrograph()
+    # TODO: Any Shane Kast Red Ret files to read?
+
+
+def test_tngdolores():
+    s = spectrographs.tng_dolores.TngDoloresSpectrograph()
+    # TODO: Any TNG Dolores files to read?
+
+
+def test_whtisisblue():
+    s = spectrographs.wht_isis.WhtIsisBlueSpectrograph()
+    if skip_test:
+        return
+    example_file = os.path.join(os.environ['PYPIT_DEV'], 'RAW_DATA', 'WHT_ISIS_blue',
+                                'long_R300B_d5300', 'r2324566.fit.gz')
+    if not os.path.isfile(example_file):
+        raise FileNotFoundError('Could not find example file for WHT ISIS Blue read.')
+    s.load_raw_frame(example_file)
+
