28f567f61407ece8010e880933da93c5416bfc5e
EmAstro <emanuele.paolo.farina@gmail.com>
2018-08-01 08:23:14 +0200

    minor changes

--- pypeit/core/pydl.py
+++ pypeit/core/pydl.py
@@ -224,7 +224,7 @@ class bspline(object):
     """
 
     def __init__(self, x, nord=4, npoly=1, bkpt=None, fullbkpt = None, bkspread=1.0,
-                 verbose=False, from_dict=None, **kwargs):
+                 verbose=False, **kwargs):
         """Init creates an object whose attributes are similar to the
         structure returned by the create_bspline function.
         """
