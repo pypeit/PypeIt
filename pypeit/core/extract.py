80f6f2e90119af6b1808e4e67a6e42c31bf4cda7
joe <joe@physics.ucsb.edu>
2018-08-24 00:20:23 +0200

    Changed object finding default SIG_THRESH to 10

--- pypeit/core/extract.py
+++ pypeit/core/extract.py
@@ -1099,7 +1099,7 @@ specobj_dict = {'setup': None, 'slitid': None, 'scidx': 1, 'det': 1, 'objtype': 
 
 def objfind(image, invvar, thismask, slit_left, slit_righ, inmask = None, FWHM = 3.0,
             HAND_EXTRACT_DICT = None, std_trace = None, ncoeff = 5, nperslit = 10,  BG_SMTH = 5.0, PKWDTH = 3.0,
-            SIG_THRESH = 7.0, PEAK_THRESH = 0.0, ABS_THRESH = 0.0, TRIM_EDG = (3,3), OBJMASK_NTHRESH = 2.0,
+            SIG_THRESH = 10.0, PEAK_THRESH = 0.0, ABS_THRESH = 0.0, TRIM_EDG = (3,3), OBJMASK_NTHRESH = 2.0,
             specobj_dict=specobj_dict, SHOW_PEAKS=True, SHOW_FITS = False, SHOW_TRACE = False):
 
     """ DOCS COMING SOON
