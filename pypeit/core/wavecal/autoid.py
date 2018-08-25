a4540ba06567e778a17ab2be6a4661027c9b9c93
rcooke-ast <rcooke.ast@gmail.com>
2018-08-25 23:10:47 +0100

    reinstate min_ampl

--- pypeit/core/wavecal/autoid.py
+++ pypeit/core/wavecal/autoid.py
@@ -298,7 +298,7 @@ def semi_brute(spec, lines, wv_cen, disp, min_ampl=300.,
 
 class General:
 
-    def __init__(self, spec, lines, ok_mask=None, min_ampl=200., islinelist=False,
+    def __init__(self, spec, lines, ok_mask=None, min_ampl=1000., islinelist=False,
               outroot=None, debug=False, verbose=False,
               fit_parm=None, lowest_ampl=200., rms_threshold=0.1,
               binw=None, bind=None, nstore=1, use_unknowns=True):
