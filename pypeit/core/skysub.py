ac042bd1c81c88d60a8f0b769f3d662e9b3c6ac1
joe <joe@physics.ucsb.edu>
2018-09-12 03:20:21 -0700

    Major bugfix in local_skysub_extract where sigma_x was being computed completely wrong causing crashes.

--- pypeit/core/skysub.py
+++ pypeit/core/skysub.py
@@ -223,7 +223,9 @@ def skyoptimal(wave,data,ivar, oprof, sortpix, sigrej = 3.0, npoly = 1, spatial 
     good = good[wave[good].argsort()]
     relative, = np.where(relative_mask[good])
 
-    (sset1, outmask_good1, yfit1, red_chi1) = utils.bspline_profile(wave[good], data[good], ivar[good], profile_basis[good, :],
+
+
+    sset1, outmask_good1, yfit1, red_chi1 = utils.bspline_profile(wave[good], data[good], ivar[good], profile_basis[good, :],
                                                               fullbkpt=fullbkpt, upper=sigrej, lower=sigrej,
                                                               relative=relative,
                                                               kwargs_reject={'groupbadpix': True, 'maxrej': 5})
@@ -237,7 +239,7 @@ def skyoptimal(wave,data,ivar, oprof, sortpix, sigrej = 3.0, npoly = 1, spatial 
     msgs.info('2nd round....')
     msgs.info('Iter     Chi^2     Rejected Pts')
 
-    (sset, outmask_good, yfit, red_chi) = utils.bspline_profile(wave[good], data[good], ivar[good] * mask1,
+    sset, outmask_good, yfit, red_chi = utils.bspline_profile(wave[good], data[good], ivar[good] * mask1,
                                                           profile_basis[good, :],
                                                           fullbkpt=fullbkpt, upper=sigrej, lower=sigrej,
                                                           relative=relative,
