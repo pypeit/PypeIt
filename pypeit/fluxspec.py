78c9fbea7e3353f8c5e990388823153a8646ebd8
EmAstro <emanuele.paolo.farina@gmail.com>
2018-09-27 17:54:48 -0700

    minor

--- pypeit/fluxspec.py
+++ pypeit/fluxspec.py
@@ -411,7 +411,6 @@ class FluxSpec(masterframe.MasterFrame):
         jsensfunc['bspline'] = jsensfunc['bspline'].to_dict()
         jsensfunc['mag_set'] = jsensfunc['mag_set'].to_dict()
         jsensfunc = linetools.utils.jsonify(jsensfunc)
-        for key in jsensfunc: print(key)
         with open(outfile, 'w') as jsonf:
             linetools.utils.savejson(outfile, jsensfunc, overwrite=True, indent=4, easy_to_read=True)
 
