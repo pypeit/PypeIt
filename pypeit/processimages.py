0f18c80cedf4f6049a5ac40423f0e3e7c83589b6
joe <joe@physics.ucsb.edu>
2018-11-16 06:52:15 -0800

    crmask now takes an argument.

--- pypeit/processimages.py
+++ pypeit/processimages.py
@@ -341,7 +341,7 @@ class ProcessImages(object):
         self.steps.append(inspect.stack()[0][3])
         return self.stack
 
-    def build_crmask(self, varframe=None, par=None):
+    def build_crmask(self, stack, varframe=None, par=None):
         """
         Generate the CR mask frame
 
@@ -366,7 +366,7 @@ class ProcessImages(object):
         # Run LA Cosmic to get the cosmic ray mask
         saturation = self.spectrograph.detector[self.det-1]['saturation']
         nonlinear = self.spectrograph.detector[self.det-1]['nonlinear']
-        self.crmask = procimg.lacosmic(self.det, self.stack, saturation, nonlinear,
+        self.crmask = procimg.lacosmic(self.det, stack, saturation, nonlinear,
                                          varframe=varframe, maxiter=self.proc_par['lamaxiter'],
                                          grow=self.proc_par['grow'],
                                          remove_compact_obj=self.proc_par['rmcompact'],
