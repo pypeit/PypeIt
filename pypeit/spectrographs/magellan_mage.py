a9e73be36b142753fb722cc4d7f222ae7df28637
EmAstro <emanuele.paolo.farina@gmail.com>
2018-11-20 15:26:14 -0800

    1st attempt to have magellan_mage to run

--- pypeit/spectrographs/magellan_mage.py
+++ pypeit/spectrographs/magellan_mage.py
@@ -31,17 +31,21 @@ class MagellanMAGESpectrograph(spectrograph.Spectrograph):
                             xgap            = 0.,
                             ygap            = 0.,
                             ysize           = 1.,
-                            platescale      = 0.3, # plate scale in arcsec/pixel
-                            darkcurr        = 1.00, # electrons/pixel/hour. From: http://www.lco.cl/telescopes-information/magellan/instruments/mage/the-mage-spectrograph-user-manual
-                            saturation      = 65535., 
-                            nonlinear       = 0.99, # CCD is linear to better than 0.5 per cent up to digital saturation (65,536 DN including bias) in the Fast readout mode.
+                            # plate scale in arcsec/pixel
+                            platescale      = 0.3,
+                            # electrons/pixel/hour. From: http://www.lco.cl/telescopes-information/magellan/instruments/mage/the-mage-spectrograph-user-manual
+                            darkcurr        = 1.00,
+                            saturation      = 65535.,
+                            # CCD is linear to better than 0.5 per cent up to digital saturation (65,536 DN including bias) in the Fast readout mode.
+                            nonlinear       = 0.99,
                             numamplifiers   = 1,
                             gain            = 1.02, # depends on the readout
                             ronoise         = 2.9, # depends on the readout
                             datasec         = '[1:2048,1:1024]',      # complementary to oscansec
                             oscansec        = '[2049:2176,1025:1152]' # as taken from the header
                             )]
-        self.norders = 15 # Taken from the MASE paper: https://arxiv.org/pdf/0910.1834.pdf
+        # Taken from the MASE paper: https://arxiv.org/pdf/0910.1834.pdf
+        self.norders = 15 
         # Uses default timeunit
         # Uses default primary_hdrext
         # self.sky_file = ?
@@ -68,13 +72,13 @@ class MagellanMAGESpectrograph(spectrograph.Spectrograph):
         # 1D wavelength solution
         par['calibrations']['wavelengths']['rms_threshold'] = 0.20  # Might be grating dependent..
         par['calibrations']['wavelengths']['sigdetect']=5.0
-        par['calibrations']['wavelengths']['lamps'] = ['OH_XSHOOTER']
+        par['calibrations']['wavelengths']['lamps'] = ['ThAr']
         par['calibrations']['wavelengths']['nonlinear_counts'] = self.detector[0]['nonlinear'] * self.detector[0]['saturation']
 
         #par['calibrations']['wavelengths']['method'] = 'reidentify'
 
         # Reidentification parameters
-        #par['calibrations']['wavelengths']['reid_arxiv'] = 'keck_nires.json'
+        #par['calibrations']['wavelengths']['reid_arxiv'] = 'magellan_thar.json'
         par['calibrations']['wavelengths']['ech_fix_format'] = True
         # Echelle parameters
         par['calibrations']['wavelengths']['echelle'] = True
@@ -90,7 +94,7 @@ class MagellanMAGESpectrograph(spectrograph.Spectrograph):
 
         # Set slits and tilts parameters
         par['calibrations']['tilts']['order'] = 2
-        par['calibrations']['tilts']['tracethresh'] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+        par['calibrations']['tilts']['tracethresh'] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
         par['calibrations']['slits']['polyorder'] = 5
         par['calibrations']['slits']['maxshift'] = 3.
         par['calibrations']['slits']['pcatype'] = 'order'
@@ -119,10 +123,10 @@ class MagellanMAGESpectrograph(spectrograph.Spectrograph):
             headers (list):
                 A list of headers read from a fits file
         """
-        expected_values = { '0.INSTRUME': 'MAGE',
+        expected_values = { '0.INSTRUME': 'MagE',
                             '1.NAXIS': 2,
-                            '1.NAXIS1': 1024,
-                            '1.NAXIS2': 2048 }
+                            '1.NAXIS1': 2176,
+                            '1.NAXIS2': 1152}
         super(MagellanMAGESpectrograph, self).check_headers(headers, expected_values=expected_values)
 
     def header_keys(self):
@@ -329,6 +333,131 @@ class MagellanMAGESpectrograph(spectrograph.Spectrograph):
         return np.full(5, 0.15)
 
 
+    def bpm(self, shape=None, filename=None, det=None, **null_kwargs):
+        """
+        Override parent bpm function with BPM specific to X-ShooterNIR.
+
+        .. todo::
+            Allow for binning changes.
+
+        Parameters
+        ----------
+        det : int, REQUIRED
+        **null_kwargs:
+            Captured and never used
+
+        Returns
+        -------
+        bpix : ndarray
+          0 = ok; 1 = Mask
+
+        """
+
+        self.empty_bpm(shape=shape, filename=filename, det=det)
+        return self.bpm_img
+
+    @staticmethod
+    def slit2order(islit):
+
+        """
+        Parameters
+        ----------
+        islit: int, float, or string, slit number
+
+        Returns
+        -------
+        order: int
+        """
+
+        if isinstance(islit,str):
+            islit = int(islit)
+        elif isinstance(islit,np.ndarray):
+            islit = islit.astype(int)
+        elif isinstance(islit,float):
+            islit = int(islit)
+        elif isinstance(islit, int):
+            pass
+        else:
+            msgs.error('Unrecognized type for islit')
+
+        orders = np.arange(26,10,-1, dtype=int)
+        return orders[islit]
+
+    @staticmethod
+    def order_platescale(self, binning = None):
+
+
+        """
+        Returns the plate scale in arcseconds for each order
+
+        Parameters
+        ----------
+        None
+
+        Optional Parameters
+        --------------------
+        binning: str
+
+        Returns
+        -------
+        order_platescale: ndarray, float
+
+        """
+
+        # NIR has no binning, but for an instrument with binning we would do this
+        #binspatial, binspectral = parse.parse_binning(binning)
+
+        # ToDO Either assume a linear trend or measure this
+        # X-shooter manual says, but gives no exact numbers per order.
+        # NIR: 52.4 pixels (0.210”/pix) at order 11 to 59.9 pixels (0.184”/pix) at order 26.
+
+        # Right now I just took the average
+        return np.full(16, 0.197)
+
+
+
+
+    @staticmethod
+    def slitmask(tslits_dict, pad=None, binning=None):
+        """
+         Generic routine ton construct a slitmask image from a tslits_dict. Children of this class can
+         overload this function to implement instrument specific slitmask behavior, for example setting
+         where the orders on an echelle spectrograph end
+
+         Parameters
+         -----------
+         tslits_dict: dict
+            Trace slits dictionary with slit boundary information
+
+         Optional Parameters
+         pad: int or float
+            Padding of the slit boundaries
+         binning: tuple
+            Spectrograph binning in spectral and spatial directions
+
+         Returns
+         -------
+         slitmask: ndarray int
+            Image with -1 where there are no slits/orders, and an integer where there are slits/order with the integer
+            indicating the slit number going from 0 to nslit-1 from left to right.
+
+         """
+
+        # These lines are always the same
+        pad = tslits_dict['pad'] if pad is None else pad
+        slitmask = pixels.slit_pixels(tslits_dict['lcen'], tslits_dict['rcen'], tslits_dict['nspat'], pad=pad)
+
+        spec_img = np.outer(np.arange(tslits_dict['nspec'], dtype=int), np.ones(tslits_dict['nspat'], dtype=int))  # spectral position everywhere along image
+
+        nslits = tslits_dict['lcen'].shape[1]
+        # These are the order boundaries determined by eye by JFH. 2025 is used as the maximum as the upper bit is not illuminated
+        order_max = [1476,1513,1551, 1592,1687,1741,1801, 1864,1935,2007, 2025, 2025,2025,2025,2025,2025]
+        order_min = [418 ,385 , 362,  334, 303, 268, 230,  187, 140,  85,   26,    0,   0,   0,   0,   0]
+        # TODO add binning adjustments to these
+        for islit in range(nslits):
+            orderbad = (slitmask == islit) & ((spec_img < order_min[islit]) | (spec_img > order_max[islit]))
+            slitmask[orderbad] = -1
+        return slitmask
 
 
 
