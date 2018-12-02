e83f12a9ac81c6ff1ef041451699a995e114016e
joe <joe@physics.ucsb.edu>
2018-12-02 12:51:41 -0800

    Getting tilts to work.

--- pypeit/core/extract.py
+++ pypeit/core/extract.py
@@ -1962,6 +1962,8 @@ def ech_objfind(image, ivar, ordermask, slit_left, slit_righ,inmask=None, order_
     :return: all objects found
     """
 
+    # TODO Update FOF algorithm here with the one from scikit-learn.
+
     if inmask is None:
         inmask = (ordermask > -1)
 
