ac02a510bc70d71569bed77b3ee6e4a2f1814175
Feige Wang <fgwang.astro@gmail.com>
2018-12-23 09:32:04 -0700

    move some routines from ech_coadd.py to load.py

--- pypeit/ech_coadd.py
+++ pypeit/ech_coadd.py
@@ -13,108 +13,6 @@ from linetools.spectra.utils import collate
 from linetools.spectra.xspectrum1d import XSpectrum1D
 
 ## ToDo: change it to a CLASS and modify coadd_1dspec.py
-def load_spec_order(fname,objid=None,order=None,extract='OPT',flux=True):
-    """Loading single order spectrum from a PypeIt 1D specctrum fits file.
-        it will be called by ech_load_spec
-    Parameters:
-        fname (str) : The file name of your spec1d file
-        objid (str) : The id of the object you want to load. (default is the first object)
-        order (int) : which order you want to load (default is None, loading all orders)
-        extract (str) : 'OPT' or 'BOX'
-        flux (bool) : default is True, loading fluxed spectra
-    returns:
-        spectrum_out : XSpectrum1D
-    """
-    if objid is None:
-        objid = 0
-    if order is None:
-        msgs.error('Please specify which order you want to load')
-
-    # read extension name into a list
-    primary_header = fits.getheader(fname, 0)
-    nspec = primary_header['NSPEC']
-    extnames = [primary_header['EXT0001']] * nspec
-    for kk in range(nspec):
-        extnames[kk] = primary_header['EXT' + '{0:04}'.format(kk + 1)]
-    extnameroot = extnames[0]
-
-    # Figure out which extension is the required data
-    ordername = '{0:04}'.format(order)
-    extname = extnameroot.replace('OBJ0000', objid)
-    extname = extname.replace('ORDER0000', 'ORDER' + ordername)
-    try:
-        exten = extnames.index(extname) + 1
-        msgs.info("Loading extension {:s} of spectrum {:s}".format(extname, fname))
-    except:
-        msgs.error("Spectrum {:s} does not contain {:s} extension".format(fname, extname))
-
-    spectrum = load.load_1dspec(fname, exten=exten, extract=extract, flux=flux)
-    # Polish a bit -- Deal with NAN, inf, and *very* large values that will exceed
-    #   the floating point precision of float32 for var which is sig**2 (i.e. 1e38)
-    bad_flux = np.any([np.isnan(spectrum.flux), np.isinf(spectrum.flux),
-                       np.abs(spectrum.flux) > 1e30,
-                       spectrum.sig ** 2 > 1e10,
-                       ], axis=0)
-    # Sometimes Echelle spectra have zero wavelength
-    bad_wave = spectrum.wavelength < 1000.0*units.AA
-    bad_all = bad_flux + bad_wave
-    ## trim bad part
-    wave_out,flux_out,sig_out = spectrum.wavelength[~bad_all],spectrum.flux[~bad_all],spectrum.sig[~bad_all]
-    spectrum_out = XSpectrum1D.from_tuple((wave_out,flux_out,sig_out), verbose=False)
-    #if np.sum(bad_flux):
-    #    msgs.warn("There are some bad flux values in this spectrum.  Will zero them out and mask them (not ideal)")
-    #    spectrum.data['flux'][spectrum.select][bad_flux] = 0.
-    #    spectrum.data['sig'][spectrum.select][bad_flux] = 0.
-
-    return spectrum_out
-
-def ech_load_spec(files,objid=None,order=None,extract='OPT',flux=True):
-    """Loading Echelle spectra from a list of PypeIt 1D spectrum fits files
-    Parameters:
-        files (str) : The list of file names of your spec1d file
-        objid (str) : The id (one per fits file) of the object you want to load. (default is the first object)
-        order (int) : which order you want to load (default is None, loading all orders)
-        extract (str) : 'OPT' or 'BOX'
-        flux (bool) : default is True, loading fluxed spectra
-    returns:
-        spectrum_out : XSpectrum1D
-    """
-
-    nfiles = len(files)
-    if objid is None:
-        objid = ['OBJ0000'] * nfiles
-    elif len(objid) == 1:
-        objid = objid * nfiles
-    elif len(objid) != nfiles:
-        msgs.error('The length of objid should be either 1 or equal to the number of spectra files.')
-
-    fname = files[0]
-    ext_final = fits.getheader(fname, -1)
-    norder = ext_final['ORDER'] + 1
-    msgs.info('spectrum {:s} has {:d} orders'.format(fname, norder))
-    if norder <= 1:
-        msgs.error('The number of orders have to be greater than one for echelle. Longslit data?')
-
-    # Load spectra
-    spectra_list = []
-    for ii, fname in enumerate(files):
-
-        if order is None:
-            msgs.info('Loading all orders into a gaint spectra')
-            for iord in range(norder):
-                spectrum = load_spec_order(fname,objid=objid[ii],order=iord,extract=extract,flux=flux)
-                # Append
-                spectra_list.append(spectrum)
-        elif order >= norder:
-            msgs.error('order number cannot greater than the total number of orders')
-        else:
-            spectrum = load_spec_order(fname, objid=objid[ii], order=order, extract=extract, flux=flux)
-            # Append
-            spectra_list.append(spectrum)
-    # Join into one XSpectrum1D object
-    spectra = collate(spectra_list)
-    # Return
-    return spectra
 
 def spec_from_array(wave,flux,sig,**kwargs):
     """
@@ -180,7 +78,7 @@ def ech_coadd(files,objids=None,extract='OPT',flux=True,giantcoadd=False,
 
     if giantcoadd:
         msgs.info('Coadding all orders and exposures at once')
-        spectra = ech_load_spec(files, objid=objids,order=None, extract=extract, flux=flux)
+        spectra = load.ech_load_spec(files, objid=objids,order=None, extract=extract, flux=flux)
         wave_grid = np.zeros((2,spectra.nspec))
         for i in range(spectra.nspec):
             wave_grid[0, i] = spectra[i].wvmin.value
@@ -203,7 +101,7 @@ def ech_coadd(files,objids=None,extract='OPT',flux=True,giantcoadd=False,
         rsp_kwargs['sig_tag'] = '{:s}_FLAM_SIG'.format(extract)
         wave_grid = np.zeros((2,norder))
         for iord in range(norder):
-            spectra = ech_load_spec(files, objid=objids, order=iord, extract=extract, flux=flux)
+            spectra = load.ech_load_spec(files, objid=objids, order=iord, extract=extract, flux=flux)
             ech_kwargs = {'echelle': False, 'wave_grid_min': spectra.wvmin.value, 'wave_grid_max': spectra.wvmax.value, 'v_pix': v_pix}
             wave_grid[0,iord] = spectra.wvmin.value
             wave_grid[1,iord] = spectra.wvmax.value
@@ -224,8 +122,7 @@ def ech_coadd(files,objids=None,extract='OPT',flux=True,giantcoadd=False,
         kwargs['echelle'] = True
         kwargs['wave_grid_min'] = np.min(wave_grid)
         kwargs['wave_grid_max'] = np.max(wave_grid)
-        # ToDo: Currently I'm not using the first order due to some problem. Need to add it back after fix the problem.
-        spec1d = coadd.coadd_spectra(spectra_coadd[1:], wave_grid_method=wave_grid_method, niter=niter,
+        spec1d = coadd.coadd_spectra(spectra_coadd, wave_grid_method=wave_grid_method, niter=niter,
                                           scale_method=scale_method, do_offset=do_offset, sigrej_final=sigrej_final,
                                           do_var_corr=do_var_corr, qafile=qafile, outfile=outfile,
                                           do_cr=do_cr, debug=debug, **kwargs)
