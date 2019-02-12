5e546ae77d6280d6adf0d134400821ed10075643
Feige Wang <fgwang.astro@gmail.com>
2019-02-12 10:31:15 -0800

    tunning fluxspec children class

--- pypeit/fluxspec.py
+++ pypeit/fluxspec.py
@@ -450,7 +450,8 @@ class MultiSlit(FluxSpec):
                                                self.std.boxcar['COUNTS_IVAR'],
                                                self.std_header['AIRMASS'],
                                                self.std_header['EXPTIME'],
-                                               self.spectrograph,
+                                               self.spectrograph.telescope['longitude'],
+                                               self.spectrograph.telescope['latitude'],
                                                BALM_MASK_WID=self.par['balm_mask_wid'],
                                                telluric=self.telluric,
                                                ra=self.std_ra,
@@ -535,9 +536,12 @@ class Echelle(FluxSpec):
             wavemask = std.boxcar['WAVE'] > 1000.0 * units.AA
             wave, counts, ivar = std.boxcar['WAVE'][wavemask], std.boxcar['COUNTS'][wavemask], \
                                  std.boxcar['COUNTS_IVAR'][wavemask]
-            sens_dict_iord = flux.generate_sensfunc(wave, counts, ivar, float(std_header['AIRMASS']),
-                                                    std_header['EXPTIME'],
-                                                    self.spectrograph, star_type=self.star_type,
+            sens_dict_iord = flux.generate_sensfunc(wave, counts, ivar,
+                                                    float(self.std_header['AIRMASS']),
+                                                    self.std_header['EXPTIME'],
+                                                    self.spectrograph.telescope['longitude'],
+                                                    self.spectrograph.telescope['latitude'],
+                                                    star_type=self.star_type,
                                                     star_mag=self.star_mag,
                                                     telluric=self.telluric, ra=self.std_ra, dec=self.std_dec,
                                                     resolution=self.resolution,
