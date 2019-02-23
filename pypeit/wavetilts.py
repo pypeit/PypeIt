4cdfeefe15fe63365a9e8df824d061e0ea097920
Feige Wang <fgwang.astro@gmail.com>
2019-02-22 17:21:51 -0800

    fix bug introduced by Kyle on tilts_dict

--- pypeit/wavetilts.py
+++ pypeit/wavetilts.py
@@ -350,7 +350,7 @@ class WaveTilts(masterframe.MasterFrame):
             spat_order = hdu[3].data
             spec_order = hdu[4].data
             tilts_dict = {'tilts':tilts, 'coeffs':coeffs, 'slitcen':slitcen,
-                          'func2d':hdu[1].header['FUNC2D'], 'nslit':hdu[1].header['NSLIT'],
+                          'func2d':hdu[0].header['FUNC2D'], 'nslit':hdu[0].header['NSLIT'],
                           'spat_order':spat_order, 'spec_order':spec_order}
             return tilts_dict, head0
 
