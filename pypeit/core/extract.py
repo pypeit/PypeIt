6de37efbb322ae6aadbf2cd96ac9e0af033b5382
joe <joe@physics.ucsb.edu>
2019-02-24 22:22:16 -0800

    Fixing tests \.

--- pypeit/core/extract.py
+++ pypeit/core/extract.py
@@ -583,6 +583,35 @@ def qa_fit_profile(x_tot,y_tot, model_tot, l_limit = None, r_limit = None, ind =
 def return_gaussian(sigma_x, norm_obj, fwhm, med_sn2, obj_string, show_profile,
                     ind = None, l_limit = None, r_limit=None, xlim = None, xtrunc = 1e6):
 
+    """
+    Utility function to return Gaussian object profile in the case of low S/N ratio or too many rejected pixels.
+    Args:
+        sigma_x: ndarray (nspec, nspat)
+            Spatial of gaussian
+        norm_obj: ndarray (nspec, nspat)
+            Normalized 2-d spectrum.
+        fwhm: float
+            FWHM parameter for Gaussian profile
+        med_sn2: float
+            Median (S/N)^2 used only for QA
+        obj_string: str
+            String identifying object. Used only for QA.
+        show_profile: bool
+            Is set, qa plot will be shown to screen
+        ind: array, int
+            Good indices in object profile. Used by QA routine.
+        l_limit: float
+            Left limit of profile fit where derivative is evaluated for Gaussian apodization. Used by QA routine.
+        r_limit: float
+            Right limit of profile fit where derivative is evaluated for Gaussian apodization. Used by QA routine.
+        xlim: float
+            Spatial location to trim object profile for plotting in QA routine.
+        xtrunc: float
+            Spatial nsigma to truncate object profile for plotting in QA routine. 
+    Returns:
+
+    """
+
     profile_model = np.exp(-0.5*sigma_x**2)/np.sqrt(2.0 * np.pi)*(sigma_x ** 2 < 25.)
     info_string = "FWHM=" + "{:6.2f}".format(fwhm) + ", S/N=" + "{:8.3f}".format(np.sqrt(med_sn2))
     title_string = obj_string + ', ' + info_string
