b58a91eaeae2130684d34787d02725dfbb680b9a
joe <joe@physics.ucsb.edu>
2019-03-13 14:12:53 -0700

    added dependency to IPython

--- pypeit/masterframe.py
+++ pypeit/masterframe.py
@@ -232,7 +232,16 @@ class MasterFrame(object):
             data = hdu[_ext[0]].data.astype(np.float)
             return (data, hdu[0].header) if return_header else data
         # Multiple extensions
-        data = tuple([ hdu[k].data.astype(np.float) for k in _ext ])
+        data = ()
+        for k in _ext:
+            try:
+                data += (hdu[k].data.astype(np.float),)
+            except AttributeError:
+                data += (None,)
+
+        # JFH The above case deals with cases where a nUll image is stored to some extension, i.e. illumflats
+        # which were crashing the list comprehension below. 
+        #data = tuple([ hdu[k].data.astype(np.float) for k in _ext ])
         return data + (hdu[0].header,) if return_header else data
 
     @staticmethod
