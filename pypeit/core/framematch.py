039524ba5e08511d365ca8b80af54ed92772c611
joe <joe@physics.ucsb.edu>
2019-03-21 22:45:49 -0700

    improved logic for bg_id frame matching to allow images to be used as the background more than once

--- pypeit/core/framematch.py
+++ pypeit/core/framematch.py
@@ -87,17 +87,11 @@ def check_frame_exptime(exptime, exprng):
         return np.ones(len(exptime), dtype=bool)
     if len(exprng) != 2:
         raise ValueError('exprng must have two elements.')
-
-    # JFH This modification below is to prevent None values from corrupt/empty files from crashing this routine.
-    is_not_none = exptime != None
-    indx = is_not_none.copy()
-    if exprng[0] is not None:
-        indx[is_not_none] = exptime[is_not_none] > exprng[0]
-
-    #indx = np.ones(len(exptime), dtype=bool) if exprng[0] is None else exptime > exprng[0]
+    # Original code below
+    indx = np.ones(len(exptime), dtype=bool) if exprng[0] is None \
+                else exptime > exprng[0]
     if exprng[1] is not None:
-        indx[is_not_none] = exptime[is_not_none] < exprng[1]
-        #indx &= (exptime < exprng[1])
+        indx &= (exptime < exprng[1])
 
     return indx
 
