fe368b2b7baddff8da8d6d78ac9642e5b1d70e17
profxj <xavier@ucolick.org>
2019-04-13 10:29:23 -0700

    turn em on + Notebook

--- pypeit/spectrographs/shane_kast.py
+++ pypeit/spectrographs/shane_kast.py
@@ -229,8 +229,10 @@ class ShaneKastBlueSpectrograph(ShaneKastSpectrograph):
         # Wavelength calibrations
         if self.get_meta_value(scifile, 'dispname') == '600/4310':
             par['calibrations']['wavelengths']['reid_arxiv'] = 'shane_kast_blue_600.fits'
-        elif self.get_meta_value(scifile, 'dispname') == '452/3306':
+        elif self.get_meta_value(scifile, 'dispname') == '452/3306':  # NOT YET TESTED
             par['calibrations']['wavelengths']['reid_arxiv'] = 'shane_kast_blue_452.fits'
+        elif self.get_meta_value(scifile, 'dispname') == '830/3460':  # NOT YET TESTED
+            par['calibrations']['wavelengths']['reid_arxiv'] = 'shane_kast_blue_830.fits'
         else:
             msgs.error("NEED TO ADD YOUR GRISM HERE!")
         # Return
