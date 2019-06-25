801e66d92228b47d7b3869004690e8ed7124a401
Feige Wang <fgwang.astro@gmail.com>
2019-06-24 17:33:16 -0700

    clean coadd1d

--- pypeit/core/coadd1d.py
+++ pypeit/core/coadd1d.py
@@ -1407,12 +1407,6 @@ def combspec(wave_grid, waves, fluxes, ivars, masks, ref_percentile=30.0, maxite
 
     # Compute an initial stack as the reference, this has its own wave grid based on the weighted averages
     wave_stack, flux_stack, ivar_stack, mask_stack, nused = compute_stack(wave_grid, waves, fluxes, ivars, masks, weights)
-    # TODO: change to astropy sigma_clip median/mean for the initial guess
-    #flux_grid, ivar_grid, mask_grid = interp_spec(wave_grid, waves, fluxes, ivars, masks)
-    #wave_stack = wave_grid.copy()
-    #flux_stack = np.nanmedian(flux_grid,axis=1)
-    #ivar_stack = np.nanmedian(ivar_grid,axis=1) # TODO: this is incorrect!
-    #mask_stack = np.isfinite(flux_stack) & np.isfinite(ivar_stack) & (ivar_stack>0.0)
 
     # Interpolate the stack onto each individual exposures native wavelength grid
     flux_stack_nat, ivar_stack_nat, mask_stack_nat = interp_spec(waves, wave_stack, flux_stack, ivar_stack, mask_stack)
