8f5c4032bf2ff644a9721305e023fe3634f89ec2
joe <joe@physics.ucsb.edu>
2019-07-01 14:20:53 -1000

    minor fix to debugging.

--- pypeit/reduce.py
+++ pypeit/reduce.py
@@ -599,6 +599,7 @@ class MultiSlit(Reduce):
                                 specobj_dict=specobj_dict, show_peaks=show_peaks,
                                 show_fits=show_fits, show_trace=show_trace,
                                 trim_edg=self.redux_par['find_trim_edge'],
+                                cont_fit=self.redux_par['find_cont_fit'],
                                 qa_title=qa_title, nperslit=self.redux_par['maxnumber'])
             sobjs.add_sobj(sobjs_slit)
 
@@ -726,7 +727,7 @@ class Echelle(Reduce):
             inmask=inmask, ncoeff=self.redux_par['trace_npoly'], order_vec=order_vec,
             hand_extract_dict=manual_extract_dict, plate_scale=plate_scale, std_trace=std_trace,
             specobj_dict=specobj_dict,sig_thresh=sig_thresh, show_peaks=show_peaks, show_fits=show_fits,
-            trim_edg=self.redux_par['find_trim_edge'], show_trace=show_trace, debug=debug)
+            trim_edg=self.redux_par['find_trim_edge'], cont_fit=self.redux_par['find_cont_fit'], show_trace=show_trace, debug=debug)
 
         # Steps
         self.steps.append(inspect.stack()[0][3])
