9542e8976673d70b18004f96cc3b58d8b3f49bd1
profxj <xavier@ucolick.org>
2019-07-24 13:12:30 -0700

    fussing..

--- pypeit/spectrographs/gemini_gmos.py
+++ pypeit/spectrographs/gemini_gmos.py
@@ -153,7 +153,7 @@ class GeminiGMOSSpectrograph(spectrograph.Spectrograph):
 
     def load_raw_frame(self, raw_file, det=None, **null_kwargs):
         """
-        Wrapper to the raw image reader for LRIS
+        Wrapper to the raw image reader for GMOS
 
         Args:
             raw_file:  str, filename
@@ -172,7 +172,7 @@ class GeminiGMOSSpectrograph(spectrograph.Spectrograph):
 
         return raw_img, hdu
 
-    def get_image_shape(self, filename=None, det=None, **null_kwargs):
+    def get_raw_image_shape(self, filename=None, det=None, **null_kwargs):
         """
         Overrides :class:`Spectrograph.get_image_shape` for LRIS images.
 
@@ -180,14 +180,15 @@ class GeminiGMOSSpectrograph(spectrograph.Spectrograph):
         """
         # Cannot be determined without file
         if filename is None:
-            raise ValueError('Must provide a file to determine the shape of an LRIS image.')
+            raise ValueError('Must provide a file to determine the shape of a GMOS image.')
 
         # Use a file
         self._check_detector()
-        self.naxis = (self.load_raw_frame(filename, det=det)[0]).shape
+        shape, datasec, oscansec, _ = gmos_image_sections(filename, det)
+        self.naxis = shape
         return self.naxis
 
-    def get_image_section(self, inp=None, det=1, section='datasec'):
+    def get_image_section(self, inp, det, section='datasec'):
         """
         Return a string representation of a slice defining a section of
         the detector image.
@@ -229,16 +230,15 @@ class GeminiGMOSSpectrograph(spectrograph.Spectrograph):
         """
         # Read the file
         if inp is None:
-            msgs.error('Must provide Gemini GMOS file to get image section.')
-        elif not os.path.isfile(inp):
-            msgs.error('File {0} does not exist!'.format(inp))
-        temp, head0, secs = read_gmos(inp, det=det)
+            msgs.error('Must provide Gemini GMOS file or hdulist to get image section.')
+        # Load it up
+        shape, datasec, oscansec, _ = gmos_image_sections(inp, det)
         if section == 'datasec':
-            return secs[0], False, False
+            return datasec, False, False
         elif section == 'oscansec':
             # Need to flip these
             # TODO: What does the above mean?
-            return secs[1], False, False
+            return oscansec, False, False
         else:
             raise ValueError('Unrecognized keyword: {0}'.format(section))
 
@@ -592,7 +592,78 @@ class GeminiGMOSNE2VSpectrograph(GeminiGMOSNSpectrograph):
         #
         return par
 
-# TODO: Put this inside the class or abstract it.
+def gmos_image_sections(inp, det):
+    if isinstance(inp, str):
+        hdu = fits.open(inp)
+    else:
+        hdu = inp
+    head0 = hdu[0].header
+    head1 = hdu[1].header
+
+    # Number of amplifiers (could pull from DetectorPar but this avoids needing the spectrograph, e.g. view_fits)
+    numamp = (len(hdu)-1)//3
+
+    # Setup for datasec, oscansec
+    dsec = []
+    osec = []
+
+    # get the x and y binning factors...
+    binning = head1['CCDSUM']
+    xbin, ybin = [int(ibin) for ibin in binning.split(' ')]
+
+    # First read over the header info to determine the size of the output array...
+    datasec = head1['DATASEC']
+    x1, x2, y1, y2 = np.array(parse.load_sections(datasec, fmt_iraf=False)).flatten()
+    biassec = head1['BIASSEC']
+    b1, b2, b3, b4 = np.array(parse.load_sections(biassec, fmt_iraf=False)).flatten()
+    nxb = b2-b1 + 1
+
+    # determine the output array size...
+    nx = (x2-x1+1)*numamp + nxb*numamp
+    ny = y2-y1+1
+
+    if numamp == 2:   # E2V
+        if det == 1: # BLUEST DETECTOR
+            order = range(6,4,-1)
+        elif det == 2: # NEXT
+            order = range(3,5)
+        elif det == 3: # REDDEST DETECTOR
+            order = range(1,3)
+    elif numamp == 4:  # Hamamatsu
+        if det == 1: # BLUEST DETECTOR
+            order = range(12,8,-1)
+        elif det == 2: # BLUEST DETECTOR
+            order = range(8,4,-1)
+        elif det == 3: # BLUEST DETECTOR
+            order = range(4,0,-1)
+    else:
+        embed()
+
+    # Do it
+    shape = (nx, ny)
+    for kk, jj in enumerate(order):
+        header = hdu[jj].header
+        # datasec
+        datasec = header['DATASEC']
+        xdata1, xdata2, ydata1, ydata2 = np.array(parse.load_sections(datasec, fmt_iraf=False)).flatten()
+        inx = xdata2-xdata1+1
+        xs = inx*kk
+        xe = xs + inx
+        section = '[{:d}:{:d},:]'.format(xs*xbin, xe*xbin)
+        dsec.append(section)
+        # oscansec
+        xs = nx - numamp*nxb + kk*nxb
+        xe = xs + nxb
+        osection = '[{:d}:{:d},:]'.format(xs*xbin, xe*xbin)
+        osec.append(osection)
+
+    # Pack up extras
+    ext_items = hdu, order, xbin, numamp, nxb, nx
+
+    # Return
+    return shape, dsec, osec, ext_items
+
+
 def read_gmos(raw_file, det=1):
     """
     Read the GMOS data file
@@ -614,7 +685,7 @@ def read_gmos(raw_file, det=1):
     sections : list
       List of datasec, oscansec, ampsec sections
     """
-
+    '''
     # Check for file; allow for extra .gz, etc. suffix
     fil = glob.glob(raw_file+'*') 
     if len(fil) != 1:
@@ -667,14 +738,21 @@ def read_gmos(raw_file, det=1):
             order = range(4,0,-1)
     else:
         embed()
+    '''
+    # Parse
+    shape, dsec2, osec2, ext_items = gmos_image_sections(raw_file, det)
+    # Nupack
+    hdu, order, xbin, numamp, nxb, nx = ext_items
 
+    # Init
+    array = np.zeros(shape)
+
+    dsec, osec = [], []  # TESTING
     # insert extensions into master image...
     for kk, jj in enumerate(order):
 
         # grab complete extension...
         data, overscan, datasec, biassec, x1, x2 = gemini_read_amp(hdu, jj)
-                            #, linebias=linebias, nobias=nobias, $
-                            #x1=x1, x2=x2, y1=y1, y2=y2, gaindata=gaindata)
         # insert components into output array...
         inx = data.shape[0]
         xs = inx*kk
@@ -682,7 +760,6 @@ def read_gmos(raw_file, det=1):
 
         # insert data...
         # Data section
-        #section = '[:,{:d}:{:d}]'.format(xs, xe)  # Eliminate lines
         section = '[{:d}:{:d},:]'.format(xs*xbin, xe*xbin)  # Eliminate lines
         dsec.append(section)
         array[xs:xe, :] = np.flipud(data)
@@ -691,18 +768,12 @@ def read_gmos(raw_file, det=1):
         xs = nx - numamp*nxb + kk*nxb
         xe = xs + nxb
 
-        #section = '[:,{:d}:{:d}]'.format(xs, xe)
         osection = '[{:d}:{:d},:]'.format(xs*xbin, xe*xbin)  # TRANSPOSED FOR WHAT COMES
         osec.append(osection)
         array[xs:xe, :] = overscan
 
-    # make sure BZERO is a valid integer for IRAF
-    obzero = head1['BZERO']
-    #head0['O_BZERO'] = obzero
-    head0['BZERO'] = 32768-obzero
-
+    embed(header='774 of gmos')
     # Return, transposing array back to goofy Python indexing
-
     return array, hdu, (dsec, osec)
 
 
@@ -738,12 +809,6 @@ def gemini_read_amp(inp, ext):
         hdu = fits.open(inp)
     else:
         hdu = inp
-
-    head1 = hdu[1].header
-
-    # Deal with binning
-    binning = head1['CCDSUM']
-    xbin, ybin = [int(ibin) for ibin in binning.split(' ')]
 
     # get entire extension...
     temp = hdu[ext].data.transpose()
