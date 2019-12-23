3123899041fb76fed6d04885e10f7030c0f63730
Kyle Westfall <kbwestfall@gmail.com>
2019-12-23 11:44:06 -0800

    Bump version: 0.11.1dev → 0.12.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def get_requirements():
 
 NAME = 'pypeit'
 # do not use x.x.x-dev.  things complain.  instead use x.x.xdev
-VERSION = '0.11.1dev'
+VERSION = '0.12.0'
 RELEASE = 'dev' not in VERSION
 
 def run_setup(data_files, scripts, packages, install_requires):
