f3c9dcd03c25a6170814a9e3dfc6be2e6ca8cbc5
profxj <xavier@ucolick.org>
2019-12-30 09:06:44 -0800

    box

--- pypeit/scripts/ql_mos.py
+++ pypeit/scripts/ql_mos.py
@@ -17,13 +17,14 @@ def parser(options=None):
     parser = argparse.ArgumentParser(description='Script to run PypeIt in QuickLook on a set of MOS files')
     parser.add_argument('spectrograph', type=str, help='Name of spectograph, e.g. shane_kast_blue')
     parser.add_argument('full_rawpath', type=str, help='Full path to the raw files')
-    parser.add_argument('arc', type=str, help='Arc frame')
-    parser.add_argument('flat', type=str, help='Flat frame')
-    parser.add_argument('science', type=str, help='Science frame')
+    parser.add_argument('arc', type=str, help='Arc frame filename')
+    parser.add_argument('flat', type=str, help='Flat frame filename')
+    parser.add_argument('science', type=str, help='Science frame filename')
     parser.add_argument('-b', '--box_radius', type=float, help='Set the radius for the boxcar extraction (arcsec)')
     parser.add_argument('-d', '--det', type=int, default=1, help='Detector number')
     parser.add_argument("--ignore_headers", default=False, action="store_true",
                         help="Ignore bad headers?")
+    parser.add_argument("--user_pixflat", type=str, help="Use a user-supplied pixel flat (e.g. keck_lris_blue)")
 
     if options is None:
         pargs = parser.parse_args()
@@ -56,6 +57,9 @@ def main(pargs):
     cfg_lines += ['    [[scienceframe]]']
     cfg_lines += ['        [[process]]']
     cfg_lines += ['              cr_reject = False']
+    if pargs.user_pixflat is not None:
+        cfg_lines += ['    [[flatfield]]']
+        cfg_lines += ['        frame = {0}'.format(pargs.user_pixflat)]
     cfg_lines += ['[scienceimage]']
     cfg_lines += ['    [[extraction]]']
     cfg_lines += ['         boxcar_only = True']
@@ -64,17 +68,24 @@ def main(pargs):
     cfg_lines += ['    [[findobj]]']
     cfg_lines += ['         skip_second_find = True']
 
-    # Setup
+    # Data files
     data_files = [os.path.join(pargs.full_rawpath, pargs.arc),
-                  os.path.join(pargs.full_rawpath,pargs.flat),
+                  os.path.join(pargs.full_rawpath, pargs.flat),
                   os.path.join(pargs.full_rawpath,pargs.science)]
-    ps = pypeitsetup.PypeItSetup(data_files, path='./', spectrograph_name=spec,
+    # Damn JFH sorting
+    asrt = np.argsort(np.array(data_files))
+
+    # Setup
+    ps = pypeitsetup.PypeItSetup(np.array(data_files)[asrt].tolist(), path='./', spectrograph_name=spec,
                                  cfg_lines=cfg_lines)
     ps.build_fitstbl()
     # TODO -- Get the type_bits from  'science'
     bm = framematch.FrameTypeBitMask()
     bits = [bm.bits[iftype] for iftype in ['arc', 'pixelflat', 'trace', 'science', 'tilt']]
-    ps.fitstbl.set_frame_types(np.array([2**bits[0]+2**bits[4], 2**bits[1] + 2**bits[2], 2**bits[3]]))  # 1=arc, 16=pixelflat, 32=science, trace=128
+    bits_flat = 2 ** bits[1] + 2 ** bits[2] if pargs.user_pixflat is None else 2**bits[2]
+    file_bits = np.array([2 ** bits[0] + 2 ** bits[4], bits_flat,
+              2 ** bits[3]])  # 1=arc, 16=pixelflat, 32=science, trace=128
+    ps.fitstbl.set_frame_types(file_bits[asrt])
     ps.fitstbl.set_combination_groups()
     # Extras
     ps.fitstbl['setup'] = 'A'
