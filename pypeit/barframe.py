686538b9cf62c2fd28336d1e96645a94af14968a
rcooke-ast <rcooke.ast@gmail.com>
2020-02-09 17:59:37 +0000

    bar made

--- pypeit/barframe.py
+++ pypeit/barframe.py
@@ -13,6 +13,7 @@ from pypeit import masterframe
 from pypeit.par import pypeitpar
 from pypeit.images import calibrationimage
 from pypeit.images import pypeitimage
+from pypeit.core import procimg
 
 
 class BarFrame(calibrationimage.CalibrationImage, masterframe.MasterFrame):
@@ -80,7 +81,10 @@ class BarFrame(calibrationimage.CalibrationImage, masterframe.MasterFrame):
 
     # Keep order same as processimages (or else!)
     def __init__(self, spectrograph, files=None, det=1, par=None, master_key=None,
-                 master_dir=None, reuse_masters=False):
+                 master_dir=None, reuse_masters=False, msbias=None):
+
+        # Parameters unique to this Object
+        self.msbias = msbias
 
         # Parameters
         self.par = pypeitpar.FrameGroupPar(self.frametype) if par is None else par
@@ -92,40 +96,15 @@ class BarFrame(calibrationimage.CalibrationImage, masterframe.MasterFrame):
         # spectrograph even though it really only needs the string name
         masterframe.MasterFrame.__init__(self, self.master_type, master_dir=master_dir,
                                          master_key=master_key, reuse_masters=reuse_masters)
-
-        # Processing steps
-        self.process_steps = []
-        if self.par['process']['overscan'].lower() != 'none':
-            self.process_steps.append('subtract_overscan')
+        # Process steps
+        self.process_steps = procimg.init_process_steps(self.msbias, self.par['process'])
         self.process_steps += ['trim']
         self.process_steps += ['orient']
-
-    def build_image(self, overwrite=False, trim=True):
-        """
-        Grab the bar files (as needed) and then process the input bar
-        frames with :func:`pypeit.processimages.ProcessImages.process`.
-
-        Args:
-            overwrite: (:obj: `bool`, optional):
-                Regenerate the combined image
-            trim (:obj:`bool`, optional):
-                If True, trim the image
-
-        Returns:
-            `numpy.ndarray`_: Combined, processed image.
-        """
-        if self.nfiles == 0:
-            msgs.info("No bar frames provided.  No bar image will be generated or used")
-            return None
-        # Build
-        self.pypeItImage = super(BarFrame, self).build_image(ignore_saturation=True)
-        self.pypeitImage.ivar = None  # Zero this out as it non-sensical
-        # Return
-        return self.pypeItImage
+        self.process_steps += ['apply_gain']
 
     def save(self, outfile=None, overwrite=True):
         """
-        Save the bar master data.
+        Save the master bar data.
 
         Args:
             outfile (:obj:`str`, optional):
@@ -134,45 +113,36 @@ class BarFrame(calibrationimage.CalibrationImage, masterframe.MasterFrame):
             overwrite (:obj:`bool`, optional):
                 Overwrite any existing file.
         """
-        # Some checks
-        if self.pypeitImage is None:
-            msgs.warn('No MasterBar to save!')
-            return
-        if not self.pypeitImage.validate():
-            msgs.warn('MasterBar is not a proper image.')
-            return
-        # Proceed
         _outfile = self.master_file_path if outfile is None else outfile
         # Check if it exists
         if os.path.exists(_outfile) and not overwrite:
             msgs.warn('Master file exists: {0}'.format(_outfile) + msgs.newline()
                       + 'Set overwrite=True to overwrite it.')
             return
-        # Save
+        #
         hdr = self.build_master_header(steps=self.process_steps, raw_files=self.file_list)
         self.pypeitImage.write(_outfile, hdr=hdr, iext='BAR')
         msgs.info('Master frame written to {0}'.format(_outfile))
-        #super(BarFrame, self).save(self.pypeitImage, 'BAR', outfile=outfile, overwrite=overwrite,
-        #                            raw_files=self.file_list, steps=self.process_steps)
 
     def load(self, ifile=None):
         """
-        Load the bar frame according to how par['useframe'] is set.
+        Load the bar frame data from a saved master frame.
 
         Args:
             ifile (:obj:`str`, optional):
                 Name of the master frame file.  Defaults to
                 :attr:`file_path`.
+            return_header (:obj:`bool`, optional):
+                Return the header
 
         Returns:
-            Returns either the `numpy.ndarray`_ with the bar image
-            or None if no bar is to be subtracted.
+            Returns a `numpy.ndarray`_ with the bar master frame image.
+            Also returns the primary header, if requested.
         """
         # Check on whether to reuse and whether the file exists
         master_file = self.chk_load_master(ifile)
         if master_file is None:
             return
-        else:  # Load
-            self.pypeitImage = pypeitimage.PypeItImage.from_file(master_file)
-            return self.pypeitImage
-        #return super(BarFrame, self).load('BAR', ifile=ifile, is_pypeitImage=True)
+        # Load it up
+        self.pypeitImage = pypeitimage.PypeItImage.from_file(master_file)
+        return self.pypeitImage
