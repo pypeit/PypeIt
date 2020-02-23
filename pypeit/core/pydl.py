5cc884edb0ae98f4aeed76ff9fbd0b4a100c87b1
rcooke-ast <rcooke.ast@gmail.com>
2020-02-23 21:04:07 +0000

    update pydl

--- pypeit/core/pydl.py
+++ pypeit/core/pydl.py
@@ -10,6 +10,9 @@ from pypeit import utils
 from IPython import embed
 import copy
 
+from numpy.linalg.linalg import LinAlgError
+from scipy.linalg import cholesky_banded, cho_solve_banded
+
 """This module corresponds to the image directory in idlutils.
 """
 
@@ -825,7 +828,6 @@ class bspline(object):
         return (0, yfit)
 
 
-
 def cholesky_band(l, mininf=0.0):
     """Compute Cholesky decomposition of banded matrix.
 
@@ -850,6 +852,7 @@ def cholesky_band(l, mininf=0.0):
     bw, nn = lower.shape
     n = nn - bw
     negative = lower[0, 0:n] <= mininf
+
     # JFH changed this below to make it more consistent with IDL version. Not sure
     # why the np.all(np.isfinite(lower)) was added. The code could return an empty
     # list for negative.nonzero() and crash if all elements in lower are NaN.
@@ -862,52 +865,54 @@ def cholesky_band(l, mininf=0.0):
 #        msgs.warn('Found {:d}'.format(len(negative.nonzero()[0])) +
 #                  ' bad entries: ' + str(negative.nonzero()[0]))
 #        return (negative.nonzero()[0], l)
-    kn = bw - 1
-    spot = np.arange(kn, dtype='i4') + 1
-    bi = np.arange(kn, dtype='i4')
-    for i in range(1, kn):
-        bi = np.append(bi, np.arange(kn-i, dtype='i4') + (kn+1)*i)
-    for j in range(n):
-        lower[0, j] = np.sqrt(lower[0, j])
-        lower[spot, j] /= lower[0, j]
-        x = lower[spot, j]
-        if not np.all(np.isfinite(x)):
-            msgs.warn('NaN found in cholesky_band.')
-            return (j, l)
-        hmm = np.outer(x, x)
-        here = bi+(j+1)*bw
-        lower.T.flat[here] -= hmm.flat[bi]
-    return (-1, lower)
+    try:
+        lower = cholesky_banded(l[:, 0:n], lower=True)
+    except LinAlgError:
+        #
+        # Figure out where the error is.
+        #
+        lower = l.copy()
+        kn = bw - 1
+        spot = np.arange(kn, dtype='i4') + 1
+        for j in range(n):
+            lower[0, j] = np.sqrt(lower[0, j])
+            lower[spot, j] /= lower[0, j]
+            x = lower[spot, j]
+            if not np.all(np.isfinite(x)):
+                msgs.warn('NaN found in cholesky_band.')
+                return (j, l)
+        #
+        # Restore padding.
+        #
+    L = np.zeros(l.shape, dtype=l.dtype)
+    L[:, 0:n] = lower[:, 0:n]
+    return (-1, L)
 
 
 def cholesky_solve(a, bb):
-    """Solve the equation Ax=b where A is a Cholesky-banded matrix.
+    """Solve the equation :math:`A x = b` where `a` is a *lower*
+    Cholesky-banded matrix.
 
-    Parameters
-    ----------
-    a : :class:`numpy.ndarray`
-        :math:`A` in :math:`A x = b`.
-    bb : :class:`numpy.ndarray`
-        :math:`b` in :math:`A x = b`.
+    In the :class:`~pydl.pydlutils.bspline.bspline` machinery, `a` needs to
+    be padded.  This function should only used with the output of
+    :func:`~pydl.pydlutils.bspline.cholesky_band`, to ensure the proper
+    padding on `a`.  Otherwise the computation is delegated to
+    :func:`scipy.linalg.cho_solve_banded`.
 
-    Returns
-    -------
-    :func:`tuple`
-        A tuple containing the status and the result of the solution.  The
-        status is always -1.
+    Args:
+        a : :class:`numpy.ndarray`
+            *Lower* Cholesky decomposition of :math:`A` in :math:`A x = b`.
+        bb : :class:`numpy.ndarray`
+            :math:`b` in :math:`A x = b`.
+
+    Returns:
+        :class:`numpy.ndarray` : The solution, padded to be the same shape as `bb`.
     """
-    b = bb.copy()
     bw = a.shape[0]
-    n = b.shape[0] - bw
-    kn = bw - 1
-    spot = np.arange(kn, dtype='i4') + 1
-    for j in range(n):
-        b[j] /= a[0, j]
-        b[j+spot] -= b[j]*a[spot, j]
-    spot = kn - np.arange(kn, dtype='i4')
-    for j in range(n-1, -1, -1):
-        b[j] = (b[j] - np.sum(a[spot, j] * b[j+spot]))/a[0, j]
-    return (-1, b)
+    n = bb.shape[0] - bw
+    x = np.zeros(bb.shape, dtype=bb.dtype)
+    x[0:n] = cho_solve_banded((a[:, 0:n], True), bb[0:n])
+    return x
 
 
 def iterfit(xdata, ydata, invvar=None, inmask = None, upper=5, lower=5, x2=None,
