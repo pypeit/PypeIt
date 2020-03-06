6645962e1f92cb821c2407b4a90eca3eed6fdea5
profxj <xavier@ucolick.org>
2020-03-06 08:12:36 -0800

    fixed

--- pypeit/io.py
+++ pypeit/io.py
@@ -90,10 +90,7 @@ def rec_to_fits_type(col_element, single_row=False):
         return '{0}E'.format(n)
 
     # If it makes it here, assume its a string
-    try:
-        l = int(col_element.dtype.str[col_element.dtype.str.find('U')+1:])
-    except:
-        embed(header='93 of io.py')
+    l = int(col_element.dtype.str[col_element.dtype.str.find('U')+1:])
 #    return '{0}A'.format(l) if n==1 else '{0}A{1}'.format(l*n,l)
     return '{0}A'.format(l*n)
 
