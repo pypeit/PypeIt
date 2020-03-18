83098a0cea6cfd3fa3fede4213fdb1b96e571a78
profxj <xavier@ucolick.org>
2020-03-18 15:41:23 -0700

    bias test fix

--- pypeit/images/combineimage.py
+++ pypeit/images/combineimage.py
@@ -173,6 +173,7 @@ class CombineImage(object):
             for ff, file in enumerate(self.files):
                 print(msgs.indent() + strout.format(os.path.split(file)[1], " ".join(lampstat[ff].split("_"))))
             print(msgs.indent() + '-'*maxlen + "  " + '-'*maxlmp)
+            embed(header='')
             msgs.error("Unable to combine frames with different lamp status")
 
         # Coadd them
--- pypeit/tests/test_biasframe.py
+++ pypeit/tests/test_biasframe.py
@@ -30,8 +30,11 @@ def deimos_flat_files():
 @pytest.fixture
 @dev_suite_required
 def kast_blue_bias_files():
-    kast_blue_bias_files = glob.glob(os.path.join(os.getenv('PYPEIT_DEV'), 'RAW_DATA',
+    kast_blue_files = glob.glob(os.path.join(os.getenv('PYPEIT_DEV'), 'RAW_DATA',
                                                   'shane_kast_blue', '600_4310_d55', 'b1?.fits*'))
+    kast_blue_files.sort()
+    # Trim to bias
+    kast_blue_bias_files = kast_blue_files[5:]
     return kast_blue_bias_files
 
 @dev_suite_required
@@ -41,7 +44,7 @@ def test_instantiate(kast_blue_bias_files):
     assert bias_frame0.nfiles == 0
     #
     bias_frame1 = biasframe.BiasFrame(shane_kast_blue, files=kast_blue_bias_files)
-    assert bias_frame1.nfiles == 10
+    assert bias_frame1.nfiles == 5
 
 @dev_suite_required
 def test_process(kast_blue_bias_files):
