ef0fa7a4a57674235aa190cbe67e88515f0a0c92
Kyle Westfall <westfall@ucolick.org>
2020-04-03 09:53:18 -0700

    doc fixes

--- pypeit/core/trace.py
+++ pypeit/core/trace.py
@@ -832,6 +832,7 @@ def fit_trace(flux, trace_cen, order, ivar=None, bpm=None, trace_bpm=None, weigh
     Iteratively fit the trace of a feature in the provided image.
 
     Each iteration performs two steps:
+
         - Remeasure the trace centroid using :func:`masked_centroid`.
           The size of the integration window (see the definition of
           the `width` parameter for
@@ -923,13 +924,13 @@ def fit_trace(flux, trace_cen, order, ivar=None, bpm=None, trace_bpm=None, weigh
         xmax (:obj:`float`, optional):
             Upper reference for robust_polyfit polynomial fitting.
             Default is to use the image size in nspec direction
-        flavor (:obj:`str:, optional):
-            Defines the type of fit performed.
-            Only used by QA
+        flavor (:obj:`str`, optional):
+            Defines the type of fit performed. Only used by QA
 
     Returns:
-        tuple: Returns four `numpy.ndarray`_ objects all with the same
-        shape as the input positions (`trace_cen`) and provide:
+        :obj:`tuple`: Returns four `numpy.ndarray`_ objects all with
+        the same shape as the input positions (`trace_cen`) and
+        provide:
 
             - The best-fitting positions of each trace determined by the
               polynomial fit.
@@ -937,7 +938,7 @@ def fit_trace(flux, trace_cen, order, ivar=None, bpm=None, trace_bpm=None, weigh
               Gaussian-weighting, to which the polynomial is fit.
             - The errors in the centroids.
             - Boolean flags for each centroid measurement (see
-              :func:`pypeit.core.moment.moment1d`).
+              :func:`~pypeit.core.moment.moment1d`).
 
     """
     # Ensure setup is correct
