ab5d237f17e7a19d8cc91ae1eb0188e2be9089f7
profxj <xavier@ucolick.org>
2020-04-18 07:04:01 -0700

    rm try/except

--- pypeit/calibrations.py
+++ pypeit/calibrations.py
@@ -819,10 +819,7 @@ class Calibrations(object):
         # Return existing data
         if self._cached('wavecalib', self.master_key_dict['arc']):
             self.wv_calib = self.calib_dict[self.master_key_dict['arc']]['wavecalib']
-            try:
-                self.slits.mask_wvcalib(self.wv_calib)
-            except:
-                embed(header='825 of header')
+            self.slits.mask_wvcalib(self.wv_calib)
             return self.wv_calib
 
         # No wavelength calibration requested
@@ -847,10 +844,7 @@ class Calibrations(object):
         if os.path.isfile(masterframe_name) and self.reuse_masters:
             # Load from disk
             self.wv_calib = self.waveCalib.load(masterframe_name)
-            try:
-                self.slits.mask_wvcalib(self.wv_calib)
-            except:
-                embed(header='853 of calibrations')
+            self.slits.mask_wvcalib(self.wv_calib)
         else:
             self.wv_calib = self.waveCalib.run(skip_QA=(not self.write_qa))
             # Save to Masters
