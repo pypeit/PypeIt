77c956cc9f0bb844e3a163bd867b39d8630e2d17
Feige Wang <fgwang.astro@gmail.com>
2020-04-18 17:39:36 -0700

    parset works for tell_corr

--- pypeit/scripts/tell_corr.py
+++ pypeit/scripts/tell_corr.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+#
+# See top-level LICENSE file for Copyright information
+#
+# -*- coding: utf-8 -*-
 
 from pypeit import par, msgs
 from astropy.io import fits
@@ -56,8 +61,8 @@ def parser(options=None):
     parser.add_argument("-g", "--tell_grid", type=str, help="Telluric model grid.")
     parser.add_argument("-p", "--pca_file", type=str, help="PCA pickle file")
     parser.add_argument("-t", "--tell_file", type=str, help="Configuration file to change default telluric parameters")
-    parser.add_argument("-r", "--redshift", type=float, default=0., help="Object redshift")
-    parser.add_argument("-n", "--norder", type=float, default=0., help="Polynomial order")
+    parser.add_argument("-r", "--redshift", type=float, default=None, help="Object redshift")
+    parser.add_argument("-n", "--norder", type=int, default=None, help="Polynomial order")
     parser.add_argument("--debug", default=False, action="store_true", help="show debug plots?")
     parser.add_argument("--plot", default=False, action="store_true", help="Show the telluric corrected spectrum")
     parser.add_argument("--par_outfile", default='telluric.par', help="Name of outut file to save the parameters used by the fit")
@@ -77,6 +82,7 @@ def main(args):
     header = fits.getheader(args.spec1dfile)
     spectrograph = load_spectrograph(header['PYP_SPEC'])
     spectrograph_def_par = spectrograph.default_pypeit_par()
+
     # If the .tell file was passed in read it and overwrite default parameters
     if args.tell_grid is not None:
         cfg_lines = read_tellfile(args.tell_file)
@@ -87,33 +93,33 @@ def main(args):
 
     # If args was provided override defaults. Note this does undo .tell file
     if args.algorithm is not None:
-        par['telluric']['algorithm'] = args.algorithm
+        par['tellfit']['algorithm'] = args.algorithm
     if args.tell_grid is not None:
-        par['telluric']['tell_grid'] = args.tell_grid
+        par['sensfunc']['IR']['telgridfile'] = args.tell_grid
     if args.pca_file is not None:
-        par['telluric']['pca_file'] = args.pca_file
+        par['tellfit']['pca_file'] = args.pca_file
     if args.redshift is not None:
-        par['telluric']['redshift'] = args.redshift
+        par['tellfit']['redshift'] = args.redshift
     if args.norder is not None:
-        par['telluric']['norder'] = args.norder
+        par['tellfit']['polyorder'] = args.norder
 
     # Write the par to disk
     print("Writing the parameters to {}".format(args.par_outfile))
-    par['telluric'].to_config('telluric.par', section_name='telluric', include_descr=False)
+    par['tellfit'].to_config('telluric.par', section_name='tellfit', include_descr=False)
 
     # Parse the output filename
-    outfile = (os.path.basename(args.spec1dfile)).replace('.fits','tellcorr.fits')
-    modelfile = (os.path.basename(args.spec1dfile)).replace('.fits','tellmodel.fits')
+    outfile = (os.path.basename(args.spec1dfile)).replace('.fits','_tellcorr.fits')
+    modelfile = (os.path.basename(args.spec1dfile)).replace('.fits','_tellmodel.fits')
 
     # Run the telluric fitting procedure.
-    if par['telluric']['algorithm']=='qso':
+    if par['tellfit']['algorithm']=='qso':
         # run telluric.qso_telluric to get the final results
-        TelQSO = telluric.qso_telluric(args.spec1dfile, par['telluric']['tell_grid'], par['telluric']['pca_file'],
-                                       par['telluric']['redshift'], modelfile, outfile,
+        TelQSO = telluric.qso_telluric(args.spec1dfile, par['sensfunc']['IR']['telgridfile'], par['tellfit']['pca_file'],
+                                       par['tellfit']['redshift'], modelfile, outfile,
                                        create_bal_mask=None, disp=args.plot, debug=args.debug, show=args.plot)
-    elif par['telluric']['algorithm']=='poly':
-        TelPoly = telluric.poly_telluric(args.spec1dfile, par['telluric']['tell_grid'], modelfile, outfile,
-                                         polyorder=par['telluric']['norder'],
+    elif par['tellfit']['algorithm']=='poly':
+        TelPoly = telluric.poly_telluric(args.spec1dfile, par['sensfunc']['IR']['telgridfile'], modelfile, outfile,
+                                         polyorder=par['tellfit']['polyorder'],
                                          fit_region_min=None, fit_region_max=None, func='legendre',
                                          model='exp', mask_lyman_a=True,
                                          debug_init=args.debug, debug=args.debug, show=args.plot)
