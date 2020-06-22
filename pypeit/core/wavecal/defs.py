fdd6893dc0a4ff7794b10625b0ad20cbb2df528b
rcooke-ast <rcooke.ast@gmail.com>
2020-06-22 11:15:06 +0100

    add unknwn

--- pypeit/core/wavecal/defs.py
+++ pypeit/core/wavecal/defs.py
@@ -58,6 +58,7 @@ class LinesBitMask(BitMask):
             ('ThAr_MagE', 'Comment'),
             ('OH_FIRE_Echelle', 'Comment'),
             ('Ar_IR_GNIRS', 'Comment'),
+            ('UNKNWN', 'Comment'),
         ])
         super(LinesBitMask, self).__init__(list(mask.keys()), descr=list(mask.values()))
 
@@ -98,6 +99,7 @@ def lines():
     line_dict['ThAr_MagE'] = 2**18  # R=4100
     line_dict['OH_FIRE_Echelle'] = 2**19  # R=6000
     line_dict['Ar_IR_GNIRS'] = 2**20  # R=6000
+    line_dict['UNKNWN'] = 2**21
 
 
 
