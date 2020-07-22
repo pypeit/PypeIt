84512eac6bfa80504fbe6a6d395b82c7f7471d5e
Andreas Flörs <33418619+afloers@users.noreply.github.com>
2020-07-22 19:58:39 +0200

    corrected decker card

--- pypeit/spectrographs/gemini_gnirs.py
+++ pypeit/spectrographs/gemini_gnirs.py
@@ -221,7 +221,7 @@ class GeminiGNIRSSpectrograph(spectrograph.Spectrograph):
         meta['ra'] = dict(ext=0, card='RA')
         meta['dec'] = dict(ext=0, card='DEC')
         meta['target'] = dict(ext=0, card='OBJECT')
-        meta['decker'] = dict(ext=0, card='SLIT')
+        meta['decker'] = dict(ext=0, card='decker')
 
         meta['binning'] = dict(ext=0, card=None, default='1,1')
         meta['mjd'] = dict(ext=0, card='MJD_OBS')
