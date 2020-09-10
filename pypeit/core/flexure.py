70277862929022a1a34f8660b3a7d88c9b0624ed
rcooke-ast <rcooke.ast@gmail.com>
2020-09-10 16:07:13 +0100

    fix fitting

--- pypeit/core/flexure.py
+++ pypeit/core/flexure.py
@@ -463,8 +463,7 @@ def spec_flexure_qa(slitords, bpm, basename, det, flex_list, out_dir=None):
         # Fit
         fit = this_flex_dict['polyfit']
         xval = np.linspace(-10., 10, 100) + this_flex_dict['corr_cen'] #+ flex_dict['shift'][o]
-        #model = (fit[2]*(xval**2.))+(fit[1]*xval)+fit[0]
-        model = utils.func_val(fit, xval, 'polynomial')
+        model = fit.eval(xval)
         mxmod = np.max(model)
         ylim_min = np.min(model/mxmod) if np.isfinite(np.min(model/mxmod)) else 0.0
         ylim = [ylim_min, 1.3]
