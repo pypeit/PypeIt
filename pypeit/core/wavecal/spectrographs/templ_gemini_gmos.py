b0b136aff6341d39975888078a5d3eb27f67e25e
profxj <xavier@ucolick.org>
2020-10-22 16:18:57 -0700

    working?

--- pypeit/core/wavecal/spectrographs/templ_gemini_gmos.py
+++ pypeit/core/wavecal/spectrographs/templ_gemini_gmos.py
@@ -70,4 +70,4 @@ def gemini_gmos_b600_ham(overwrite=False):
 
 if __name__ == '__main__':
     #gemini_gmos_r400_e2v(overwrite=True)
-    gemini_gmos_b600_ham()
+    gemini_gmos_b600_ham(overwrite=True)
