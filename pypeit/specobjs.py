7232aa8e1120726edd7dec1665b027a4dbed92c2
deborape <dpelliccia@ucolick.org>
2020-11-19 14:39:53 -0800

    some fixes

--- pypeit/specobjs.py
+++ pypeit/specobjs.py
@@ -644,8 +644,8 @@ class SpecObjs:
         """
         # TODO -- Deal with update_det
         # Lists for a Table
-        slits, names, maskdef_id, objname, spat_pixpos, spat_fracpos, boxsize, opt_fwhm, s2n = \
-            [], [], [], [], [], [], [], [], []
+        slits, names, maskdef_id, objname, objra, objdec, spat_pixpos, spat_fracpos, boxsize, opt_fwhm, s2n = \
+            [], [], [], [], [], [], [], [], [], [], []
         # binspectral, binspatial = parse.parse_binning(binning)
         for specobj in self.specobjs:
             det = specobj.DET
@@ -704,6 +704,9 @@ class SpecObjs:
                 maskdef_id.append(specobj.MASKDEF_ID)
             if specobj.MASKDEF_OBJNAME is not None:
                 objname.append(specobj.MASKDEF_OBJNAME)
+            if specobj.RA is not None:
+                objra.append(specobj.RA)
+                objdec.append(specobj.DEC)
 
         # Generate the table, if we have at least one source
         if len(names) > 0:
@@ -723,6 +726,11 @@ class SpecObjs:
                 obj_tbl['maskdef_id'].format = 'd'
             if specobj.MASKDEF_OBJNAME is not None:
                 obj_tbl['objname'] = objname
+            if specobj.RA is not None:
+                obj_tbl['objra'] = objra
+                obj_tbl['objra'].format = '.4f'
+                obj_tbl['objdec'] = objdec
+                obj_tbl['objdec'].format = '.4f'
             obj_tbl['spat_pixpos'] = spat_pixpos
             obj_tbl['spat_pixpos'].format = '.1f'
             obj_tbl['spat_fracpos'] = spat_fracpos
