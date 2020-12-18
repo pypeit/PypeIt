ab2fd2db12f2698a1b71acb40811cff4e6f14904
Milan Roberson <finagle29@gmail.com>
2020-12-17 17:37:48 -0800

    rewrite check_requirements.py

--- pypeit/check_requirements.py
+++ pypeit/check_requirements.py
@@ -3,18 +3,28 @@ Version checking.
 """
 
 import pkg_resources
+from pypeit import msgs
 
-requirements_file = pkg_resources.resource_filename('pypeit', 'requirements.txt')
+requirements_file = pkg_resources.resource_filename('pypeit', 'requirements_imports.txt')
 install_requires = [line.strip().replace('==', '>=') for line in open(requirements_file)
                     if not line.strip().startswith('#') and line.strip() != '']
 for requirement in install_requires:
-    pkg, version = requirement.split('>=')
+    pkg, version = requirement.split('>=', maxsplit=1)
+    version, *not_found_msg = version.split(",", maxsplit=1)
     try:
-        pv = pkg_resources.get_distribution(pkg).version
-    except pkg_resources.DistributionNotFound:
-        raise ImportError("Package: {:s} not installed!".format(pkg))
+        pkg = __import__(pkg, globals(), locals(), [], 0)
+        pv = pkg.__version__
+    except ModuleNotFoundError:
+        if not_found_msg:
+            msgs.warn(f"Package {pkg} not found. {not_found_msg[0]}")
+        else:
+            msgs.warn(f"Package {pkg} not found.")
     else:
         if pkg_resources.parse_version(pv) < pkg_resources.parse_version(version):
-            raise ImportError('Your version of {0} is incompatible with PypeIt.  '.format(pkg)
-                                + 'Please update to version >= {0}'.format(version))
-
+            if not_found_msg:
+                msgs.warn(f"Your {pkg.__name__} (version {pv}) is incompatible"
+                    f" with PypeIt. Please update to version >= {version}. "
+                    f"{not_found_msg[0]}")
+            else:
+                msgs.warn(f"Your {pkg.__name__} (version {pv}) is incompatible"
+                    f" with PypeIt. Please update to version >= {version}")
