7e1bc53d426c76b76ef06fb80a866b7a1bb89209
profxj <xavier@ucolick.org>
2021-07-12 16:31:51 -0700

    add runpypeit

--- pypeit/tests/test_runpypeit.py
+++ pypeit/tests/test_runpypeit.py
@@ -89,30 +89,39 @@ def test_run_pypeit_calib_only():
         shutil.rmtree(outdir)
         shutil.rmtree(testrawdir)
 
-@dev_suite_required
+#@dev_suite_required
 def test_run_pypeit():
+    '''
     # Get the directories
-    rawdir = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA', 'shane_kast_blue', '600_4310_d55')
+    rawdir = os.path.join(os.environ['PYPEIT_DEV'], 'RAW_DATA', 
+                          'shane_kast_blue', '600_4310_d55')
     assert os.path.isdir(rawdir), 'Incorrect raw directory'
+    files = [ 'b21.fits.gz', 'b22.fits.gz', 'b23.fits.gz', 
+             'b27.fits.gz', 'b1.fits.gz', 'b11.fits.gz', 
+             'b12.fits.gz', 'b13.fits.gz' ]
+    '''
 
     # Just get a few files
-    testrawdir = os.path.join(rawdir, 'TEST')
+    #testrawdir = os.path.join(rawdir, 'TEST')
+    testrawdir = data_path('')
+    '''
     if os.path.isdir(testrawdir):
         shutil.rmtree(testrawdir)
     os.makedirs(testrawdir)
-    files = [ 'b21.fits.gz', 'b22.fits.gz', 'b23.fits.gz', 'b27.fits.gz', 'b1.fits.gz',
-              'b11.fits.gz', 'b12.fits.gz', 'b13.fits.gz' ]
     for f in files:
         shutil.copy(os.path.join(rawdir, f), os.path.join(testrawdir, f))
+    '''
 
-    outdir = os.path.join(os.getenv('PYPEIT_DEV'), 'REDUX_OUT_TEST')
+    #outdir = os.path.join(os.getenv('PYPEIT_DEV'), 'REDUX_OUT_TEST')
+    outdir = data_path('REDUX_OUT_TEST')
 
     # For previously failed tests
     if os.path.isdir(outdir):
         shutil.rmtree(outdir)
 
     # Run the setup
-    sargs = Setup.parse_args(['-r', testrawdir, '-s', 'shane_kast_blue', '-c all', '-o',
+    sargs = Setup.parse_args(['-r', testrawdir+'b', '-s', 
+                              'shane_kast_blue', '-c all', '-o', 
                               '--output_path', outdir])
     Setup.main(sargs)
 
@@ -150,5 +159,7 @@ def test_run_pypeit():
 
     # Clean-up
     shutil.rmtree(outdir)
+    '''
     shutil.rmtree(testrawdir)
+    '''
 
