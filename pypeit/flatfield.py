9f2babbb0f8767f9e35c22bd0514c5f2f9d17694
deborape <dpelliccia@ucolick.org>
2021-09-09 16:45:53 -1000

    minor

--- pypeit/flatfield.py
+++ pypeit/flatfield.py
@@ -265,7 +265,7 @@ class FlatImages(datamodel.DataContainer):
         # Loop
         for slit_idx in range(slits.nslits):
             # Skip masked
-            if (slits.mask[slit_idx] != 0):
+            if slits.mask[slit_idx] != 0:
                 continue
             # Skip those without a bspline
             # DO it
