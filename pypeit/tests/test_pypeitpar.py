3a660c4d36aea35ccc510580c0a7dbca96efcdfa
profxj <xavier@ucolick.org>
2021-10-13 12:00:49 -0700

    test fix

--- pypeit/tests/test_pypeitpar.py
+++ pypeit/tests/test_pypeitpar.py
@@ -50,8 +50,8 @@ def test_sensfuncuvis():
 def test_telluric():
     pypeitpar.TelluricPar()
 
-def test_manualextraction():
-    pypeitpar.ManualExtractionPar()
+#def test_manualextraction():
+#    pypeitpar.ManualExtractionPar()
 
 # TODO: Valid spectrographs are not longer read by pypeit.pypeitpar; it causes
 # a circular import.
