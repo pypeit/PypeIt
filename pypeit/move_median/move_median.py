9b3480d4d250aad5fea86b1134ccd3d74172dad6
joe <joe@physics.ucsb.edu>
2022-04-22 11:10:21 -1000

    Devsuite fixes.

--- pypeit/move_median/move_median.py
+++ pypeit/move_median/move_median.py
@@ -3,10 +3,7 @@ import warnings
 try:
     from bottleneck import move_median as move_median_
     def move_median(seq, window):
-        try:
-            return move_median_(seq, window=window)
-        except:
-            embed()
+        return move_median_(seq, window=window)
     #warnings.warn("using bottleneck")
 except:
     warnings.warn('Unable to load bottleneck moving median.  Try reinstalling bottleneck.  In the '
