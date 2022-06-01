7bed733d1c9a79a6dc1d45e0c292f66e3ff9a1be
Dusty Reichwein <dusty@ucolick.org>
2022-05-31 17:18:30 -0700

    Convert test_archive to not use cooked

--- pypeit/tests/test_archive.py
+++ pypeit/tests/test_archive.py
@@ -8,7 +8,7 @@ from functools import partial
 import filecmp
 
 from pypeit.archive import ArchiveMetadata, ArchiveDir
-from pypeit.tests.tstutils import cooked_required, data_path
+from pypeit.tests.tstutils import data_path
 
 
 class mock_file_info:
@@ -16,15 +16,15 @@ class mock_file_info:
         self.path = path
         self.name = name
 
-_COOKED_FILE1 = "spec1d_b27-J1217p3905_KASTb_20150520T045733.560.fits"
-_COOKED_FILE2 = "spec1d_b28-J1217p3905_KASTb_20150520T051801.470.fits"
+_DATA_FILE1 = "spec1d_b27.fits"
+_DATA_FILE2 = "spec1d_b28.fits"
 def get_simple_metadata(file_info):
     if isinstance(file_info, mock_file_info):
         return (None, None)
 
     dest_file = os.path.basename(file_info)
-    metadata = {_COOKED_FILE1: [1, dest_file, 1.234, "2021-01-01"],
-                _COOKED_FILE2: [2, dest_file, 1.234, "2021-01-02"] }
+    metadata = {_DATA_FILE1: [1, dest_file, 1.234, "2021-01-01"],
+                _DATA_FILE2: [2, dest_file, 1.234, "2021-01-02"] }
 
     return ([metadata[dest_file]], [(file_info, dest_file)])
 
@@ -33,9 +33,9 @@ def get_multirow_metadata(file_info):
         return (None, None)
 
     dest_file = file_info.name
-    metadata = {_COOKED_FILE1: [[1, dest_file, "part1"],
+    metadata = {_DATA_FILE1: [[1, dest_file, "part1"],
                                 [1, dest_file, "part2"]],
-                _COOKED_FILE2: [[2, dest_file, "part1"],
+                _DATA_FILE2: [[2, dest_file, "part1"],
                                 [2, dest_file, "part2"],
                                 [2, dest_file, "part3"]]}
 
@@ -46,10 +46,10 @@ def test_archive_meta(tmp_path):
 
     col_names = ["id", "file", "num", "date"]
 
-    orig_file1 = os.path.join("orig_path", _COOKED_FILE1)
-    dest_file1 = _COOKED_FILE1
-    orig_file2 = os.path.join("orig_path", _COOKED_FILE2)
-    dest_file2 = _COOKED_FILE2
+    orig_file1 = os.path.join("orig_path", _DATA_FILE1)
+    dest_file1 = _DATA_FILE1
+    orig_file2 = os.path.join("orig_path", _DATA_FILE2)
+    dest_file2 = _DATA_FILE2
 
     expected_rows = [ [1, dest_file1, 1.23, '2021-01-01'],
                       [2, dest_file2, 1.234, '2021-01-02']]
@@ -76,14 +76,12 @@ def test_archive_meta(tmp_path):
 
     assert test_meta3._metadata == expected_rows
 
-# TODO -- Move to DevSuite [DR]
-@cooked_required
 def test_archive_dir(tmp_path):
 
     archive_root = str(tmp_path)
     metadata_file1 = os.path.join(archive_root, "by_id_meta.dat")
     metadata_file2 = os.path.join(archive_root, "by_object_meta.dat")
-    source_path = os.path.join(os.environ['PYPEIT_DEV'], 'Cooked', 'Science')
+    source_path = data_path("")
 
     # Test an Archiver with two metadta files, one with multiple rows per item
     col_names1 = ["id", "file", "num", "date"]
@@ -96,22 +94,22 @@ def test_archive_dir(tmp_path):
 
     # Test an archvier that doesn't copy files.
     archive_dir_nocopy = ArchiveDir(archive_root, [test_meta1, test_meta2], False)
-    archive_dir_nocopy.add(os.path.join(source_path, _COOKED_FILE1))
+    archive_dir_nocopy.add(os.path.join(source_path, _DATA_FILE1))
     archive_dir_nocopy.save()
 
-    assert not os.path.exists(os.path.join(archive_root, _COOKED_FILE1))
+    assert not os.path.exists(os.path.join(archive_root, _DATA_FILE1))
 
     # Test an archiver that does copy. This uses a metadta object that won't
     # append to the metadata from the previous archive_dir_nocopy save
     archive_dir = ArchiveDir(archive_root, [test_meta3, test_meta2], True)
 
-    archive_dir.add(os.path.join(source_path, _COOKED_FILE1))
-    archive_dir.add(mock_file_info(source_path, _COOKED_FILE2))
+    archive_dir.add(os.path.join(source_path, _DATA_FILE1))
+    archive_dir.add(mock_file_info(source_path, _DATA_FILE2))
     archive_dir.save()
 
     # Verify files were copied
-    dest_file1 = tmp_path / _COOKED_FILE1
-    dest_file2 = tmp_path / _COOKED_FILE2
+    dest_file1 = tmp_path / _DATA_FILE1
+    dest_file2 = tmp_path / _DATA_FILE2
     assert dest_file1.exists()
     assert dest_file2.exists()
 
