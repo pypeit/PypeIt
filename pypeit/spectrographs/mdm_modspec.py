88a93f7c4478fa9d386765d8a2afc41a1cc4137a
hnmartin <106616476+hnmartin@users.noreply.github.com>
2022-06-17 12:06:46 -0500

    Began following PypeIt's dev info for new spectrograph
    Up to now, had been comparing the mdm_osmos PypeIt module to a mdm_modspec Iraf document and simply adjusting the variables accordingly; now began to follow the systematic approach outlined in https://pypeit.readthedocs.io/en/latest/new_spectrograph.html
    
    Added a pypeline
    referenced this module in the startup file
    Added in blank MetaData cards to fill in later
    Added a bad pixels mask

--- pypeit/spectrographs/mdm_modspec.py
+++ pypeit/spectrographs/mdm_modspec.py
@@ -21,6 +21,7 @@ class MDMModspecEchelleSpectrograph(spectrograph.Spectrograph):
     telescope = telescopes.KPNOTelescopePar()
     camera = 'Echelle'
     header_name = 'ModSpec'
+    pypeline = 'MultiSlit'
     supported = True
     comment = 'MDM Modspec spectrometer'
 
@@ -108,7 +109,7 @@ class MDMModspecEchelleSpectrograph(spectrograph.Spectrograph):
         par['calibrations']['wavelengths']['lamps'] = ['ArI', 'XeI', 'NeI']
         par['calibrations']['wavelengths']['reid_arxiv'] = 'mdm_modspec.fits' #abovementioned archived wavelength solution; need one for Echelle / Modspec
         ###|||||| do this one below ||||||###
-        par['calibrations']['wavelengths']['sigdetect'] = 10.0 #Sigma threshold above fluctuations for arc-line detection
+        par['calibrations']['wavelengths']['sigdetect'] = 5.0 #Sigma threshold above fluctuations for arc-line detection
         
         # Set the default exposure time ranges for the frame typing
         par['calibrations']['biasframe']['exprng'] = [None, 1]
@@ -127,26 +128,32 @@ class MDMModspecEchelleSpectrograph(spectrograph.Spectrograph):
         That is, this associates the ``PypeIt``-specific metadata keywords
         with the instrument-specific header cards using :attr:`meta`.
         """
+        # see https://pypeit.readthedocs.io/en/latest/setup.html?highlight=decker#overview
         self.meta = {}
         # Required (core)
         self.meta['ra'] = dict(ext=0, card='RA')
         self.meta['dec'] = dict(ext=0, card='DEC')
         self.meta['target'] = dict(ext=0, card='OBJECT')
-        self.meta['decker'] = dict(card=None)
+        self.meta['decker'] = dict(card=None) # str -- ie long_1.0 -- Name of the decker or slit mask 
         self.meta['binning'] = dict(card=None, compound=True)
-        #self.meta['binning'] = [dict(ext=0, card='CCDBIN1'), dict(ext=0, card='CCDBIN2')]
-
+        
+        self.meta['amp'] = '' # str -- ie SINGLE:B -- Name of the amplifier used to read the detector
+        self.meta['arm'] = '' # str -- ie VIS -- Name of the spectrograph arm used to collect the data
+        self.meta['datasec'] = dict(ext=0, card='DATASEC') # str -- ie [1:256,1:512] -- The science region of the detector
+        self.meta['detector'] = '' # str -- ie CHIP1 -- Name of the detector
+        self.meta['dichroic'] = '' # str -- ie 560 -- Name of the dichroic
+        self.meta['filter1'] = dict(ext=0, card='FILTER') # str -- ie J -- Name of the order-sorting filter
+        
         self.meta['mjd'] = float(dict(ext=0, card='JD')) - 2400000.5
         self.meta['exptime'] = dict(ext=0, card='EXPTIME')
         self.meta['airmass'] = dict(ext=0, card='AIRMASS')
+        
         # Extras for config and frametyping
         # in an example of what this code generates, see https://pypeit.readthedocs.io/en/latest/pypeit_file.html#pypeit-file
-        ## on that note, 'dispname' is showing 600/4310 and that makes no sense to me 
-        ## okay apparently according to https://watermark.silverchair.com/360-4-1281.pdf?token=AQECAHi208BE49Ooan9kkhW_Ercy7Dm3ZL_9Cf3qfKAc485ysgAAAuYwggLiBgkqhkiG9w0BBwagggLTMIICzwIBADCCAsgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMYDY3odQx18n61h_OAgEQgIICmRDa6yhMHVq4rhm9vxfUOxJJk38Ppwr37LtGSH08YK5TV_Y3sUaRQ6Pd_CRDP4HpCy0UBoRIEvJMaW77Tnq1k45akgXw26T5D-SsWWWX8v3JYjgD5wJRzcM_lbQOgljRuBgffDixM-kc4kFvlzAUGdPfSAKT-0kYp9Bj3X6SX2QUKJabemNm3kuApMDpgOQYj1_JXDxH1FAzqUco3abNAJSJwXp33t8vhNaK3hTI_CO0X66ul6PYx2aGEs1MG2AFLT0rnPt_Q1NSoXnGhRV4AJVMXJF58I7oZ_0bLuN_P-e87J9VNA1JBp9CNJzqYN9Rz0TOHEDhmC05NTYmfNpbbZUW6R4x9u6s8cQiaK0J5C6m_DtZgxokt5Rg7fSEtOZFzD42i-B1Aln4BqX57o423Uhi1QkYGfk5eeReq-fKRFEy-vrsuEiAPYhLE045xQR_OiaKeQkeQEMxumZcDA4FPLHcF9W_cIBc9-Qr24h-xdKXYpkCg994hOcdOzMvJon9nAFJyyW6CAENtDDmFewW9-Ht3EwPYCR0Remb3SuddgZPCxBXoijUcjf7YPw9PRqRLEJ26K0ag10B_eAgwjkcEAwThhHGDp9EkkB8cmlwjca5uPOlfrZ_lqen0y-UC-8wMh4bdcvqUCmsMg3GoPC4q8CVgTPdPvXOhROgyCSbJi_J53RZD8CkE1K7K9dfDN7UsFPwhb31qWOw1FoG5dAv4xWUJZG_zOe503hFhWvql8J9Go2J1IcdZXIA1eRBueW7GF9SffIp0YQddpg4e8oAwiMKD3Tqed0--lZ2Oqx6fbbubfuo5ppFTDZi83VrlRbxdPU-blJYdYJDSDH5DoQ6xQZ6QBOwU0M95TkmiNb7LgJWz5k6pdDCbDqu
-        ## it is Grism/grating (lines mm^-1 / blaze), whatever that means
-        #self.meta['dispname'] = 'Diffraction Grating'
-        self.meta['dispname'] = '1200/5000' ## check on how this needs to be formatted
+        self.meta['dispname'] = '1200/5100' # str -- ie 830G -- Name of the dispersing element
+        self.meta['dispangle'] = 5100.0 # float -- ie 7500.0 -- Central wavelength for the dispersing element at the observed angle
         self.meta['idname'] = dict(ext=0, card='IMAGETYP')
+       
         # Lamps
         self.meta['lampstat01'] = dict(ext=0, card='LAMPS')
         self.meta['instrument'] = dict(ext=0, card='INSTRUME')
@@ -188,6 +195,9 @@ class MDMModspecEchelleSpectrograph(spectrograph.Spectrograph):
             object.
         """
         return ['dispname', 'decker', 'binning']
+        ## since there is no 'decker' card, maybe replace this with a different parameter
+        ## return ['dispname', 'dispangle', 'binning']
+        ## for why 'dispangle' was chosen, see https://pypeit.readthedocs.io/en/latest/setup.html?highlight=decker#overview
 
     def pypeit_file_keys(self):
         """
@@ -225,6 +235,93 @@ class MDMModspecEchelleSpectrograph(spectrograph.Spectrograph):
         if ftype == 'bias':
             return good_exp & (fitstbl['imagetyp'] == 'zero')
         if ftype in ['arc','tilt']:
-            return good_exp & np.array([ilamp in ['Ar','Xe'] for ilamp in fitstbl['lampstat01']]) & (fitstbl['idname'] == 'COMP')
+            return good_exp & np.array([ilamp in ['Ar','Xe', 'Ne'] for ilamp in fitstbl['lampstat01']]) & (fitstbl['idname'] == 'COMP')
         msgs.warn('Cannot determine if frames are of type {0}.'.format(ftype))
         return np.zeros(len(fitstbl), dtype=bool)
+    
+    def bpm(self, filename, det, shape=None, msbias=None):
+        """
+        Generate a default bad-pixel mask.
+
+        Even though they are both optional, either the precise shape for
+        the image (``shape``) or an example file that can be read to get
+        the shape (``filename`` using :func:`get_image_shape`) *must* be
+        provided.
+
+        Args:
+            filename (:obj:`str` or None):
+                An example file to use to get the image shape.
+            det (:obj:`int`):
+                1-indexed detector number to use when getting the image
+                shape from the example file.
+            shape (tuple, optional):
+                Processed image shape
+                Required if filename is None
+                Ignored if filename is not None
+            msbias (`numpy.ndarray`_, optional):
+                Master bias frame used to identify bad pixels
+
+        Returns:
+            `numpy.ndarray`_: An integer array with a masked value set
+            to 1 and an unmasked value set to 0.  All values are set to
+            0.
+        """
+        # Validate the entered (list of) detector(s)
+        nimg, _det = self.validate_det(det)
+        _det = list(_det)
+
+        # Call the base-class method to generate the empty bpm
+        bpm_img = super().bpm(filename, det, shape=shape, msbias=msbias)
+        # NOTE: expand_dims does *not* copy the array.  We can edit it directly
+        # because we've created it inside this function.
+        _bpm_img = np.expand_dims(bpm_img, 0) if nimg == 1 else bpm_img
+
+        if 1 in _det:
+            i = _det.index(1)
+            _bpm_img[i,:,1052:1054] = 1
+        if 2 in _det:
+            i = _det.index(2)
+            _bpm_img[i,:,0:4] = 1
+            _bpm_img[i,:,376:381] = 1
+            _bpm_img[i,:,489] = 1
+            _bpm_img[i,:,1333:1335] = 1
+            _bpm_img[i,:,2047] = 1
+        if 3 in _det:
+            i = _det.index(3)
+            _bpm_img[i,:,0:4] = 1
+            _bpm_img[i,:,221] = 1
+            _bpm_img[i,:,260] = 1
+            _bpm_img[i,:,366] = 1
+            _bpm_img[i,:,816:819] = 1
+            _bpm_img[i,:,851] = 1
+            _bpm_img[i,:,940] = 1
+            _bpm_img[i,:,1167] = 1
+            _bpm_img[i,:,1280] = 1
+            _bpm_img[i,:,1301:1303] = 1
+            _bpm_img[i,:,1744:1747] = 1
+            _bpm_img[i,:,-4:] = 1
+        if 4 in _det:
+            i = _det.index(4)
+            _bpm_img[i,:,0:4] = 1
+            _bpm_img[i,:,47] = 1
+            _bpm_img[i,:,744] = 1
+            _bpm_img[i,:,790:792] = 1
+            _bpm_img[i,:,997:999] = 1
+        if 5 in _det:
+            i = _det.index(5)
+            _bpm_img[i,:,25:27] = 1
+            _bpm_img[i,:,128:130] = 1
+            _bpm_img[i,:,1535:1539] = 1
+        if 7 in _det:
+            i = _det.index(7)
+            _bpm_img[i,:,426:428] = 1
+            _bpm_img[i,:,676] = 1
+            _bpm_img[i,:,1176:1178] = 1
+        if 8 in _det:
+            i = _det.index(8)
+            _bpm_img[i,:,440] = 1
+            _bpm_img[i,:,509:513] = 1
+            _bpm_img[i,:,806] = 1
+            _bpm_img[i,:,931:934] = 1
+
+        return _bpm_img[0] if nimg == 1 else _bpm_img
