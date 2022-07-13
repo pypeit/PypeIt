da4ca900caa313519ccf4b2f4e5d1c5cabca6221
rcooke <rcooke.ast@gmail.com>
2022-07-13 16:34:49 +0100

    add a test

--- pypeit/spectrographs/keck_nirspec.py
+++ pypeit/spectrographs/keck_nirspec.py
@@ -257,16 +257,16 @@ class KeckNIRSPECSpectrograph(spectrograph.Spectrograph):
         if status == 'off':
             # Check if all are off
             lamp_stat = [k for k in fitstbl.keys() if 'lampstat' in k]
-            retarr = np.zeros((len(lamp_stat), len(fitstbl)))
+            retarr = np.zeros((len(lamp_stat), len(fitstbl)), dtype=bool)
             for kk, key in enumerate(lamp_stat):
-                retarr[kk,:] = fitstbl['lampstat01'].data.astype(np.int) == 0
+                retarr[kk,:] = fitstbl[key].data.astype(np.int) == 0
             return np.all(retarr, axis=0)
         if status == 'arcs':
             # Check if any arc lamps are on
             lamp_stat = [ 'lampstat{0:02d}'.format(i) for i in range(1,6) ]
             retarr = np.zeros((len(lamp_stat), len(fitstbl)))
             for kk, key in enumerate(lamp_stat):
-                retarr[kk,:] = fitstbl['lampstat01'].data.astype(np.int) == 1
+                retarr[kk,:] = fitstbl[key].data.astype(np.int) == 1
             return np.any(retarr, axis=0)
         if status == 'dome':
             return fitstbl['lampstat06'].data.astype(int) == 1
