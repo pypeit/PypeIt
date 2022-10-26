3b19e09b3d69f389d432574537a7d05526c54207
rcooke-ast <rcooke-ast@gmail.com>
2022-10-26 15:26:46 +0100

    doc fix

--- pypeit/par/pypeitpar.py
+++ pypeit/par/pypeitpar.py
@@ -3548,9 +3548,9 @@ class FindObjPar(ParSet):
                              'For mulitslit maxnumber applies per slit, for echelle observations this ' \
                              'applies per order. Note that objects on a slit/order impact the sky-modeling and so ' \
                              'maxnumber should never be lower than the true number of detectable objects on your slit. ' \
-                             'For image differenced observations with positive and negative object traces, maxnumber applies' \
-                             'to the number of positive (or negative) traces individually. In other words, if you had two positive objects and' \
-                             'one negative object, then you would set maxnumber to be equal to two (not three). Note that if manually' \
+                             'For image differenced observations with positive and negative object traces, maxnumber applies ' \
+                             'to the number of positive (or negative) traces individually. In other words, if you had two positive objects and ' \
+                             'one negative object, then you would set maxnumber to be equal to two (not three). Note that if manually ' \
                              'extracted apertures are explicitly requested, they do not count against this maxnumber. If more than ' \
                              'maxnumber objects are detected, then highest S/N ratio objects will be the ones that are kept. ' \
                              'For multislit observations the choice here depends on the slit length. For echelle observations ' \
