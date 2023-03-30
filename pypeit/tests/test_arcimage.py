f44c8e2c123b2939c2a84b357bd99db27df592b6
Kyle Westfall <westfall@ucolick.org>
2023-03-30 10:05:13 -0700

    test fix

--- pypeit/tests/test_arcimage.py
+++ pypeit/tests/test_arcimage.py
@@ -31,7 +31,8 @@ def test_io():
     pypeitImage.PYP_SPEC = 'shane_kast_blue'
     # Now the arcimage
     arcImage = buildimage.ArcImage.from_pypeitimage(pypeitImage, calib_dir=data_path(''),
-                                                    setup='A', calib_id=['1'])
+                                                    setup='A', calib_id=['1'],
+                                                    detname='DET01')
     # Set paths and check name
     ofile = Path(arcImage.get_path()).resolve()
     assert str(ofile) == data_path('Arc_A_1_DET01.fits'), 'Calibration file name changed'
