Added some functinoality to display and fixed masking issues in ech_local_skysub_extract

pypeit/core/skysub.py
@@ -1025,7 +1025,7 @@ def local_skysub_extract(sciimg, sciivar, tilts, waveimg, global_sky, thismask, 
 
 def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
                              global_sky, left, right,
-                             slitmask, sobjs, order_vec, spat_pix=None,
+                             slitmask, sobjs, spat_pix=None,
                              fit_fwhm=False,
                              min_snr=2.0, bsp=0.6, trim_edg=(3,3), std=False, prof_nsigma=None,
                              niter=4, sigrej=3.5, bkpts_optimal=True,
@@ -1034,9 +1034,20 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
                              show_resids=False, show_fwhm=False, adderr=0.01, base_var=None,
                              count_scale=None):
     """
-    Perform local sky subtraction, profile fitting, and optimal extraction slit by slit
+    Perform local sky subtraction, profile fitting, and optimal extraction slit by slit. Objects
+    are sky/subtracted extracted in order of the highest average (across all orders) S/N ratio object first, and then
+    for a given object the highest S/N ratio orders are extracted first. The profile fitting FWHM are stored and
+    progressively fit as the objects are extracted to properly ensure that low S/N orders use Gaussian extracted
+    FWHMs from higher-S/N orders (i.e. in the regime where the data is too noisy for a non-parametric object
+    profile fit. The FWHM of higher S/N ratio objects are used for lower S/N ratio objects
+    (note this assumes point sources with FWHM set by the seeing).
 
-    IMPROVE THIS DOCSTRING
+    Note on masking:  Tis routine requires that all masking be performed in the upstream calling routine
+    (:func:`pypeit.extraction`) and thus the slitmask image must only include the unmasked (good) SLITIDs that are to be
+    extracted, and likewise the sobjs object must only include the unmasked (good) objects that are to be extracted. The
+    number of sobjs objects must equal to an integer multiple of the number of good slits/orders in the slitmask image,
+    and there must be a one-to-one correspondence between the unique SLITDS in the sobj obect and the SLITIDs in the
+    slitmask image. The routine will fault if any of these criteria are not met.
 
     Parameters
     ----------
@@ -1068,8 +1079,6 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
     sobjs : :class:`~pypeit.specobjs.SpecoObjs` object
         Object containing the information about the objects found on the
         slit/order from objfind or ech_objfind
-    order_vec: `numpy.ndarray`_
-        Vector of order numbers
     spat_pix: `numpy.ndarray`_, optional
         Image containing the spatial location of pixels. If not
         input, it will be computed from ``spat_img =
@@ -1210,35 +1219,59 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
     ivarmodel = np.copy(sciivar)
     sobjs = sobjs.copy()
 
-    norders = order_vec.size
-    slit_vec = np.arange(norders)
+    # Identify the unique SLITIDs and orders in the sobjs object
+    slitids = np.unique(sobjs.SLITID) # This will also sort the slitids
+    norders = (np.unique(sobjs.ECH_ORDER)).size
 
     # Find the spat IDs
     gdslit_spat = np.unique(slitmask[slitmask >= 0]).astype(int)  # Unique sorts
-    #if gdslit_spat.size != norders:
-    #    msgs.error("You have not dealt with masked orders properly")
 
-    #if (np.sum(sobjs.sign > 0) % norders) == 0:
-    #    nobjs = int((np.sum(sobjs.sign > 0)/norders))
-    #else:
-    #    msgs.error('Number of specobjs in sobjs is not an integer multiple of the number or ordres!')
+    if not np.array_equal(gdslit_spat, slitids):
+        msgs.error('The slitmask image and the sobjs object do not have the same number of good orders! '
+                   'There is a problem with the object/slitmask masking. This routine requires that all '
+                   'masking is performed in the calling routine.')
+    if norders != len(slitids):
+        msgs.error('The number of orders in the sobjs object does not match the number of good slits in the '
+                   'slitmask image! There is a problem with the object/slitmask masking. This routine '
+                   'requires that all masking is performed in the calling routine.')
 
-    # Set bad obj to -nan
+    # Check that the slit edges are masked consistent with the number of orders and the number of unique spatids
+    nleft = left.shape[1]
+    nrigh = right.shape[1]
+    if (nleft != nrigh) or norders != nleft or norders != nrigh:
+        msgs.error('The number of left and right edges must be the same as the number of orders. '
+                   'There is likely a problem with your masking')
+
+    # Now assign the order_sn, and generate an order_vec aligned with the slitids
     uni_objid = np.unique(sobjs[sobjs.sign > 0].ECH_OBJID)
     nobjs = len(uni_objid)
-    order_snr = np.zeros((norders, nobjs))
-    order_snr_gpm = np.ones_like(order_snr, dtype=bool)
-    for iord in range(norders):
+    order_snr = np.full((norders, nobjs), np.nan)
+    order_vec = np.zeros(norders, dtype=int)
+    for islit, slitid in enumerate(slitids):
         for iobj in range(nobjs):
-            ind = (sobjs.ECH_ORDERINDX == iord) & (sobjs.ECH_OBJID == uni_objid[iobj])
-            # Allow for missed/bad order
+            ind = (sobjs.SLITID == slitid) & (sobjs.ECH_OBJID == uni_objid[iobj])
+            # Check for a missed order and fault if they exist
             if np.sum(ind) == 0:
-                order_snr_gpm[iord,iobj] = False
+                msgs.error('There is a missing order for object {0:d} on slit {1:d}!'.format(iobj, slitid))
             else:
-                order_snr[iord,iobj] = sobjs[ind].ech_snr
+                if iobj == 0:
+                    order_vec[islit] = sobjs[ind].ECH_ORDER
+                order_snr[islit,iobj] = sobjs[ind].ech_snr
+
+    # Enforce that the number of objects in the sobjs object is an integer multiple of the number of good orders
+    if (np.sum(sobjs.sign > 0) % norders) == 0:
+        nobjs = int((np.sum(sobjs.sign > 0)/norders))
+    else:
+        msgs.error('Number of specobjs in sobjs is not an integer multiple of the number or orders!')
+    # Enforce that every object in sobj has an specobj on every good order
+    if np.any(np.isnan(order_snr)):
+        msgs.error('There are missing orders for one or more objects in sobjs. There is a problem with how you have '
+                   'masked objects in sobjs or slits in slitmask in the calling routine')
+
+
 
     # Compute the average SNR and find the brightest object
-    snr_bar = np.sum(order_snr*order_snr_gpm,axis=0) / np.sum(order_snr_gpm,axis=0)
+    snr_bar = np.mean(order_snr,axis=0)
     srt_obj = snr_bar.argsort()[::-1]
     ibright = srt_obj[0] # index of the brightest object
 
@@ -1250,8 +1283,7 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
     # Print out a status message
     str_out = ''
     for iord in srt_order_snr:
-        if order_snr_gpm[iord,ibright]:
-            str_out += '{:<8d}{:<8d}{:>10.2f}'.format(slit_vec[iord], order_vec[iord], order_snr[iord,ibright]) + msgs.newline()
+        str_out += '{:<8d}{:<8d}{:>10.2f}'.format(slitids[iord], order_vec[iord], order_snr[iord,ibright]) + msgs.newline()
     dash = '-'*27
     dash_big = '-'*40
     msgs.info(msgs.newline() + 'Reducing orders in order of S/N of brightest object:' + msgs.newline() + dash +
@@ -1259,9 +1291,6 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
               msgs.newline() + str_out)
     # Loop over orders in order of S/N ratio (from highest to lowest) for the brightest object
     for iord in srt_order_snr:
-        # Is this a bad slit?
-        if not np.any(order_snr_gpm[iord,:]):
-            continue
         order = order_vec[iord]
         msgs.info("Local sky subtraction and extraction for slit/order: {:d}/{:d}".format(iord,order))
         other_orders = (fwhm_here > 0) & np.invert(fwhm_was_fit)
@@ -1292,13 +1321,13 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
                         fwhm_this_ord = np.median(fwhm_here[other_orders])
                         fwhm_all = np.full(norders,fwhm_this_ord)
                         fwhm_str = 'median '
-                    indx = (sobjs.ECH_OBJID == uni_objid[iobj]) & (sobjs.ECH_ORDERINDX == iord)
+                    indx = (sobjs.ECH_OBJID == uni_objid[iobj]) & (sobjs.SLITID == slitids[iord])
                     for spec in sobjs[indx]:
                         spec.FWHM = fwhm_this_ord
 
                     str_out = ''
                     for slit_now, order_now, snr_now, fwhm_now in zip(
-                        slit_vec[other_orders], order_vec[other_orders],
+                        slitids[other_orders], order_vec[other_orders],
                         order_snr[other_orders,ibright], 
                         fwhm_here[other_orders]):
                         str_out += '{:<8d}{:<8d}{:>10.2f}{:>10.2f}'.format(slit_now, order_now, snr_now, fwhm_now) + msgs.newline()
@@ -1325,13 +1354,13 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
                         plt.show()
                 else:
                     # If this is not the brightest object then assign it the FWHM of the brightest object
-                    indx     = np.where((sobjs.ECH_OBJID == uni_objid[iobj]) & (sobjs.ECH_ORDERINDX == iord))[0][0]
-                    indx_bri = np.where((sobjs.ECH_OBJID == uni_objid[ibright]) & (sobjs.ECH_ORDERINDX == iord))[0][0]
+                    indx     = np.where((sobjs.ECH_OBJID == uni_objid[iobj]) & (sobjs.SLITID == slitids[iord]))[0][0]
+                    indx_bri = np.where((sobjs.ECH_OBJID == uni_objid[ibright]) & (sobjs.SLITID == slitids[iord]))[0][0]
                     spec = sobjs[indx]
                     spec.FWHM = sobjs[indx_bri].FWHM
 
-        thisobj = (sobjs.ECH_ORDERINDX == iord) # indices of objects for this slit
-        thismask = slitmask == gdslit_spat[iord] # pixels for this slit
+        thisobj = (sobjs.SLITID == slitids[iord]) # indices of objects for this slit
+        thismask = slitmask == slitids[iord] # pixels for this slit
         # True  = Good, False = Bad for inmask
         inmask = fullmask.flagged(invert=True) & thismask
         # Local sky subtraction and extraction
@@ -1350,7 +1379,7 @@ def ech_local_skysub_extract(sciimg, sciivar, fullmask, tilts, waveimg,
         except:
             embed()
         # update the FWHM fitting vector for the brighest object
-        indx = (sobjs.ECH_OBJID == uni_objid[ibright]) & (sobjs.ECH_ORDERINDX == iord)
+        indx = (sobjs.ECH_OBJID == uni_objid[ibright]) & (sobjs.SLITID == slitids[iord])
         fwhm_here[iord] = np.median(sobjs[indx].FWHMFIT)
         # Did the FWHM get updated by the profile fitting routine in local_skysub_extract? If so, include this value
         # for future fits