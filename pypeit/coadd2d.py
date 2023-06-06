finally got everything working, fixed my sign error in header offsets

pypeit/coadd2d.py
@@ -27,7 +27,7 @@ from pypeit.images import pypeitimage
 from pypeit.core import findobj_skymask
 from pypeit.core.wavecal import wvutils
 from pypeit.core import coadd
-#from pypeit.core import parse
+from pypeit.core import parse
 from pypeit import calibrations
 from pypeit import spec2dobj
 from pypeit.core.moment import moment1d
@@ -471,7 +471,15 @@ class CoAdd2D:
         coadd_list = []
         for slit_idx in self.good_slits:
             msgs.info('Performing 2d coadd for slit: {:d}/{:d}'.format(slit_idx, self.nslits_single - 1))
-            ref_trace_stack = self.reference_trace_stack(slit_idx, offsets=self.offsets,
+            
+            if self.angle_offsets:
+                if self.pypeline == 'Echelle':
+                    pscale = self.spectrograph.order_platescale(self.spectrograph.orders,self.binning)[slit_idx]
+                else:
+                    bin_spec, bin_spat = parse.parse_binning(self.binning)
+                    pscale = self.stack_dict['detectors'][0].platescale*bin_spat
+                offsets = self.offsets / pscale
+            ref_trace_stack = self.reference_trace_stack(slit_idx, offsets=offsets,
                                                          objid=self.objid_bri)
 
             thismask_stack = [np.abs(slitmask - self.stack_dict['slits_list'][0].spat_id[slit_idx]) <= self.par['coadd2d']['spat_toler'] for slitmask in self.stack_dict['slitmask_stack']]
@@ -484,9 +492,9 @@ class CoAdd2D:
             else:
                 weights = self.use_weights
                 
-            if self.angle_offsets and self.pypeline == 'Echelle':
-                self.offsets = np.round(self.offsets / self.spectrograph.order_platescale(self.spectrograph.orders,self.binning)[slit_idx]).astype(int)
-
+            # adjust offsets for sampling
+            offsets = np.round(offsets / self.spat_samp_fact).astype(int)
+                
             # Perform the 2d coadd
             # NOTE: mask_stack is a gpm, and this is called inmask_stack in
             # compute_coadd2d, and outmask in coadd_dict is also a gpm
@@ -501,9 +509,9 @@ class CoAdd2D:
                                                self.wave_grid, self.spat_samp_fact,
                                                maskdef_dict=maskdef_dict,
                                                weights=weights, interp_dspat=interp_dspat,
-                                               offsets=self.offsets, angle_offsets=self.angle_offsets)
+                                               offsets=offsets, angle_offsets=self.angle_offsets)
             coadd_list.append(coadd_dict)
-
+        
         return coadd_list
 
     def create_pseudo_image(self, coadd_list):
@@ -816,7 +824,7 @@ class CoAdd2D:
             :obj:`list`: A list of reference traces for the 2d coadding that
             have been offset.
         """
-        return [slits.center[:,slitid] - offsets[iexp]
+        return [slits.center[:,slitid] + offsets[iexp]
                     for iexp, slits in enumerate(self.stack_dict['slits_list'])]
 #        ref_trace_stack = []
 #        for iexp, slits in enumerate(self.stack_dict['slits_list']):
@@ -1003,8 +1011,11 @@ class CoAdd2D:
             if dithoffs[0] is None:
                 dithoffs = np.array([self.spectrograph.get_meta_value(f, 'dither') for f in self.spec2d])
                 if dithoffs[0] is None:
-                    msgs.error('Invalid header keyword for dithers (only: dithoff, dither)')
-            self.offsets = dithoffs[0]-dithoffs
+                    msgs.error('Invalid header keyword for dither offsets (only: dithoff, dither)')
+                else:
+                    self.offsets = -dithoffs
+            else:
+                self.offsets = dithoffs[0]-dithoffs
             self.offsets_report(self.offsets, 'header keyword')
             self.angle_offsets = True
 

pypeit/core/coadd.py
@@ -2945,7 +2945,7 @@ def compute_coadd2d(ref_trace_stack, sciimg_stack, sciivar_stack, skymodel_stack
     # Determine the wavelength grid that we will use for the current slit/order
     wave_bins = get_wave_bins(thismask_stack, waveimg_stack, wave_grid)
     dspat_bins, dspat_stack = get_spat_bins(thismask_stack, ref_trace_stack, spat_samp_fact=spat_samp_fact)
-
+    
     skysub_stack = [sciimg - skymodel for sciimg, skymodel in zip(sciimg_stack, skymodel_stack)]
     #sci_list = [weights_stack, sciimg_stack, skysub_stack, tilts_stack,
     #            waveimg_stack, dspat_stack]
@@ -2953,11 +2953,11 @@ def compute_coadd2d(ref_trace_stack, sciimg_stack, sciivar_stack, skymodel_stack
 
     var_list = [[utils.inverse(sciivar) for sciivar in sciivar_stack]]
 
-
     sci_list_rebin, var_list_rebin, norm_rebin_stack, nsmp_rebin_stack \
             = rebin2d(wave_bins, dspat_bins, waveimg_stack, dspat_stack, thismask_stack,
-                      inmask_stack, sci_list, var_list, offsets, angle_offsets)
-    #embed()
+                      inmask_stack, sci_list, var_list,
+                      offsets, angle_offsets, spat_samp_fact)
+    
     # Now compute the final stack with sigma clipping
     sigrej = 3.0
     maxiters = 10
@@ -3042,7 +3042,7 @@ def compute_coadd2d(ref_trace_stack, sciimg_stack, sciivar_stack, skymodel_stack
 
 def rebin2d(spec_bins, spat_bins, waveimg_stack, spatimg_stack,
             thismask_stack, inmask_stack, sci_list, var_list,
-            offsets, angle_offsets):
+            offsets, angle_offsets, spat_samp_fact):
     """
     Rebin a set of images and propagate variance onto a new spectral and spatial grid. This routine effectively
     "recitifies" images using np.histogram2d which is extremely fast and effectively performs
@@ -3124,29 +3124,31 @@ def rebin2d(spec_bins, spat_bins, waveimg_stack, spatimg_stack,
     for jj in range(len(var_list)):
         var_list_out.append(np.zeros(shape_out))
         
-    if angle_offsets is not True:
+    if angle_offsets:
+        pass
+    else:
         offsets = np.zeros(nimgs)
 
     for img, (waveimg, spatimg, thismask, inmask, offset) in enumerate(zip(waveimg_stack, spatimg_stack, thismask_stack, inmask_stack, offsets)):
 
         spec_rebin_this = waveimg[thismask]
         # Have to "cancel" the original offsets, which are along detector pixels
-        spat_rebin_this = spatimg[thismask]+offset
+        spat_rebin_this = spatimg[thismask]-offset
 
         # This fist image is purely for bookeeping purposes to determine the number of times each pixel
         # could have been sampled
         nsmp_rebin_stack[img, :, :], spec_edges, spat_edges = np.histogram2d(spec_rebin_this, spat_rebin_this,
                                                                bins=[spec_bins, spat_bins], density=False)
         # Apply the offset after rectification to operate in the true spatial direction
-        nsmp_rebin_stack[img, :, :] = np.roll(nsmp_rebin_stack[img, :, :], -offset, axis=1)
+        nsmp_rebin_stack[img, :, :] = np.roll(nsmp_rebin_stack[img, :, :], offset, axis=1)
 
         finmask = thismask & inmask
         spec_rebin = waveimg[finmask]
-        spat_rebin = spatimg[finmask]+offset
+        spat_rebin = spatimg[finmask]-offset
         norm_img, spec_edges, spat_edges = np.histogram2d(spec_rebin, spat_rebin,
                                                           bins=[spec_bins, spat_bins], density=False)
         norm_rebin_stack[img, :, :] = norm_img
-        norm_rebin_stack[img, :, :] = np.roll(norm_rebin_stack[img, :, :], -offset, axis=1)
+        norm_rebin_stack[img, :, :] = np.roll(norm_rebin_stack[img, :, :], offset, axis=1)
 
         # Rebin the science images
         for indx, sci in enumerate(sci_list):
@@ -3154,7 +3156,7 @@ def rebin2d(spec_bins, spat_bins, waveimg_stack, spatimg_stack,
                                                                bins=[spec_bins, spat_bins], density=False,
                                                                weights=sci[img][finmask])
             sci_list_out[indx][img, :, :] = (norm_img > 0.0) * weigh_sci/(norm_img + (norm_img == 0.0))
-            sci_list_out[indx][img, :, :] = np.roll(sci_list_out[indx][img, :, :], -offset, axis=1)
+            sci_list_out[indx][img, :, :] = np.roll(sci_list_out[indx][img, :, :], offset, axis=1)
 
         # Rebin the variance images, note the norm_img**2 factor for correct error propagation
         for indx, var in enumerate(var_list):
@@ -3162,6 +3164,7 @@ def rebin2d(spec_bins, spat_bins, waveimg_stack, spatimg_stack,
                                                                bins=[spec_bins, spat_bins], density=False,
                                                                weights=var[img][finmask])
             var_list_out[indx][img, :, :] = (norm_img > 0.0)*weigh_var/(norm_img + (norm_img == 0.0))**2
-            var_list_out[indx][img, :, :] = np.roll(var_list_out[indx][img, :, :], -offset, axis=1)
+            var_list_out[indx][img, :, :] = np.roll(var_list_out[indx][img, :, :], offset, axis=1)
+
 
     return sci_list_out, var_list_out, norm_rebin_stack.astype(int), nsmp_rebin_stack.astype(int)