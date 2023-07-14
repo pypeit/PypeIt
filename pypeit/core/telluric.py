6b64d5980f76b163907ed41bbfdaaa2c76046478
Frederick Davies <davies@mpia.de>
2023-07-14 15:05:04 +0200

    os not imported anymore

--- pypeit/core/telluric.py
+++ pypeit/core/telluric.py
@@ -167,9 +167,9 @@ def read_telluric_pca(filename, wave_min=None, wave_max=None, pad_frac=0.10):
     Returns:
         :obj:`dict`: Dictionary containing the telluric PCA components.
     """
-    # Check for file
-    if not os.path.isfile(filename):
-        msgs.error(f"File {filename} is not on your disk.  You likely need to download the Telluric files.  See https://pypeit.readthedocs.io/en/release/installing.html#atmospheric-model-grids")
+#    # Check for file
+#    if not os.path.isfile(filename):
+#        msgs.error(f"File {filename} is not on your disk.  You likely need to download the Telluric files.  See https://pypeit.readthedocs.io/en/release/installing.html#atmospheric-model-grids")
 
     hdul = fits.open(filename)
     wave_grid_full = hdul[1].data
