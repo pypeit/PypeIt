ced2a44adca2c50ce65c128465d85f9fe07e1e51
Frederick Davies <davies@mpia.de>
2023-07-28 14:00:09 +0200

    merging

--- pypeit/sensfunc.py
+++ pypeit/sensfunc.py
@@ -216,7 +216,6 @@ class SensFunc(datamodel.DataContainer):
         # spectrograph objects with configuration specific information from
         # spec1d files.
         self.spectrograph.dispname = header['DISPNAME']
-        
         self.par_fluxcalib = self.spectrograph.default_pypeit_par()['fluxcalib'] if par_fluxcalib is None else par_fluxcalib
 
         # Set the algorithm in the datamodel
