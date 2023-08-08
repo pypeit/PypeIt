109c28045dda3f02d07a714fd2f334f0bacbb508
Frederick Davies <davies@mpia.de>
2023-08-08 11:02:08 +0200

    cleanup

--- pypeit/spectrographs/soar_goodman.py
+++ pypeit/spectrographs/soar_goodman.py
@@ -527,7 +527,7 @@ class SOARGoodmanBlueSpectrograph(SOARGoodmanSpectrograph):
         par['scienceframe']['exprng'] = [90, None]
 
         # par['sensfunc']['algorithm'] = 'IR'
-        par['sensfunc']['IR']['telgridfile'] = 'TelFit_LasCampanas_3100_26100_R20000.fits'
+        par['sensfunc']['IR']['telgridfile'] = 'TellPCA_3000_26000_R15000.fits'
 
         return par
 
