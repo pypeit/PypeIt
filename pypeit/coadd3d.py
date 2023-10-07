fix DAR pressure units consistent

pypeit/coadd3d.py
@@ -210,7 +210,7 @@ class DARcorrection:
             airmass (:obj:`float`):
                 The airmass of the observations (unitless)
             parangle (:obj:`float`):
-                The parallactic angle of the observations (units=degree, relative to North, towards East is postive)
+                The parallactic angle of the observations (units=radians, relative to North, towards East is postive)
             pressure (:obj:`float`):
                 The atmospheric pressure during the observations in Pascal. Valid range is from 10kPa - 140 kPa.
             temperature (:obj:`float`):
@@ -269,7 +269,7 @@ class DARcorrection:
         #                                   self.humidity, self.wave_ref.to_value(units.micron))
         cnsa, cnsb = erfa.refco(self.pressure.to_value(units.hPa), self.temperature.to_value(units.deg_C),
                                 self.humidity, (waves*units.Angstrom).to_value(units.micron))
-        dar_full =  (180.0/np.pi) * ((self.refa-cnsa) * np.tan(z) + (self.refb-cnsb) * np.tan(z)**3)
+        dar_full = (180.0/np.pi) * ((self.refa-cnsa) * np.tan(z) + (self.refb-cnsb) * np.tan(z)**3)
         return dar_full
 
     def correction(self, waves):

pypeit/spectrographs/keck_kcwi.py
@@ -222,8 +222,8 @@ class KeckKCWIKCRMSpectrograph(spectrograph.Spectrograph):
                 return headarr[0]['WXPRESS']  # Must be in astropy.units.mbar
             except KeyError:
                 msgs.warn("Pressure is not in header")
-                msgs.info("The default pressure will be assumed: 0.611 mbar")
-                return 0.611
+                msgs.info("The default pressure will be assumed: 611 mbar")
+                return 611.0
         elif meta_key == 'temperature':
             try:
                 return headarr[0]['WXOUTTMP']  # Must be in astropy.units.deg_C
@@ -242,7 +242,7 @@ class KeckKCWIKCRMSpectrograph(spectrograph.Spectrograph):
         elif meta_key == 'parangle':
             try:
                 # Parallactic angle expressed in radians
-                return headarr[0]['PARANG'] * np.pi / 180
+                return headarr[0]['PARANG'] * np.pi / 180.0
             except KeyError:
                 msgs.error("Parallactic angle is not in header")
         elif meta_key == 'obstime':