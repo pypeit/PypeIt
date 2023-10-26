41398c0767818ff053b90700dfc538fd880a5a1f
rcooke <rcooke.ast@gmail.com>
2023-10-26 22:03:22 +0100

    docstring

--- pypeit/core/scattlight.py
+++ pypeit/core/scattlight.py
@@ -13,7 +13,7 @@ from IPython import embed
 from pypeit import msgs
 
 
-def scattered_light_model(param, img, kernel='gaussian'):
+def scattered_light_model(param, img):
     """ Model used to calculate the scattered light.
 
     The current model to generate the scattered light is a shifted, scaled, and blurred version of the
@@ -30,21 +30,24 @@ def scattered_light_model(param, img, kernel='gaussian'):
         subsequent parameters in this list are polynomial corrections to the spectral
         direction. Here are the individual parameters:
 
-        * param[0] = Kernel width in the spectral direction
-        * param[1] = Kernel width in the spatial direction
-        * param[2] = Pixel shift of the scattered light in the spectral direction
-        * param[3] = Pixel shift of the scattered light in the spatial direction
-        * param[4] = Zoom factor of the scattered light (~1)
-        * param[5:] = Polynomial scaling coefficients in the spectral direction
+        * param[0] = Gaussian kernel width in the spectral direction
+        * param[1] = Gaussian kernel width in the spatial direction
+        * param[2] = Lorentzian kernel width in the spectral direction
+        * param[3] = Lorentzian kernel width in the spatial direction
+        * param[4] = Pixel shift of the scattered light in the spectral direction
+        * param[5] = Pixel shift of the scattered light in the spatial direction
+        * param[6] = Zoom factor of the scattered light (~1)
+        * param[7] = Kernel angle
+        * param[8] = Relative importance of Gaussian vs Lorentzian.
+                     0 < value < 1 means Lorentzian is weighted more
+                     value > 1 means Gaussian is weighted more.
+        * param[9:] = Polynomial scaling coefficients
     img : `numpy.ndarray`_
         Raw image that you want to compute the scattered light model.
         shape is (nspec, nspat)
         Model used to calculate the scattered light. This function is used to
         generate a model of the scattered light, based on a set of model parameters
         that have been optimized using self.scattered_light().
-    kernel : :obj:`str`_, optional
-        The shape of the kernel to use. The allowed values are 'gaussian' and 'lorentzian',
-        but the default value ('gaussian') is currently believed to provide the best fit.
 
     Returns
     -------
@@ -52,37 +55,74 @@ def scattered_light_model(param, img, kernel='gaussian'):
         Model of the scattered light for the input
     """
     # Extract the parameters into more conveniently named variables
-    sigmx, sigmy, shft_spec, shft_spat, zoom = param[0], param[1], param[2], param[3], param[4]
-    polyterms = param[5:]
-    # Generate a 2D smoothing kernel
-    if kernel == 'gaussian':
-        # Gaussian
-        subkrnx = np.exp(-0.5 * (np.arange(int(6 * sigmx)) - 3 * sigmx) ** 2 / sigmx ** 2)
-        subkrny = np.exp(-0.5 * (np.arange(int(6 * sigmy)) - 3 * sigmy) ** 2 / sigmy ** 2)
-    elif kernel == 'lorentzian':
-        # Lorentzian
-        subkrnx = sigmx / ((np.arange(int(10 * sigmx)) - 5 * sigmx) ** 2 + sigmx ** 2)
-        subkrny = sigmy / ((np.arange(int(10 * sigmy)) - 5 * sigmy) ** 2 + sigmy ** 2)
-    else:
-        msgs.error(f"Unknown kernel: {kernel}")
-    kernel = np.outer(subkrnx, subkrny)
-    kernel /= np.sum(kernel)
+    sigmx_g, sigmy_g, sigmx_l, sigmy_l = param[0], param[1], param[2], param[3]
+    shft_spec, shft_spat, zoom = param[4], param[5], param[6]
+    kern_angle, kern_scale = param[7], param[8]
+    polyterms = param[9:]
+
     # Make a grid of coordinates
     specvec, spatvec = np.arange(img.shape[0]), np.arange(img.shape[1])
-    spat, spec = np.meshgrid(spatvec, specvec / (specvec.size - 1))
+    spat, spec = np.meshgrid(spatvec/(spatvec.size-1), specvec/(specvec.size - 1))
     # Generate the polynomial efficiency scaling in the spectral direction
-    polyscale = np.zeros_like(img)
-    for pp in range(polyterms.size):
-        polyscale += polyterms[pp] * spec ** pp
+    polyscale = polyterms[0] + polyterms[1]*spec + polyterms[2]*spat + polyterms[3]*spec*spat
+    # polyscale = np.zeros_like(img)
+    # for pp in range(polyterms_spec.size):
+    #     polyscale += polyterms_spec[pp] * spec ** pp
+    # for pp in range(polyterms_spat.size):
+    #     polyscale += polyterms_spat[pp] * spat ** pp
+
+    # Generate a 2D smoothing kernel, composed of a 2D Gaussian and a 2D Lorentzian
+    sigmx, sigmy = max(sigmx_g, sigmx_l), max(sigmy_g, sigmy_l),
+    xkern, ykern = np.meshgrid(np.arange(int(10 * sigmx)) - 5 * sigmx,
+                               np.arange(int(10 * sigmy)) - 5 * sigmy)
+    # Rotate the kernel
+    xkernrot = (xkern * np.cos(kern_angle) - ykern * np.sin(kern_angle))
+    ykernrot = (xkern * np.sin(kern_angle) + ykern * np.cos(kern_angle))
+    # Create and normalise the Gaussian kernel
+    kernel_gaussian = np.exp(-((xkernrot/sigmx_g)**2 + (ykernrot/sigmy_g)**2))
+    kernel_gaussian /= np.sum(kernel_gaussian)
+    # Create and normalise the Lorenztian kernel
+    kernel_lorentzian = 1 / ((xkernrot/sigmx_l) ** 2 + (ykernrot/sigmy_l) ** 2 + 1)
+    kernel_lorentzian /= np.sum(kernel_lorentzian)
+    # Add the individual kernels into a single kernel. Arbitrarily scale the Gaussian kernel (either is fine).
+    # The point of this is to make it so either a lorentzian, gaussian, or something in-between can be
+    # used as the kernel, making it more flexible for different spectrographs.
+    kernel = kernel_lorentzian + kern_scale * kernel_gaussian
+    kernel /= np.sum(kernel)
+
     # Convolve the input image (note: most of the time is spent here)
     # oaconvolve is the fastest option when the kernel is much smaller dimensions than the image
-    # scale_img = polyscale * signal.fftconvolve(img, kernel, mode='same')
-    scale_img = polyscale * signal.oaconvolve(img, kernel, mode='same')
+    scale_img = polyscale * signal.fftconvolve(img, kernel, mode='same')
+    # scale_img = polyscale * signal.oaconvolve(img, kernel, mode='same')
     spl = interpolate.RectBivariateSpline(specvec, spatvec, scale_img, kx=1, ky=1)
     return spl(zoom * (specvec + shft_spec), zoom * (spatvec + shft_spat))
 
 
-def scattlight_resid(param, wpix, img):
+def evaluate(param, frame, detpad=300):
+    """ Evaluate the scattered light model, allowing for some image padding to minimise edge effects
+
+    Parameters
+    ----------
+    param : `numpy.ndarray`_
+        Model parameters that determine the scattered light based on the input img.
+        See `scattered_light_model()`_
+    img : `numpy.ndarray`_
+        Raw image that you want to compute the scattered light model.
+        shape is (nspec, nspat)
+
+    Returns
+    -------
+    model : `numpy.ndarray`_
+        Model of the scattered light for the input
+    """
+    _frame = frame.copy()
+    _frame[0, :] = np.median(_frame[0:10, :], axis=0)
+    _frame[-1, :] = np.median(_frame[-10:, :], axis=0)
+    img = np.pad(_frame, detpad, mode='edge')  # Model should be generated on padded data
+    return scattered_light_model(param, img)[detpad:-detpad, detpad:-detpad]
+
+
+def scattlight_resid(param, wpix, img, modimg):
     """ Residual function used to optimize the model parameters
 
     Parameters
@@ -100,7 +140,7 @@ def scattlight_resid(param, wpix, img):
     resid : `numpy.ndarray`_
         A 1D vector of the residuals
     """
-    model = scattered_light_model(param, img)
+    model = scattered_light_model(param, modimg)
     return img[wpix] - model[wpix]
 
 
@@ -137,12 +177,7 @@ def scattered_light(frame, bpm, offslitmask, x0, bounds, detpad=300, debug=False
     success : :obj:`bool`_
         True if the fit was successful, False otherwise
     """
-
-    # Grab a copy of the input frame, and do some pre-processing on it
     _frame = frame.copy()
-
-    # First pad the edges to minimize edge effects
-    # Do a median filter near the edges
     _frame[0, :] = np.median(_frame[0:10, :], axis=0)
     _frame[-1, :] = np.median(_frame[-10:, :], axis=0)
     img = np.pad(_frame, detpad, mode='edge')  # Model should be generated on padded data
@@ -151,30 +186,49 @@ def scattered_light(frame, bpm, offslitmask, x0, bounds, detpad=300, debug=False
     # Grab the pixels to be included in the fit
     wpix = np.where(offslitmask_pad)
 
-    # Compute the best-fitting model parameters
-    msgs.info("Computing best-fitting model parameters of the scattered light")
-    res_lsq = least_squares(scattlight_resid, x0, bounds=bounds, args=(wpix, img), verbose=2, ftol=1.0E-4)
+    # Iterate on the scattered light
+    scatt_img = 0.0
+    niter = 1
+    for ii in range(niter):
+        # Grab a copy of the input frame, and do some pre-processing on it
+        _frame = frame.copy()-scatt_img
 
-    # Store if this is a successful fit
-    success = res_lsq.success
-    if success:
-        msgs.info("Generating best-fitting scattered light model")
-        scatt_img = scattered_light_model(res_lsq.x, img)[detpad:-detpad, detpad:-detpad]
-    else:
-        msgs.warn("Scattered light model fitting failed")
-        scatt_img = np.zeros_like(_frame)
+        # First pad the edges to minimize edge effects
+        # Do a median filter near the edges
+        _frame[0, :] = np.median(_frame[0:10, :], axis=0)
+        _frame[-1, :] = np.median(_frame[-10:, :], axis=0)
+        modimg = np.pad(_frame, detpad, mode='edge')  # Model should be generated on padded data
+
+        # Compute the best-fitting model parameters
+        msgs.info(f"ITERATION {ii+1}: Computing best-fitting model parameters of the scattered light")
+        res_lsq = least_squares(scattlight_resid, x0, bounds=bounds, args=(wpix, img, modimg),
+                                verbose=2, ftol=1.0E-4)
+
+        # Store if this is a successful fit
+        success = res_lsq.success
+        if success:
+            msgs.info("Generating best-fitting scattered light model")
+            scatt_img = scattered_light_model(res_lsq.x, modimg)[detpad:-detpad, detpad:-detpad]
+        else:
+            msgs.warn("Scattered light model fitting failed")
+            scatt_img = np.zeros_like(_frame)
+            break
     if debug:
         # Do some checks on the results
         embed()
-        from matplotlib import pyplot as plt
         scatt_img_alt = scattered_light_model(x0, img)[detpad:-detpad, detpad:-detpad]
-        vmin, vmax = 0, np.max(scatt_img_alt)
+        from matplotlib import pyplot as plt
+        vmin, vmax = 0, 40.0#np.max(scatt_img_alt)
+        plt.imshow(frame - scatt_img, vmin=-vmax/2, vmax=vmax/2)
+        plt.show()
+        print(res_lsq.x)
+
         plt.subplot(231)
         plt.imshow(_frame, vmin=vmin, vmax=vmax)
         plt.subplot(232)
         plt.imshow(scatt_img, vmin=vmin, vmax=vmax)
         plt.subplot(233)
-        plt.imshow(_frame - scatt_img, vmin=-vmax/2, vmax=vmax/2)
+        plt.imshow(frame - scatt_img, vmin=-vmax/2, vmax=vmax/2)
         plt.subplot(234)
         plt.imshow(_frame, vmin=vmin, vmax=vmax)
         plt.subplot(235)
