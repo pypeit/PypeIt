Got slit masking to work with alignframe. Fixed a slitspatnum bug.

pypeit/core/procimg.py
@@ -816,7 +816,10 @@ def subtract_pattern(rawframe, datasec_img, oscansec_img, frequency=None, axis=1
             sgnl = overscan[ii,:]
             LSfreq, power = LombScargle(pixels, sgnl).autopower(minimum_frequency=use_fr*(1-100/frame_orig.shape[1]), maximum_frequency=use_fr*(1+100/frame_orig.shape[1]), samples_per_peak=10)
             bst = np.argmax(power)
-            cc = np.polyfit(LSfreq[bst-2:bst+3],power[bst-2:bst+3],2)
+            imin = np.clip(bst-2,0,None)
+            imax = np.clip(bst+3,None,overscan.shape[1])
+            # TODO is this correct??
+            cc = np.polyfit(LSfreq[imin:imax],power[imin:imax],2)
             all_freq[ii] = -0.5*cc[1]/cc[0]
         cc = np.polyfit(all_rows, all_freq, 1)
         frq_mod = np.polyval(cc, all_rows) * (overscan.shape[1]-1)