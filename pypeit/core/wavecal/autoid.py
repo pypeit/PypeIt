242f16c48b8ef00d92598a1b3164524d0fff716a
rcooke <rcooke.ast@gmail.com>
2023-12-30 17:01:53 +0000

    reduce FWHM slit range

--- pypeit/core/wavecal/autoid.py
+++ pypeit/core/wavecal/autoid.py
@@ -913,7 +913,7 @@ def map_fwhm(image, gpm, slits_left, slits_right, slitmask, npixel=None, nsample
         msgs.info(f"Calculating spectral resolution of slit {sl + 1}/{nslits}")
         # Fraction along the slit in the spatial direction to sample the arc line width
         nmeas = int(0.5+slit_lengths[sl]/_npixel) if nsample is None else nsample
-        slitsamp = np.linspace(0.01, 0.99, nmeas)
+        slitsamp = np.linspace(0.05, 0.95, nmeas)
         this_samp, this_cent, this_fwhm = np.array([]), np.array([]), np.array([])
         for ss in range(nmeas):
             spat_vec = np.atleast_2d((1-slitsamp[ss]) * slits_left[:, sl] + slitsamp[ss] * slits_right[:, sl]).T
