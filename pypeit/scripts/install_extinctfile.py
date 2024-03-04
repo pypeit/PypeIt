1f293e8292f17627cec449d27f2160d7879fd2b2
Kyle Westfall <westfall@ucolick.org>
2024-03-04 14:13:38 -0800

    bug fixes

--- pypeit/scripts/install_extinctfile.py
+++ pypeit/scripts/install_extinctfile.py
@@ -25,9 +25,10 @@ class InstallExtinctfile(scriptbase.ScriptBase):
     def main(args):
         import os
         import numpy as np
+        from pypeit import msgs
 
         # Grab all the files
-        files = np.concatenate([sorted(self.expandpath(f)) for f in args.files])
+        files = np.concatenate([sorted(scriptbase.ScriptBase.expandpath(f)) for f in args.files])
         # Remove any that are *not* files (i.e., directories or symlinks)
         files = [f for f in files if f.is_file()]
 
--- pypeit/scripts/install_linelist.py
+++ pypeit/scripts/install_linelist.py
@@ -24,9 +24,10 @@ class InstallLinelist(scriptbase.ScriptBase):
     def main(args):
         import os
         import numpy as np
+        from pypeit import msgs
 
         # Grab all the files
-        files = np.concatenate([sorted(self.expandpath(f)) for f in args.files])
+        files = np.concatenate([sorted(scriptbase.ScriptBase.expandpath(f)) for f in args.files])
         # Remove any that are *not* files (i.e., directories or symlinks)
         files = [f for f in files if f.is_file()]
 
