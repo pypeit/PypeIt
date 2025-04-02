106dd78dc3083b2d38f9c781fc5cfc285d313551
Max Brodheim <max.brodheim@gmail.com>
2025-04-02 11:33:45 -1000

    more logging

--- pypeit/display/ginga_qlview.py
+++ pypeit/display/ginga_qlview.py
@@ -512,17 +512,18 @@ class QLView(GingaPlugin.LocalPlugin):
         self.logger.debug(f"Render Slits called with reduced path {self.reduced_filepath}")
 
         self.slittracesets = self.open_slits_files(os.path.join(self.reduced_filepath, "Calibrations"))
-        self.slit_canvas = self.construct_slits(self.slittracesets)
         
         if self.fitsimage.get_canvas() is not None:
             if self.display_slits_box.get_state():
                 try:
-                    self.logger.info("Removing old slits")
+                    self.logger.info("Trying to remove old slits")
                     tag = self.fitsimage.get_canvas().lookup_object_tag(self.slit_canvas)
                     self.fitsimage.get_canvas().delete_object_by_tag(tag)
+                    self.logger.info("Removed old slits (I think...)")
                 except KeyError:
                     self.logger.info("No slits found to remove")
-                self.fitsimage.get_canvas().add(self.slit_canvas)
+            self.slit_canvas = self.construct_slits(self.slittracesets)
+            self.fitsimage.get_canvas().add(self.slit_canvas)
         else:
             self.logger.error("No image has been rendered yet. Cannot add slits.")
 
