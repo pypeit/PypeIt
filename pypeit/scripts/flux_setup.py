cdcb098129eb2966e81097e36c79746d35e941a4
Dusty Reichwein <dusty@ucolick.org>
2025-05-12 22:49:17 -0700

    First stab at a new flux_setup.py for ADAP hires fluxing/coadding

--- pypeit/scripts/flux_setup.py
+++ pypeit/scripts/flux_setup.py
@@ -7,10 +7,13 @@ Setup files for flux calibration.
 
 import os
 from pathlib import Path
+from datetime import datetime, timedelta
 
 import numpy as np
 
 from astropy.table import Table
+from astropy.time import Time
+
 
 from pypeit import msgs
 from pypeit import io
@@ -18,33 +21,109 @@ from pypeit.scripts import scriptbase
 from pypeit import inputfiles
 from pypeit.spectrographs.util import load_spectrograph
 
-def match_spec1ds_to_sensfuncs(spectrograph_name, spec1dfiles, sensfiles):
-    """
-    This needs a docstring
-    """
-    result_map = {}
-    spectrograph = load_spectrograph(spectrograph_name)
 
-    # Read configurations of each sensfile
+def parse_date_from_filename(path):
+    name_wo_ext = path.stem
+    datetime_str = name_wo_ext.split("_")[-1]
+    try:
+        result = datetime.fromisoformat(datetime_str)
+    except Exception as e:
+        msgs.warn(f"Failed to parse date from {path.name}")
+        raise
+    return result
+
+def read_config_from_pypeit_fits(spectrograph, pypeit_fits_file):
+
+    with io.fits_open(pypeit_fits_file) as hdul:
+        header = hdul[0].header
+        return {config_key: header[config_key] for config_key in spectrograph.configuration_keys() if config_key in header}
+
+
+
+def read_metadata(spectrograph, pypeit_file_names, spec1dfiles):
+
+    colnames = ['dirname', 'filename', 'frametype', 'mjd', 'setup_id'] + spectrograph.configuration_keys()
+    data = []
+
+    pypeit_files = [inputfiles.PypeItFile.from_file(file) for file in pypeit_file_names]
+
+    # Find the PypeIt files for these spec1ds
+    for spec1dfile in spec1dfiles:
+        found = False
+        for i, pf in enumerate(pypeit_files):
+            for row in pf.data:
+                if Path(row['filename']).stem in spec1dfile.name:
+                    spec1d_config = read_config_from_pypeit_fits(spectrograph, spec1dfile)
+                    data.append([str(spec1dfile.parent),spec1dfile.name,str(row['frametype']),str(row['mjd']), chr(ord('A') + i)] + [spec1d_config[key] for key in spectrograph.configuration_keys()])
+                    found = True
+                    break
+            if found:
+                break
+        if not found:
+            raise ValueError(f"Failed to find .pypeit file metadata for {spec1dfile.name}")
+
+    metadata_table = Table(rows=data, names=colnames)
+    return metadata_table
+
+def match_spec1ds_to_sensfuncs(args, spectrograph, metadata, sensfiles):
+    """
+    Match sensitivity functions to spec1d files for fluxing.
+
+    Args:
+        spectrograph_name (str): Name of the spectrograph being used
+        spec1dfiles (list of Path): list of names of spec1d files to match to sensitivity function files.
+        specobjs_list (list of :obj:`pypeit.specobjs.SpecObjs`): List of SpecObjs objects corresponding to each spec1d file.
+        sensfiles (list of Path): Lis tof names of sensitivity funciton files to match to spec1d files.
+
+    Return (list of tuple):
+        A 1-to-1 map of spec1d filenames to sensitivity function files. Each file is returend as a tuple of its
+        filename and setup_id. If a sensitivity function file could not be found for a spec1d file, that spec1d file
+        is not returned in the results and a warning is logged.
+    """
+    spec1d_to_sensfile = {}
+    # Read observing configurations of each sensfunc file
     sens_configs = []
     for sensfile in sensfiles:
-        with io.fits_open(sensfile) as hdul:
-            header = hdul[0].header
+        sens_configs.append(read_config_from_pypeit_fits(spectrograph, sensfile))
 
-        sens_configs.append({config_key: header[config_key] for config_key in spectrograph.configuration_keys() if config_key in header})
+    # Read the configurations of each spec1d file, and try to find a matching sensfunc file.
+    for metadata_row in metadata:
 
-    # Read the configurations of each spec1d file, and try to find a matching sensfunc file
-    for spec1dfile in spec1dfiles:        
-        with io.fits_open(spec1dfile) as hdul:
-            header = hdul[0].header
+        if args.skip_standards:
+            if metadata_row['frametype'] == 'standard':
+                continue
 
-        spec1d_config = {config_key: header[config_key] for config_key in spectrograph.configuration_keys() if config_key in header}
-        for i, sens_config in enumerate(sens_configs):
+        matching_sensfiles = []
+        spec1d_config = {config_key: metadata_row[config_key] for config_key in spectrograph.configuration_keys() if config_key in metadata.colnames}
+
+        # Go through all of the sensfunc configs and find a match for the spec1d
+        for j, sens_config in enumerate(sens_configs):
+
             if spectrograph.same_configuration([spec1d_config, sens_config],check_keys=False):
-                result_map[spec1dfile.name] = sensfiles[i]
-                break
+                matching_sensfiles.append(sensfiles[j])
+                
+        if len(matching_sensfiles) == 0:
+            msgs.warn(f"{metadata_row['filename']} does not have any matching sensitivity functions.")
+        elif len(matching_sensfiles) == 1:
+            spec1d_to_sensfile[metadata_row['filename']] = matching_sensfiles[0]
+        else:
+            # Pick between multiple sensitivity functions by using the closest date
+            
+            try:
+                # Convert mjd from metadata to a python datetime
+                spec1d_date = Time(metadata_row['mjd'],format='mjd').to_datetime()
+            
+                # Get time differences between the dates of each each sens file and the spec1d file
+                time_differences = [(sensfile, abs(parse_date_from_filename(sensfile)-spec1d_date)) for sensfile in matching_sensfiles]
+                
+                # Select the file with the smallest time difference
+                spec1d_to_sensfile[metadata_row['filename']] = min(time_differences, key=lambda x: x[1])[0]
 
-    return result_map
+            except Exception as e:
+                msgs.warn(f"Could not compare sensfunc files by date. Using first found sensfile for {metadata_row['filename']}")
+                spec1d_to_sensfile[metadata_row['filename']] = matching_sensfiles[0]
+            
+    return spec1d_to_sensfile
 
 class FluxSetup(scriptbase.ScriptBase):
 
@@ -53,7 +132,10 @@ class FluxSetup(scriptbase.ScriptBase):
         parser = super().get_parser(description='Setup configuration files to perform flux calibration, 1D coadding, and telluric correction.',
                                     width=width, formatter=scriptbase.SmartFormatter)
         parser.add_argument("paths", type=str, nargs='+', help="One or more paths for Science folders or sensitivity functions. Sensitivity functions must start with 'sens_' to be detected.")
+        parser.add_argument("--recursive","-r", default=False, action="store_true", help="Whether to recursively search subdirectories of the given paths.")
         parser.add_argument("--name", type=str, default=None, help="The base name to use for the output files. Defaults to the instrument name is used.")
+        parser.add_argument("--coadd_output", "-o", type=str, default="YOUR_OUTPUT_FILE_NAME", help="Output file for the 1D coadfding, placed into the coadd1d configuration file.")
+        parser.add_argument("--skip_standards", default=False, action="store_true", help="If set, the flux calibration file will not contain any standard star observations.")
         parser.add_argument("--objmodel", type=str, default='qso', choices=['qso', 'star', 'poly'],
                             help='R|science object model used in the telluric fitting. The '
                                  'options are:\n\n'
@@ -77,27 +159,35 @@ class FluxSetup(scriptbase.ScriptBase):
         spectrograph name but can be overriden on the command line.
 
         """
-        allfiles = []
+        allpaths = []
         for path in args.paths:
-            allfiles += Path(path).iterdir()
+            allpaths.append(Path(path))
         spec1dfiles = []
         spec2dfiles = []
         spec1dinfos = []
+        pypeit_file_names = []
         unique_paths = set()
         sensfiles = []
-        for ifile in allfiles:
-            if ('spec1d' in ifile.name) and ('.fits' in ifile.name):
-                spec1dfiles.append(ifile)
-                unique_paths.add(str(ifile.parent))
-            elif ('spec2d' in ifile.name) and ('.fits' in ifile.name):
-                spec2dfiles.append(ifile)
-            elif ('spec1d' in ifile.name) and ('.txt' in ifile.name):
-                spec1dinfos.append(ifile)
-            elif ifile.name.startswith('sens_') and ('.fits' in ifile.name):
-                sensfiles.append(ifile)
-                unique_paths.add(str(ifile.parent))
-            else:
-                msgs.info('{:} is not a standard PypeIt output, skipping.'.format(ifile))
+        for path in allpaths:
+            for ifile in path.iterdir():
+                if ifile.is_dir():
+                    if args.recursive:
+                        allpaths.append(ifile)
+                elif ifile.name.startswith('spec1d') and ifile.name.endswith('.fits'):
+                    spec1dfiles.append(ifile)
+                    unique_paths.add(str(ifile.parent))
+                elif ifile.name.startswith('spec2d') and ifile.name.endswith('.fits'):
+                    spec2dfiles.append(ifile)
+                elif ifile.name.startswith('spec1d') and ifile.name.endswith('.txt'):
+                    spec1dinfos.append(ifile)
+                elif ifile.name.startswith('sens_') and ('.fits' in ifile.name):
+                    sensfiles.append(ifile)
+                    unique_paths.add(str(ifile.parent))
+                elif ifile.suffix == ".pypeit":
+                    pypeit_file_names.append(ifile)
+                else:
+                    msgs.info('{:} is not a standard PypeIt output, skipping.'.format(ifile))
+
         if len(spec2dfiles) > len(spec1dfiles):
             msgs.warn('The following exposures do not have 1D extractions:')
             for ii in range(len(spec2dfiles)):
@@ -105,19 +195,22 @@ class FluxSetup(scriptbase.ScriptBase):
                     msgs.info('\t {:}'.format(spec2dfiles[ii]))
 
         if len(spec1dfiles) > 0:
-            with io.fits_open(spec1dfiles[0]) as hdul:
+            with io.fits_open(str(spec1dfiles[0])) as hdul:
+                spectrograph_name = hdul[0].header['PYP_SPEC']
+            spectrograph = load_spectrograph(spectrograph_name)
+            pypeline = spectrograph.pypeline
 
-                # Get basic configuration info from first spec1d
-                spectrograph = hdul[0].header['PYP_SPEC']
-                pypeline = hdul[0].header['PYPELINE']
+            metadata_table = read_metadata(spectrograph, pypeit_file_names, spec1dfiles)
+           
+            # Get basic configuration info from first spec1d
 
             if args.name is None:
-                output_basename = spectrograph
+                output_basename = spectrograph_name
             else:
                 output_basename = args.name            
 
             # Determine how to map sensitivity functions to spec1d files
-            sensfile_mapping=match_spec1ds_to_sensfuncs(spectrograph, spec1dfiles, sensfiles)
+            spec1d_to_sensfile_map=match_spec1ds_to_sensfuncs(args, spectrograph, metadata_table, sensfiles)
 
             ## fluxing pypeit file
             # Build the bits and pieces
@@ -125,8 +218,9 @@ class FluxSetup(scriptbase.ScriptBase):
             cfg_lines += ['  extinct_correct = False # Set to True if your SENSFUNC derived with the UVIS algorithm\n']
             cfg_lines += ['# Please add your SENSFUNC file name below before running pypeit_flux_calib']
             data = Table()
-            data['filename'] = [x.name for x in spec1dfiles]
-            data['sensfile'] = ['' if x.name not in sensfile_mapping else sensfile_mapping[x.name].name for x in spec1dfiles]
+            # Data list, excluding standard stars if requested and using a blank sensfile name if no sensfile was found
+            data['filename'] = [metadata_row['filename'] for metadata_row in metadata_table if (args.skip_standards is False or metadata_row['frametype']=='science')]
+            data['sensfile'] = ['' if spec1dname not in spec1d_to_sensfile_map else spec1d_to_sensfile_map[spec1dname].name for spec1dname in data['filename']]
 
             # Instantiate
             fluxFile = inputfiles.FluxFile(
@@ -139,7 +233,7 @@ class FluxSetup(scriptbase.ScriptBase):
 
             ## coadd1d pypeit file
             cfg_lines = ['[coadd1d]']
-            cfg_lines += ['  coaddfile = YOUR_OUTPUT_FILE_NAME # Please set your output file name']
+            cfg_lines += [f'  coaddfile = {args.coadd_output} # Please set your output file name']
             if pypeline == 'Echelle':
                 cfg_lines += ['  wave_method = velocity # creates a uniformly space grid in log10(lambda)\n']
             else:
@@ -151,50 +245,47 @@ class FluxSetup(scriptbase.ScriptBase):
                 cfg_lines += ['# For Echelle spectrographs, please double check the sensfunc file and setup id\n']
 
 
-            all_specfiles, all_obj = [], []
-            for ii in range(len(spec1dfiles)):
-                txtinfofile = spec1dfiles[ii].parent / (spec1dfiles[ii].stem + ".txt")
+            all_specfiles, all_obj, all_setup_ids = [], [], []
+            for metadata_row in metadata_table:
+                if metadata_row['frametype'] == 'standard':
+                    # Skip coadding standard frames
+                    continue
+                txtinfofile = Path(metadata_row['dirname'], Path(metadata_row['filename']).stem + ".txt")
                 meta_tbl = Table.read(txtinfofile,
                                       format='ascii.fixed_width')
                 _, indx = np.unique(meta_tbl['name'],return_index=True)
                 objects = meta_tbl[indx]
                 for jj in range(len(objects)):
-                    all_specfiles.append(spec1dfiles[ii])
+                    all_specfiles.append(metadata_row['filename'])
+                    all_setup_ids.append(metadata_row['setup_id'])
                     all_obj.append(objects['name'][jj])
             data = Table()
-            data['filename'] = [str(x.name) for x in all_specfiles]
+            data['filename'] = all_specfiles
             data['obj_id'] = all_obj
             if pypeline == 'Echelle':
 
                 if len(sensfiles) > 1:
                     # If there are multiple sensfunc files, try to set sensibile values
-                    # for the 'sensfile' and 'setup_id' columns
+                    # for the 'sensfile' column
                     all_sensfiles = []
-                    all_setup_ids = []
                     for spec1d in data['filename']:
-                        if spec1d in sensfile_mapping:
-                            sensfile = sensfile_mapping[spec1d]
-                            # Use the index of the sensfile in sensfiles as the setup id,
-                            # converted to a letter. Hopefully we don't get more than 26
-                            setup_id = chr(ord('A') + sensfiles.index(sensfile))
+                        if spec1d in spec1d_to_sensfile_map:
+                            sensfile = spec1d_to_sensfile_map[spec1d]
                         else:
-                            sensfile = 'SENSFUNC FILE'
-                            setup_id = 'default'
+                            sensfile = Path('SENSFUNC FILE')
 
                         all_sensfiles.append(sensfile.name)
-                        all_setup_ids.append(setup_id)
                     data['sensfile'] = all_sensfiles
                     data['setup_id'] = all_setup_ids
 
                 else:
-                    # Just use one default sensfunc file and one setup.
+                    # Just use one default sensfunc file
                     if len(sensfiles) == 1:
                         default_sensfile = sensfiles[0].name # Use the first sensfunc and only sensfunc
                     else:
                         default_sensfile = 'SENSFUNC FILE' # Use a dummy sensfunc filename
 
                     data['sensfile'] = [default_sensfile] + ([''] * (len(all_obj)-1))
-                    data['setup_id'] = ['A'] + ([''] * (len(all_obj)-1))
 
 
                 
