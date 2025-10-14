1f86eed3ce2b61834660b55d0ab231979fa6b42f
Timothy Ellsworth-Bowers <tbowers@lowell.edu>
2025-10-14 14:32:28 -0700

    Add the spectrograph tables
    	modified:   doc/include/spectrographs_table.rst
    	modified:   doc/scripts/build_spectbl_rst.py

--- doc/scripts/build_spectbl_rst.py
+++ doc/scripts/build_spectbl_rst.py
@@ -4,9 +4,9 @@ Dynamically build table listing available spectrographs.
 
 from importlib import resources
 
-import numpy
+import astropy.table
 
-from pypeit.utils import to_string, string_table
+from pypeit.utils import to_string
 from pypeit.spectrographs import spectrograph_classes
 
 from IPython import embed
@@ -16,26 +16,26 @@ from IPython import embed
 def write_spec_table(path):
     ofile = path / 'spectrographs_table.rst'
 
-    spec = spectrograph_classes()
-    nspec = len(spec.keys())
+    data_table = []
+    for cls in spectrograph_classes().values():
 
-    data_table = numpy.empty((nspec+1, 9), dtype=object)
-    data_table[0,:] = ['``PypeIt`` Name', '``PypeIt`` Class', 'Telescope', 'Camera', 'URL',
-                       'Pipeline', 'Supported', 'QL Tested', 'Comments']
-    for i,cls in enumerate(spec.values()):
-        data_table[i+1,0] = cls.name
-        data_table[i+1,1] = ':class:`~' + cls.__module__ + '.' + cls.__name__ + '`'
-        data_table[i+1,2] = cls.telescope['name']
-        data_table[i+1,3] = cls.camera
-        data_table[i+1,4] = '' if cls.url is None else f'`Link <{cls.url}>`__'
-        data_table[i+1,5] = cls.pypeline
-        data_table[i+1,6] = to_string(cls.supported)
-        data_table[i+1,7] = to_string(cls.ql_supported)
-        data_table[i+1,8] = '' if cls.comment is None else cls.comment
+        data_table.append(
+            {
+                '``PypeIt`` Name': cls.name,
+                '``PypeIt`` Class': ':class:`~' + cls.__module__ + '.' + cls.__name__ + '`',
+                'Telescope': cls.telescope['name'],
+                'Camera': cls.camera,
+                'URL': '' if cls.url is None else f'`Link <{cls.url}>`__',
+                'Pipeline': cls.pypeline,
+                'Supported': to_string(cls.supported),
+                'QL Tested': to_string(cls.ql_supported),
+                'Comments': '' if cls.comment is None else cls.comment
+            }
+        )
 
-    lines = string_table(data_table, delimeter='rst')
-    with open(ofile, 'w') as f:
-        f.write(lines)
+    tbl = astropy.table.Table(data_table)
+
+    tbl.write(ofile, format="ascii.rst", overwrite=True)
     print(f'Wrote: {ofile}')
 
 
