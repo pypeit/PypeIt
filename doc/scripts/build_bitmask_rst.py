ad4c0157be945217316460d142ac522fb4743002
Timothy Ellsworth-Bowers <tbowers@lowell.edu>
2025-10-14 15:03:16 -0700

    Add bitmask table
    	modified:   doc/include/imagebitmask_table.rst
    	modified:   doc/scripts/build_bitmask_rst.py

--- doc/scripts/build_bitmask_rst.py
+++ doc/scripts/build_bitmask_rst.py
@@ -4,9 +4,9 @@ Dynamically build tables for the bitmasks.
 
 from importlib import resources
 
-import numpy
+import astropy.table
 
-from pypeit.utils import to_string, string_table
+from pypeit.utils import to_string
 
 from IPython import embed
 
@@ -16,17 +16,20 @@ def write_bitmask_table(obj, path):
     bm = obj()
     ofile = path / f'{obj.__name__.lower()}_table.rst'
 
-    data_table = numpy.empty((bm.nbits+1, 4), dtype=object)
-    data_table[0,:] = ['Bit Name', 'Bit Number', 'Decimal Value', 'Description']
-    for i,k in enumerate(bm.bits.keys()):
-        data_table[i+1,0] = k
-        data_table[i+1,1] = to_string(bm.bits[k])
-        data_table[i+1,2] = to_string(int(2**bm.bits[k]))
-        data_table[i+1,3] = to_string(bm.descr[bm.bits[k]])
+    data_table = []
+    for k in bm.bits.keys():
+        data_table.append(
+            {
+                'Bit Name': k,
+                'Bit Number': to_string(bm.bits[k]),
+                'Decimal Value': to_string(int(2**bm.bits[k])),
+                'Description': to_string(bm.descr[bm.bits[k]]),
+            }
+        )
 
-    lines = string_table(data_table, delimeter='rst')
-    with open(ofile, 'w') as f:
-        f.write(lines)
+    tbl = astropy.table.Table(data_table)
+
+    tbl.write(ofile, format="ascii.rst", overwrite=True)
     print(f'Wrote: {ofile}')
 
 
