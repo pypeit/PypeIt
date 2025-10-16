ead5dc1d61972e4e4f0d1b7c2060868c5069dffc
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2025-10-15 17:10:25 -0700

    Add detector tables
    	modified:   doc/include/inst_detector_table.rst
    	modified:   doc/scripts/build_detector_table.py

--- doc/scripts/build_detector_table.py
+++ doc/scripts/build_detector_table.py
@@ -4,9 +4,8 @@ Construct an rst table with the detector properties
 
 from importlib import resources
 
-import numpy
+import astropy.table
 
-from pypeit.utils import string_table
 from pypeit.spectrographs import spectrograph_classes
 
 from IPython import embed
@@ -15,40 +14,57 @@ from IPython import embed
 def write_detector_table(ofile):
 
     allspec = spectrograph_classes()
-    data_table = [['Instrument', 'Det', 'specaxis', 'specflip', 'spatflip', 'namp', 'gain',
-                   'RN', 'darkcurr', 'min', 'sat', 'nonlinear', 'platescale']]
-    for key in allspec.keys():
+
+    data_table = []
+    for key in allspec:
+        # Get the spectrograph
         spec = allspec[key]()
+        # Loop through detectors
         for i in range(spec.ndet):
             det = spec.get_detector_par(i+1)
             if det is None: # For HIRES
                 continue
-            dt_row = [f'``{key}``'] if i == 0 else ['...']
-            dt_row += [str(i+1), str(det.specaxis), str(det.specflip), str(det.spatflip),
-                       '``None``' if det.numamplifiers is None else str(det.numamplifiers),
-                       '``None``' if det.gain is None else ', '.join([str(g) for g in det.gain]),
-                       '``None``' if det.ronoise is None 
-                            else ', '.join([str(r) for r in det.ronoise]),
-                       '``None``' if det.darkcurr is None else str(det.darkcurr),
-                       f'{det.mincounts:.1e}', str(det.saturation), f'{det.nonlinear:.4f}',
-                       '``None``' if det.platescale is None else f'{det.platescale:.4f}']
-            data_table += [dt_row]
+            data_table.append(
+                {
+                    'Instrument': f'``{key}``' if i == 0 else '...',
+                    'Det': str(i+1),
+                    'specaxis': str(det.specaxis),
+                    'specflip': str(det.specflip),
+                    'spatflip': str(det.spatflip),
+                    'namp': '``None``' if det.numamplifiers is None else str(det.numamplifiers),
+                    'gain': '``None``' if det.gain is None else ', '.join([str(g) for g in det.gain]),
+                    'RN': '``None``' if det.ronoise is None else ', '.join([str(r) for r in det.ronoise]),
+                    'darkcurr': '``None``' if det.darkcurr is None else str(det.darkcurr),
+                    'min': f'{det.mincounts:.1e}',
+                    'sat': str(det.saturation),
+                    'nonlinear': f'{det.nonlinear:.4f}',
+                    'platescale': '``None``' if det.platescale is None else f'{det.platescale:.4f}',
+                }
+            )
         if key == 'vlt_fors2':
             # Get the second detector for VLT-FORS2
             det = spec.get_detector_par(2)
-            dt_row = ['...', '2', str(det.specaxis), str(det.specflip), str(det.spatflip),
-                       '``None``' if det.numamplifiers is None else str(det.numamplifiers),
-                       '``None``' if det.gain is None else ', '.join([str(g) for g in det.gain]),
-                       '``None``' if det.ronoise is None 
-                            else ', '.join([str(r) for r in det.ronoise]),
-                       '``None``' if det.darkcurr is None else str(det.darkcurr),
-                       f'{det.mincounts:.1e}', str(det.saturation), f'{det.nonlinear:.4f}',
-                       f'{det.platescale:.4f}']
-            data_table += [dt_row]
+            data_table.append(
+                {
+                    'Instrument': '...',
+                    'Det': '2',
+                    'specaxis': str(det.specaxis),
+                    'specflip': str(det.specflip),
+                    'spatflip': str(det.spatflip),
+                    'namp': '``None``' if det.numamplifiers is None else str(det.numamplifiers),
+                    'gain': '``None``' if det.gain is None else ', '.join([str(g) for g in det.gain]),
+                    'RN': '``None``' if det.ronoise is None else ', '.join([str(r) for r in det.ronoise]),
+                    'darkcurr': '``None``' if det.darkcurr is None else str(det.darkcurr),
+                    'min': f'{det.mincounts:.1e}',
+                    'sat': str(det.saturation),
+                    'nonlinear': f'{det.nonlinear:.4f}',
+                    'platescale': '``None``' if det.platescale is None else f'{det.platescale:.4f}',
+                }
+            )
 
-    lines = string_table(numpy.atleast_1d(data_table), delimeter='rst')
-    with open(ofile, 'w') as f:
-        f.write(lines)
+    tbl = astropy.table.Table(data_table)
+
+    tbl.write(ofile, format="ascii.rst", overwrite=True)
     print(f'Wrote: {ofile}')
 
 
