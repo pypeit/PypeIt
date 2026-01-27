9becc8e4930b0a2a970dddddf7ad600bf7058939
Austin Bowman <astrangbow2@gmail.com>
2026-01-27 11:25:19 -0800

    pulled upstream commit

---
+++ pypeit/dashboard/pypeit_wrapper.py
@@ -0,0 +1,6 @@
+# I will make a wrapper class for pypeit and in this it will inherit all the functions in the pypeit class 
+# but when I want to run it I will overide the run function and have it send me the state the program is at
+
+from pypeit.pypeit import PypeIt
+
+
